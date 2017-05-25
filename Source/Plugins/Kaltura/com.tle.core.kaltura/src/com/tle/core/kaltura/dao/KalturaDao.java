6c3e8efe7cb28e717674f5501c4a96f4561d7e95
cbeach47 <beachcoding47@gmail.com>
2017-05-25 10:24:53 -0500

    Issue #55 - commented out the Kaltura dep and remove the Equella Kaltura code

--- Source/Plugins/Kaltura/com.tle.core.kaltura/src/com/tle/core/kaltura/dao/KalturaDao.java
+++
@@ -1,9 +0,0 @@
-package com.tle.core.kaltura.dao;
-
-import com.tle.common.kaltura.entity.KalturaServer;
-import com.tle.core.dao.AbstractEntityDao;
-
-public interface KalturaDao extends AbstractEntityDao<KalturaServer>
-{
-	// Nothing to see here
-}
--- Source/Plugins/Kaltura/com.tle.web.wizard.controls.kaltura/resources/view/contribute-kaltura.ftl
+++
@@ -1,6 +0,0 @@
-<#ftl strip_whitespace=true />
-<#include "/com.tle.web.freemarker@/macro/sections.ftl"/>
-
-<@css path="kaltura.css" hasRtl=true />
-
-<@render section=s.divKcw />
