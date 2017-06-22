f5698103e681a08cd5f5cfab15af04c89404f2dd
Jolse Maginnis <doolse@gmail.com>
2017-06-22 14:48:58 +1000

    Take screenshot on install fail

--- Tests/src/test/scala/equellatests/SetupForTests.scala
+++ Tests/src/test/scala/equellatests/SetupForTests.scala
@@ -5,17 +5,21 @@ import java.io.File
 import com.tle.webtests.pageobject.UndeterminedPage
 import com.tle.webtests.pageobject.institution._
 import GlobalConfig._
-import com.tle.webtests.framework.PageContext
+import com.tle.webtests.framework.{PageContext, ScreenshotTaker}
 import com.tle.webtests.pageobject.multidb.InstallPage
+
+import scala.util.{Failure, Success, Try}
 
 object SetupForTests extends App {
 
   import java.util.function.{Function => JavaFunction}
+
   implicit def scalaFunctionToJavaFunction[From, To](function: (From) => To): JavaFunction[From, To] = {
     new java.util.function.Function[From, To] {
       override def apply(input: From): To = function(input)
     }
   }
+
   val INSTITUTION_FILE = "institution"
   val DEFAULT_SCHEMA = "Default schema"
 
@@ -30,35 +34,42 @@ object SetupForTests extends App {
 
   if (testConfig.getBooleanProperty("tests.install", false)) {
     TestChecker.withBrowserDriver(testConfig) { driver =>
-      val context = new PageContext(driver, testConfig, testConfig.getAdminUrl)
-      val emails = "noreply@equella.com;test@equella.com"
-      var installPage = new InstallPage(context).load
-      installPage.setPassword("")
-      installPage.setPasswordConfirm("")
-      installPage.setEmails("")
-      installPage.setSmtpServer("")
-      installPage.setNoReply("")
-      installPage = installPage.installInvalid(scalaFunctionToJavaFunction(_.isPasswordError))
-      assert(installPage.isPasswordError)
-      assert(installPage.isEmailsError)
-      assert(installPage.isStmpError)
-      assert(installPage.isNoReplyError)
-      installPage.setPassword(testConfig.getAdminPassword)
-      installPage.setPasswordConfirm(testConfig.getAdminPassword)
-      installPage.setEmails("@@")
-      installPage.setSmtpServer("localhost")
-      installPage.setNoReply("noreply@noreply.com")
-      installPage = installPage.installInvalid(scalaFunctionToJavaFunction(!_.isPasswordError))
-      assert(!installPage.isPasswordError)
-      assert(installPage.isEmailsError)
-      installPage.setPassword(testConfig.getAdminPassword)
-      installPage.setEmails(emails)
-      installPage.setSmtpServer("mail.google.com")
-      val dbPage = installPage.install
-      assert(dbPage.containsDatabase(DEFAULT_SCHEMA))
-      val dbRow = dbPage.getDatabaseRow(DEFAULT_SCHEMA)
-      dbRow.initialise()
-      dbRow.waitForMigrate()
+      Try {
+        val context = new PageContext(driver, testConfig, testConfig.getAdminUrl)
+        val emails = "noreply@equella.com;test@equella.com"
+        var installPage = new InstallPage(context).load
+        installPage.setPassword("")
+        installPage.setPasswordConfirm("")
+        installPage.setEmails("")
+        installPage.setSmtpServer("")
+        installPage.setNoReply("")
+        installPage = installPage.installInvalid(scalaFunctionToJavaFunction(_.isPasswordError))
+        assert(installPage.isPasswordError)
+        assert(installPage.isEmailsError)
+        assert(installPage.isStmpError)
+        assert(installPage.isNoReplyError)
+        installPage.setPassword(testConfig.getAdminPassword)
+        installPage.setPasswordConfirm(testConfig.getAdminPassword)
+        installPage.setEmails("@@")
+        installPage.setSmtpServer("localhost")
+        installPage.setNoReply("noreply@noreply.com")
+        installPage = installPage.installInvalid(scalaFunctionToJavaFunction(!_.isPasswordError))
+        assert(!installPage.isPasswordError)
+        assert(installPage.isEmailsError)
+        installPage.setPassword(testConfig.getAdminPassword)
+        installPage.setEmails(emails)
+        installPage.setSmtpServer("mail.google.com")
+        val dbPage = installPage.install
+        assert(dbPage.containsDatabase(DEFAULT_SCHEMA))
+        val dbRow = dbPage.getDatabaseRow(DEFAULT_SCHEMA)
+        dbRow.initialise()
+        dbRow.waitForMigrate()
+      }.transform(Success.apply, {
+        t =>
+          ScreenshotTaker.takeScreenshot(driver, testConfig.getScreenshotFolder, "install", testConfig.isChromeDriverSet)
+          Failure(t)
+      }
+      )
       driver.quit()
     }
   }
