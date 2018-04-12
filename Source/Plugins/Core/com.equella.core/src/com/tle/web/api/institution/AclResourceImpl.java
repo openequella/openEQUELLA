e3a9f8b7ba3377263365920697735fb081be8ea2
Jolse Maginnis <doolse@gmail.com>
2018-04-12 16:14:14 +1000

    #481 Make it list the entries from the Inst node

--- Source/Plugins/Core/com.equella.core/src/com/tle/web/api/institution/AclResourceImpl.java
+++ Source/Plugins/Core/com.equella.core/src/com/tle/web/api/institution/AclResourceImpl.java
@@ -16,76 +16,50 @@
 
 package com.tle.web.api.institution;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.common.collect.Lists;
+import com.tle.common.institution.CurrentInstitution;
+import com.tle.common.security.PrivilegeTree.Node;
+import com.tle.common.security.TargetList;
+import com.tle.common.security.TargetListEntry;
+import com.tle.core.guice.Bind;
+import com.tle.core.security.TLEAclManager;
+import com.tle.exceptions.AccessDeniedException;
+import com.tle.exceptions.PrivilegeRequiredException;
+import com.tle.web.api.acl.interfaces.AclResource;
+import com.tle.web.api.interfaces.beans.security.TargetListBean;
+import com.tle.web.api.interfaces.beans.security.TargetListEntryBean;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-
-import com.google.common.collect.Lists;
-import com.tle.annotation.Nullable;
-import com.tle.beans.security.AccessEntry;
-import com.tle.common.security.PrivilegeTree.Node;
-import com.tle.common.security.TargetList;
-import com.tle.common.security.TargetListEntry;
-import com.tle.core.guice.Bind;
-import com.tle.core.institution.AclService;
-import com.tle.core.security.TLEAclManager;
-import com.tle.exceptions.AccessDeniedException;
-import com.tle.web.api.acl.interfaces.AclResource;
-import com.tle.web.api.interfaces.beans.security.TargetListBean;
-import com.tle.web.api.interfaces.beans.security.TargetListEntryBean;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * @author larry
+ * @author doolse
  */
 @Bind(AclResource.class)
 @Singleton
 public class AclResourceImpl implements AclResource
 {
 	@Inject
-	private AclService aclService;
-	@Inject
 	private TLEAclManager aclManager;
 
 	@Override
 	public TargetListBean getEntries()
 	{
-		return doGetEntries();
-	}
-
-	@Override
-	public Response setEntries(TargetListBean bean)
-	{
-		TargetList targetList = getTargetList(bean);
-		if( targetList != null )
-		{
-			doSetEntries(targetList);
-			return Response.ok().build();
-		}
-		return Response.status(Status.BAD_REQUEST).build();
-	}
-
-	protected TargetListBean doGetEntries()
-	{
-		if( aclManager.filterNonGrantedPrivileges("VIEW_SECURITY_TREE", "EDIT_SECURITY_TREE").isEmpty() )
-		{
-			throw new AccessDeniedException("One of [VIEW_SECURITY_TREE, EDIT_SECURITY_TREE] is required");
-		}
-
+		checkPrivs("VIEW_SECURITY_TREE", "EDIT_SECURITY_TREE");
 		TargetListBean targetListBean = new TargetListBean();
-		List<AccessEntry> allAcls = aclService.listAll();
+		TargetList allAcls = aclManager.getTargetList(Node.INSTITUTION, null);
 		List<TargetListEntryBean> tBeanList = Lists.newArrayList();
-		for( AccessEntry ae : allAcls )
+		for( TargetListEntry ae : allAcls.getEntries() )
 		{
 			TargetListEntryBean tBean = new TargetListEntryBean();
-			boolean granted = Character.toUpperCase(ae.isGrantRevoke()) == 'G';
-			tBean.setGranted(granted);
-			tBean.setOverride(ae.getExpression().isDynamic());
+			tBean.setGranted(ae.isGranted());
+			tBean.setOverride(ae.isOverride());
 			tBean.setPrivilege(ae.getPrivilege());
-			tBean.setWho(ae.getExpression().getExpression());
+			tBean.setWho(ae.getWho());
 			tBeanList.add(tBean);
 		}
 		targetListBean.setEntries(tBeanList);
@@ -93,39 +67,24 @@ public class AclResourceImpl implements AclResource
 		return targetListBean;
 	}
 
-	protected void doSetEntries(TargetList targetList)
+	public void checkPrivs(String... privs)
 	{
-		if( aclManager.filterNonGrantedPrivileges("EDIT_SECURITY_TREE").isEmpty() )
+		if( aclManager.filterNonGrantedPrivileges(privs).isEmpty() )
 		{
-			throw new AccessDeniedException("One of [EDIT_SECURITY_TREE] is required");
+			throw new PrivilegeRequiredException(privs);
 		}
-		aclManager.setTargetList(Node.INSTITUTION, null, targetList);
+	}
+	@Override
+	public Response setEntries(TargetListBean bean)
+	{
+		checkPrivs("EDIT_SECURITY_TREE");
+		List<TargetListEntry> tle = new ArrayList<>();
+		for (TargetListEntryBean eb : bean.getEntries())
+		{
+			tle.add(new TargetListEntry(eb.isGranted(), eb.isOverride(), eb.getPrivilege(), eb.getWho()));
+		}
+		aclManager.setTargetList(Node.INSTITUTION, CurrentInstitution.get(), new TargetList(tle));
+		return Response.status(Status.OK).build();
 	}
 
-	// FIXME: mostly copied and pasted from AbsractBaseEntityEditor
-	@Nullable
-	protected TargetList getTargetList(@Nullable TargetListBean bean)
-	{
-		if( bean == null )
-		{
-			return null;
-		}
-		final TargetList tl = new TargetList();
-
-		tl.setPartial(false);
-
-		final List<TargetListEntry> tles = new ArrayList<>();
-		tl.setEntries(tles);
-		final List<TargetListEntryBean> rules = bean.getEntries();
-		if( rules != null )
-		{
-			for( TargetListEntryBean rule : rules )
-			{
-				TargetListEntry tle = new TargetListEntry(rule.isGranted(), rule.isOverride(), rule.getPrivilege(),
-					rule.getWho());
-				tles.add(tle);
-			}
-		}
-		return tl;
-	}
 }
