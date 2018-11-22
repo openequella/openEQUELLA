ced5ed3cb619eeb9e7d2c6ee9dcd474a5c6b2968
Aaron Holland <aaron.holland@edalexsolutions.com>
2018-11-23 10:41:42 +1100

    #606, #608 fix dropdown related issues

--- Source/Plugins/Core/com.equella.core/resources/web/js/whereclause.js
+++ Source/Plugins/Core/com.equella.core/resources/web/js/whereclause.js
@@ -1,17 +1,17 @@
-(function($) 
+(function($)
 {
-	$.fn.criteria = function(options) 
+	$.fn.criteria = function(options)
 	{
-		return this.each(function() 
+		return this.each(function()
 		{
 			var settings = {
 				"datewarning" : "",
 				"formwarning" : "",
 				"duplicatewarning" : ""
 			};
-			
-			if(options) 
-			{ 
+
+			if(options)
+			{
 				$.extend( settings, options );
 			}
 
@@ -22,22 +22,22 @@
 			var wherepath = container.find('.criteria-1 .wherepath');
 			var wherevalue = container.find('.criteria-2 .wherevalue');
 			var add = container.find('.whereadd');
-			
+
 			var whereoption = container.find('.criteria-1 .wherestart option[value="WHERE"]');
 			var andoption = container.find('.criteria-1 .wherestart option[value="AND"]');
 			var oroption = container.find('.criteria-1 .wherestart option[value="OR"]');
-			
+
 			whereclauses.ready(function()
 			{
 				var select = whereclauses.get(0);
-				for(var i = 0; i < select.options.length; i++) 
+				for(var i = 0; i < select.options.length; i++)
 				{
 					var criteria = select.options[i].value;
 					addWhereClause(criteria);
 				}
 			});
-			
-			// Removes AND/OR options from select on load. 
+
+			// Removes AND/OR options from select on load.
 			wherestart.ready(function()
 			{
 				buildWhereStart(true);
@@ -52,9 +52,9 @@
 					wherevalue.prop('disabled', false);
 				}
 			});
-			
-			
-			add.click(function() 
+
+
+			add.click(function()
 			{
 				var start = wherestart.val(), path = wherepath.val(), operator = whereoperator.val(), value = wherevalue.val();
 
@@ -69,8 +69,8 @@
 				{
 					alert(settings.formwarning);
 					return false;
-				} 
-				else 
+				}
+				else
 				{
 					// if the operator is >, >=, <, <= then value must be a date
 					var regexp = new RegExp(/^\d{4}-\d\d-\d\d((T)?(\s)?(\d\d:\d\d(:\d\d)?(Z)?)?)?$/gm);
@@ -83,32 +83,32 @@
 							return;
 						}
 					}
-					
+
 					var criteria = start + ' ' + path + ' ' + operator;
 					// The value is only needed if the operator is not 'EXISTS'.
 					if(!isOpExistsSet) {
 						criteria += ' \'' + value + '\'';
 					}
 					if(addWhereClause(criteria))
-					{	
+					{
 						addOption(whereclauses.get(0), criteria, criteria);
 						buildWhereStart(false);
 					}
 				}
 			});
-			
+
 			function buildWhereStart(load)
 			{
 				var list = container.find('ul.criteria-list');
 				var items = list.find('li');
-				
+
 				if(items.length == 0)
 				{
 					wherestart.children().remove();
 					whereoption.appendTo(wherestart);
 					if(!load)
 					{
-						wherestart.resetSS();
+						wherestart.trigger('change');
 					}
 				}
 				else
@@ -119,31 +119,31 @@
 					andoption.attr("selected", "selected")
 					if(!load)
 					{
-						wherestart.resetSS();
+						wherestart.trigger('change');
 					}
 				}
 			}
-			
+
 			function addWhereClause(criteria)
-			{				
+			{
 				// if results list doesn't exist yet, create it
-				if (!container.find('ul.criteria-list').length > 0) 
+				if (!container.find('ul.criteria-list').length > 0)
 				{
 					container.prepend($('<ul class="criteria-list" />'));
 				}
-				
+
 				var list = container.find('ul.criteria-list');
 				var items = list.find('li');
-				
+
 				// if new item already exists in the list, do nothing
 				var exists = false;
-				items.each(function() 
+				items.each(function()
 				{
 					var text = $(this).text();
 					var endtext = text.slice(text.indexOf(' '), text.length);
 					var endcriteria = criteria.slice(criteria.indexOf(' '), criteria.length);
-					
-					if (endtext == endcriteria) 
+
+					if (endtext == endcriteria)
 					{
 						exists = true;
 					}
@@ -158,26 +158,26 @@
 				return true;
 			}
 
-			container.find('.criteria-remove').live('click', function() 
+			container.find('.criteria-remove').live('click', function()
 			{
 				var li = $(this).parent();
 				var ul = li.parent();
-				
+
 				// Remove
 				var value = li.text();
-				removeOption(whereclauses.get(0), value);				
+				removeOption(whereclauses.get(0), value);
 				li.remove();
 
 				var items = ul.find('li');
 
 				if (items.length > 0) {
-					
+
 					// adjust classes
-					items.each(function(index) 
+					items.each(function(index)
 					{
 						var item = $(this);
 						var c = index % 2 != 0 ? 'even' : 'odd';
-						
+
 						// Ghetto hax to change the value
 						if(index == 0)
 						{
@@ -186,39 +186,39 @@
 							var start = text.slice(0, text.indexOf(' '));
 							var end = text.slice(text.indexOf(' '), text.length);
 							var newvalue = "";
-							
+
 							if (start.match("AND"))
 							{
 								start = "WHERE";
-								
+
 							}
 							else if (start.match("OR"))
 							{
 								start = "WHERE";
 							}
-							
+
 							newvalue = start + end;
 							item.text(newvalue);
 							editOption(whereclauses.get(0), text, newvalue);
 							remove.appendTo(item);
 						}
 
-						if (c == 'even') 
+						if (c == 'even')
 						{
 							item.removeClass('odd').addClass('even');
-						} 
-						else 
+						}
+						else
 						{
 							item.removeClass('even').addClass('odd');
 						}
 					});
-				} 
-				else 
+				}
+				else
 				{
 					ul.remove();
-					
+
 					buildWhereStart(false);
-					
+
 				}
 			});
 
