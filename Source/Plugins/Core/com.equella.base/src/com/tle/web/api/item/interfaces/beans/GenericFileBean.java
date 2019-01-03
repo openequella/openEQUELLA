e6956512055787e05d2f12e0a60dd0381199a8d8
Jolse Maginnis <doolse@gmail.com>
2019-01-03 16:52:58 +1100

    #696 Use @JsonTypeName to serialize, no need to deserialize

--- Source/Plugins/Core/com.equella.base/src/com/tle/web/api/item/interfaces/beans/GenericFileBean.java
+++ Source/Plugins/Core/com.equella.base/src/com/tle/web/api/item/interfaces/beans/GenericFileBean.java
@@ -18,19 +18,17 @@ package com.tle.web.api.item.interfaces.beans;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.tle.web.api.interfaces.beans.AbstractExtendableBean;
 
 /**
  * @author Aaron
  */
-@SuppressWarnings("nls")
 @XmlRootElement
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", defaultImpl = GenericFileBean.class)
 public class GenericFileBean extends AbstractExtendableBean
 {
-	public static final String TYPE = "generic";
-
 	private String filename;
 	private GenericFileBean parent;
 
