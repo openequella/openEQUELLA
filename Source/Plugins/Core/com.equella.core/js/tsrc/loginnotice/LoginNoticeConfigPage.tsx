dec91c92e2e6b714e8058f6d2df22724a29016cd
Samantha Fisher <samantha.fisher@edalex.com>
2019-01-21 15:12:47 +1100

    Create initial prototype of login notice editor page

--- Source/Plugins/Core/com.equella.core/js/tsrc/loginnotice/LoginNoticeConfigPage.tsx
+++ Source/Plugins/Core/com.equella.core/js/tsrc/loginnotice/LoginNoticeConfigPage.tsx
@@ -8,6 +8,8 @@ import Button from "@material-ui/core/Button";
 import createStyles from "@material-ui/core/styles/createStyles";
 import TextField from "@material-ui/core/TextField";
 import withStyles from "@material-ui/core/styles/withStyles";
+import axios, {AxiosResponse} from "axios";
+import {Config} from "../config";
 
 const styles = createStyles({
   card: {
@@ -33,22 +35,44 @@ interface LoginNoticeConfigPageProps {
 };
 
 interface LoginNoticeConfigPageState {
+  notice?: string
 };
 
 export const strings = prepLangStrings(
   "loginnoticepage", {
-    title: "Login Notice Editor"
+    title: "Login Notice Editor",
+    label: "Login Notice"
   }
 );
 
 class LoginNoticeConfigPage extends React.Component<LoginNoticeConfigPageProps & WithStyles<typeof styles>, LoginNoticeConfigPageState> {
 
-  handleSubmitTheme = () => {
+  constructor(props:LoginNoticeConfigPageProps&WithStyles<typeof styles>){
+    super(props);
 
+}
+  state: LoginNoticeConfigPageState = {
+    notice: ""
+  };
+
+  handleSubmitTheme = () => {
+    axios.put(
+      `${Config.baseUrl}api/loginnotice/settings/`,
+      this.state.notice
+    ).then(() => {
+      window.location.reload();
+    });
+  };
+
+  handleTextFieldChange = (e: HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement) => {
+    this.setState({notice: e.value});
   };
 
   componentDidMount = () => {
-
+    axios.get(`${Config.baseUrl}api/loginnotice/settings/`,
+    ).then((response: AxiosResponse) => {
+      this.setState({notice: response.data});
+    });
   };
 
   render() {
@@ -57,10 +81,11 @@ class LoginNoticeConfigPage extends React.Component<LoginNoticeConfigPageProps &
     return (
       <Template title={strings.title}>
         <Card className={styles.card} raised>
-          <TextField className={styles.input} label={"Login Notice"} rows={10} variant={"outlined"} multiline autoFocus
-                     placeholder={"<div></div>"}/>
+          <TextField className={styles.input} label={strings.label} rows={10} variant={"outlined"} multiline autoFocus
+                     placeholder={"<div></div>"} onChange={e => this.handleTextFieldChange(e.target)}
+                     value={this.state.notice}/>
           <CardActions className={styles.cardActions}>
-            <Button className={styles.button} variant={"contained"}>Apply</Button>
+            <Button onClick={this.handleSubmitTheme} className={styles.button} variant={"contained"}>Apply</Button>
           </CardActions>
         </Card>
       </Template>);
