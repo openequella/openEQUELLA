22fe2a8f0e5e63558e5737f2ea526a3d2860fcdc
Penghai <Penghai.Zhang@edalex.com>
2019-02-26 09:52:27 +1100

    Change the logic of NewVersionTest
    * handling the new alert when creating a new version of an
    
      item that is in moderation
    
    * refactoring code to reduce duplicated code
    
    Fix a few coding styles

--- autotest/OldTests/src/test/java/com/tle/webtests/test/workflow/rejection/NewVersionTest.java
+++ autotest/OldTests/src/test/java/com/tle/webtests/test/workflow/rejection/NewVersionTest.java
@@ -2,8 +2,6 @@ package com.tle.webtests.test.workflow.rejection;
 
 import static org.testng.Assert.assertEquals;
 
-import org.testng.annotations.Test;
-
 import com.tle.webtests.framework.TestInstitution;
 import com.tle.webtests.pageobject.searching.ItemAdminPage;
 import com.tle.webtests.pageobject.searching.ItemListPage;
@@ -14,82 +12,101 @@ import com.tle.webtests.pageobject.tasklist.TaskListPage;
 import com.tle.webtests.pageobject.wizard.ContributePage;
 import com.tle.webtests.pageobject.wizard.WizardPageTab;
 import com.tle.webtests.test.AbstractCleanupTest;
+import org.testng.annotations.Test;
 
 @TestInstitution("workflow")
-public class NewVersionTest extends AbstractCleanupTest
-{
+public class NewVersionTest extends AbstractCleanupTest {
+  private static final String BACKTICK_PASSWORD = "``````";
+  private static final String SORTING = "datemodified";
+  private static final String ADMIN_USERNAME = "admin";
+  private static final String FIRST_MODERATOR_USERNAME = "SimpleModerator";
+  private static final String SECOND_MODERATOR_USERNAME = "SecondStepModerator";
+  private static final String STEP_NAME_ONE = "moderation step 1";
+  private static final String STEP_NAME_TWO = "moderation step 2";
+  private static final String FAIL_MSG =
+      "A new version of this item cannot be created because there is already a version in moderation.";
 
-	public NewVersionTest()
-	{
-		setDeleteCredentials("admin", "``````");
-	}
+  public NewVersionTest() {
+    setDeleteCredentials(ADMIN_USERNAME, BACKTICK_PASSWORD);
+  }
 
-	@Test
-	public void newVersion()
-	{
+  @Test
+  public void newVersion() {
+    // Login as admin user and contribute an item to the collection
+    logon(ADMIN_USERNAME, BACKTICK_PASSWORD);
+    WizardPageTab wizard =
+        new ContributePage(context).load().openWizard("Move to Live During Moderation");
+    String itemFullName = context.getFullName("item");
+    String itemFullName2 = itemFullName + " 2";
+    saveItem(wizard, 1, itemFullName);
+    logout();
 
-		// Login as admin user and contribute an item to the collection
-		logon("admin", "``````");
+    // Login as SimpleModerator and moderate the item at the first step.
+    logon(FIRST_MODERATOR_USERNAME, BACKTICK_PASSWORD);
+    doModeration(STEP_NAME_ONE, itemFullName, false);
+    logout();
 
-		WizardPageTab wizard = new ContributePage(context).load().openWizard("Move to Live During Moderation");
-		String itemFullName = context.getFullName("item");
-		String itemFullName2 = itemFullName + " 2";
-		wizard.editbox(1, itemFullName);
-		wizard.save().submit();
-		logout();
+    // login as admin to validate that we can't create a new version while an item is still under
+    // moderation
+    logon(ADMIN_USERNAME, BACKTICK_PASSWORD);
+    assertEquals(
+        SearchPage.searchAndView(context, itemFullName).adminTab().newVersionFail(), FAIL_MSG);
+    logout();
 
-		// Login as SimpleModerator and moderate the item at the first step.
-		logon("SimpleModerator", "``````");
+    // Login as the SecondStepModerator and moderate again because this workflow needs moderating
+    // twice.
+    logon(SECOND_MODERATOR_USERNAME, BACKTICK_PASSWORD);
+    doModeration(STEP_NAME_TWO, itemFullName, false);
+    logout();
 
-		TaskListPage taskListPage = new TaskListPage(context).load();
-		ModerateListSearchResults modResults = taskListPage.exactQuery(itemFullName);
-		assertEquals(modResults.getStepName(itemFullName), "moderation step 1");
-		modResults.moderate(itemFullName).accept();
-		logout();
+    // Now the item should be in a state where we're allowed to create a new version.
+    // login as admin and new version it.
+    logon(ADMIN_USERNAME, BACKTICK_PASSWORD);
+    wizard = SearchPage.searchAndView(context, itemFullName).adminTab().newVersion();
+    saveItem(wizard, 1, itemFullName2);
+    // check that there is a version that is live, and one that is moderating.
+    ItemListPage itemList = getItemList(itemFullName);
+    assertEquals(itemList.getResultForTitle(itemFullName, 1).getStatus(), "live");
+    assertEquals(itemList.getResultForTitle(itemFullName2, 1).getStatus(), "moderating");
+    logout();
 
-		// login as the owner of the item and new version it.
-		logon("admin", "``````");
+    // Login as SimpleModerator and moderate the new version of this item at the first step.
+    logon(FIRST_MODERATOR_USERNAME, BACKTICK_PASSWORD);
+    doModeration(STEP_NAME_ONE, itemFullName2, false);
+    // After moderation, check that the item has gone live after the first moderation step
+    itemList = getItemList(itemFullName2);
+    assertEquals(itemList.getResultForTitle(itemFullName2, 1).getStatus(), "live");
+    logout();
 
-		wizard = SearchPage.searchAndView(context, itemFullName).adminTab().newVersion();
-		wizard.editbox(1, itemFullName2);
-		wizard.save().submit();
+    // Login as the SecondStepModerator and check that the item is still
+    // there to moderate, and is still live once moderated from second step.
+    logon(SECOND_MODERATOR_USERNAME, BACKTICK_PASSWORD);
+    doModeration(null, itemFullName2, true);
+    logout();
+  }
 
-		// check that there is a version that is live, and one that is
-		// moderating.
-		ItemAdminPage adminPage = new ItemAdminPage(context).load();
-		adminPage.exactQuery(itemFullName);
-		adminPage.setSort("datemodified");
-		ItemListPage itemList = adminPage.results();
-		assertEquals(itemList.getResultForTitle(itemFullName, 1).getStatus(), "live");
-		assertEquals(itemList.getResultForTitle(itemFullName2, 1).getStatus(), "moderating");
-		logout();
+  private void doModeration(String stepName, String itemFullName, boolean lastModeration) {
+    TaskListPage taskListPage = new TaskListPage(context).load();
+    ModerateListSearchResults modResults = taskListPage.exactQuery(itemFullName);
+    if (!lastModeration) {
+      assertEquals(modResults.getStepName(itemFullName), stepName);
+      modResults.moderate(itemFullName).accept();
+    } else {
+      ModerationView tasksTab = modResults.moderate(itemFullName);
+      tasksTab.assignToMe();
+      tasksTab.accept();
+    }
+  }
 
-		// Login as SimpleModerator and moderate the item at the first step.
-		logon("SimpleModerator", "``````");
+  private ItemListPage getItemList(String itemFullName) {
+    ItemAdminPage adminPage = new ItemAdminPage(context).load();
+    adminPage.exactQuery(itemFullName);
+    adminPage.setSort(SORTING);
+    return adminPage.results();
+  }
 
-		taskListPage = new TaskListPage(context).load();
-		modResults = taskListPage.exactQuery(itemFullName2);
-		assertEquals(modResults.getStepName(itemFullName2), "moderation step 1");
-		modResults.moderate(itemFullName2).accept();
-
-		// After moderation, check that the item has gone life after the first
-		// moderation step
-		adminPage = new ItemAdminPage(context).load();
-		adminPage.exactQuery(itemFullName2);
-		adminPage.setSort("datemodified");
-		itemList = adminPage.results();
-		assertEquals(itemList.getResultForTitle(itemFullName2, 1).getStatus(), "live");
-		logout();
-
-		// Login as the SecondStepModerator and check that the item is still
-		// there to moderate, and is still live once moderated from second step.
-		logon("SecondStepModerator", "``````");
-		taskListPage = new TaskListPage(context).load();
-		modResults = taskListPage.exactQuery(itemFullName2);
-		ModerationView tasksTab = modResults.moderate(itemFullName2);
-		tasksTab.assignToMe();
-		tasksTab.accept();
-		logout();
-	}
-
+  private void saveItem(WizardPageTab wizard, int ctrlNum, String itemFullName) {
+    wizard.editbox(ctrlNum, itemFullName);
+    wizard.save().submit();
+  }
 }
--- autotest/Tests/src/main/java/com/tle/webtests/pageobject/viewitem/ItemPage.java
+++ autotest/Tests/src/main/java/com/tle/webtests/pageobject/viewitem/ItemPage.java
@@ -1,13 +1,5 @@
 package com.tle.webtests.pageobject.viewitem;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
 import com.tle.webtests.framework.PageContext;
 import com.tle.webtests.pageobject.AbstractPage;
 import com.tle.webtests.pageobject.HomePage;
@@ -15,313 +7,290 @@ import com.tle.webtests.pageobject.PageObject;
 import com.tle.webtests.pageobject.WaitingPageObject;
 import com.tle.webtests.pageobject.searching.SearchPage;
 import com.tle.webtests.pageobject.wizard.WizardPageTab;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public abstract class ItemPage<T extends ItemPage<T>> extends AbstractPage<T>
-{
-	private static final String ITEM_DETAILS_XPATH = "id('col2')/h3[text()='Details']/following-sibling::div[1]";
+public abstract class ItemPage<T extends ItemPage<T>> extends AbstractPage<T> {
+  private static final String ITEM_DETAILS_XPATH =
+      "id('col2')/h3[text()='Details']/following-sibling::div[1]";
 
-	public ItemPage(PageContext context)
-	{
-		super(context);
-	}
+  public ItemPage(PageContext context) {
+    super(context);
+  }
 
-	public String getOwner()
-	{
-		return driver.findElement(getOwnerXPath()).getAttribute("title");
-	}
+  public String getOwner() {
+    return driver.findElement(getOwnerXPath()).getAttribute("title");
+  }
 
-	private By getOwnerXPath()
-	{
-		return By.xpath(ITEM_DETAILS_XPATH + "/ul/li[contains(text(), 'Owner:')]/span");
-	}
+  private By getOwnerXPath() {
+    return By.xpath(ITEM_DETAILS_XPATH + "/ul/li[contains(text(), 'Owner:')]/span");
+  }
 
-	// Check the Owner Existing
+  // Check the Owner Existing
 
-	public boolean isOwnerExisting()
-	{
-		return isPresent(getOwnerXPath());
-	}
+  public boolean isOwnerExisting() {
+    return isPresent(getOwnerXPath());
+  }
 
-	// Check the Collaborators Existing
+  // Check the Collaborators Existing
 
-	public boolean isCollaboratorsExisting()
-	{
-		return isPresent(getCollaboratorsXPATH());
+  public boolean isCollaboratorsExisting() {
+    return isPresent(getCollaboratorsXPATH());
+  }
 
-	}
+  public List<String> getCollaborators() {
+    List<String> rv = new ArrayList<String>();
+    for (WebElement we : driver.findElements(getCollaboratorsXPATH())) {
+      rv.add(we.getAttribute("title"));
+    }
+    return rv;
+  }
 
-	public List<String> getCollaborators()
-	{
-		List<String> rv = new ArrayList<String>();
-		for( WebElement we : driver.findElements(getCollaboratorsXPATH()) )
-		{
-			rv.add(we.getAttribute("title"));
-		}
-		return rv;
-	}
+  private By getCollaboratorsXPATH() {
+    return By.xpath(ITEM_DETAILS_XPATH + "/ul/li[contains(text(), 'Collaborators:')]/span");
+  }
 
-	private By getCollaboratorsXPATH()
-	{
-		return By.xpath(ITEM_DETAILS_XPATH + "/ul/li[contains(text(), 'Collaborators:')]/span");
-	}
+  // Check the collection existence
+  public boolean isCollectionExisting() {
+    return isPresent(getCollectionXPath());
+  }
 
-	// Check the collection existence
-	public boolean isCollectionExisting()
-	{
-		return isPresent(getCollectionXPath());
+  public String getCollection() {
+    WebElement element = driver.findElement(getCollectionXPath());
+    return element.getText();
+  }
 
-	}
+  private By getCollectionXPath() {
+    return By.xpath(ITEM_DETAILS_XPATH + "/ul/li[contains(text(), 'Collection:')]/a");
+  }
 
-	public String getCollection()
-	{
-		WebElement element = driver.findElement(getCollectionXPath());
-		return element.getText();
-	}
+  public SearchPage clickCollection() {
+    WebElement element = driver.findElement(getCollectionXPath());
+    element.click();
+    return new SearchPage(context).get();
+  }
 
-	private By getCollectionXPath()
-	{
-		return By.xpath(ITEM_DETAILS_XPATH + "/ul/li[contains(text(), 'Collection:')]/a");
-	}
+  public ItemId getItemId() {
+    String currentUrl = driver.getCurrentUrl();
+    String itemsUrl = currentUrl.substring(context.getBaseUrl().length());
+    Pattern pattern = Pattern.compile(".*(items|gen|bb)/([\\w-]*)/(\\d*)/.*");
+    Matcher matcher = pattern.matcher(itemsUrl);
+    if (!matcher.matches()) {
+      throw new Error("Wrong url:" + currentUrl);
+    }
+    return new ItemId(matcher.group(2), Integer.parseInt(matcher.group(3)));
+  }
 
-	public SearchPage clickCollection()
-	{
-		WebElement element = driver.findElement(getCollectionXPath());
-		element.click();
-		return new SearchPage(context).get();
-	}
+  public ItemId getItemIdFromAttachment() {
+    String currentUrl = driver.getCurrentUrl();
+    String itemsUrl = currentUrl.substring(context.getBaseUrl().length());
+    Pattern pattern = Pattern.compile("file/([\\w-]*)/(\\d*)/.*");
+    Matcher matcher = pattern.matcher(itemsUrl);
+    if (!matcher.matches()) {
+      throw new Error("Wrong url:" + currentUrl);
+    }
+    return new ItemId(matcher.group(1), Integer.parseInt(matcher.group(2)));
+  }
 
-	public ItemId getItemId()
-	{
-		String currentUrl = driver.getCurrentUrl();
-		String itemsUrl = currentUrl.substring(context.getBaseUrl().length());
-		Pattern pattern = Pattern.compile(".*(items|gen|bb)/([\\w-]*)/(\\d*)/.*");
-		Matcher matcher = pattern.matcher(itemsUrl);
-		if( !matcher.matches() )
-		{
-			throw new Error("Wrong url:" + currentUrl);
-		}
-		return new ItemId(matcher.group(2), Integer.parseInt(matcher.group(3)));
-	}
+  public WizardPageTab edit() {
+    return clickAction("Edit this version", new WizardPageTab(context, 0));
+  }
 
-	public ItemId getItemIdFromAttachment()
-	{
-		String currentUrl = driver.getCurrentUrl();
-		String itemsUrl = currentUrl.substring(context.getBaseUrl().length());
-		Pattern pattern = Pattern.compile("file/([\\w-]*)/(\\d*)/.*");
-		Matcher matcher = pattern.matcher(itemsUrl);
-		if( !matcher.matches() )
-		{
-			throw new Error("Wrong url:" + currentUrl);
-		}
-		return new ItemId(matcher.group(1), Integer.parseInt(matcher.group(2)));
-	}
+  public ModerationHistoryPage history() {
+    return clickAction("Moderation history", new ModerationHistoryPage(context));
+  }
 
-	public WizardPageTab edit()
-	{
-		return clickAction("Edit this version", new WizardPageTab(context, 0));
-	}
+  private By getActionBy(String actionText) {
+    return By.xpath("//a[normalize-space(text())=" + quoteXPath(actionText) + "]");
+  }
 
-	public ModerationHistoryPage history()
-	{
-		return clickAction("Moderation history", new ModerationHistoryPage(context));
-	}
+  public boolean hasAction(String actionText) {
+    return isPresent(getActionBy(actionText));
+  }
 
-	private By getActionBy(String actionText)
-	{
-		return By.xpath("//a[normalize-space(text())=" + quoteXPath(actionText) + "]");
-	}
+  private void clickActionOnly(String action) {
+    driver.findElement(getActionBy(action)).click();
+  }
 
-	public boolean hasAction(String actionText)
-	{
-		return isPresent(getActionBy(actionText));
-	}
+  protected <P extends PageObject> P clickAction(String action, WaitingPageObject<P> returnTo) {
+    clickActionOnly(action);
+    return returnTo.get();
+  }
 
-	protected <P extends PageObject> P clickAction(String action, WaitingPageObject<P> returnTo)
-	{
-		driver.findElement(getActionBy(action)).click();
-		return returnTo.get();
-	}
+  protected <P extends PageObject> P clickActionAccept(
+      String action, WaitingPageObject<P> returnTo) {
+    clickActionOnly(action);
+    acceptConfirmation();
+    return returnTo.get();
+  }
 
-	protected <P extends PageObject> P clickActionAccept(String action, WaitingPageObject<P> returnTo)
-	{
-		driver.findElement(getActionBy(action)).click();
-		acceptConfirmation();
-		return returnTo.get();
-	}
+  public WizardPageTab redraft() {
+    return clickAction("Redraft this version", new WizardPageTab(context, 0));
+  }
 
-	public WizardPageTab redraft()
-	{
-		return clickAction("Redraft this version", new WizardPageTab(context, 0));
-	}
+  public T delete() {
+    return clickActionAccept("Delete this version", updateWaiter());
+  }
 
-	public T delete()
-	{
-		return clickActionAccept("Delete this version", updateWaiter());
-	}
+  public T archive() {
+    return clickAction("Archive this version", updateWaiter());
+  }
 
-	public T archive()
-	{
-		return clickAction("Archive this version", updateWaiter());
-	}
+  public T suspend() {
+    return clickActionAccept("Suspend this version", updateWaiter());
+  }
 
-	public T suspend()
-	{
-		return clickActionAccept("Suspend this version", updateWaiter());
-	}
+  public HomePage purge() {
+    return clickActionAccept("Purge from openEQUELLA", new HomePage(context));
+  }
 
-	public HomePage purge()
-	{
-		return clickActionAccept("Purge from openEQUELLA", new HomePage(context));
-	}
+  public T review() {
+    return clickAction("Mark this for review", updateWaiter());
+  }
 
-	public T review()
-	{
-		return clickAction("Mark this for review", updateWaiter());
-	}
+  public WizardPageTab newVersion() {
+    return clickAction("Create a new version", new WizardPageTab(context, 0));
+  }
 
-	public WizardPageTab newVersion()
-	{
-		return clickAction("Create a new version", new WizardPageTab(context, 0));
-	}
+  public String newVersionFail() {
+    clickActionOnly("Create a new version");
+    Alert alert = getWaiter().until(ExpectedConditions.alertIsPresent());
+    String text = alert.getText();
+    alert.accept();
+    return text;
+  }
 
-	public MoveCloneDialog cloneAction()
-	{
-		return clickAction("Clone item into a collection", new MoveCloneDialog(context));
-	}
+  public MoveCloneDialog cloneAction() {
+    return clickAction("Clone item into a collection", new MoveCloneDialog(context));
+  }
 
-	public MoveCloneDialog move()
-	{
-		return clickAction("Move item into another collection", new MoveCloneDialog(context));
-	}
+  public MoveCloneDialog move() {
+    return clickAction("Move item into another collection", new MoveCloneDialog(context));
+  }
 
-	public ModifyKeyResourcePage modifyKeyResource()
-	{
-		return clickAction("Modify key resource", new ModifyKeyResourcePage(context));
-	}
+  public ModifyKeyResourcePage modifyKeyResource() {
+    return clickAction("Modify key resource", new ModifyKeyResourcePage(context));
+  }
 
-	public LMSExportPage lmsPage()
-	{
-		return clickAction("Add to external system", new LMSExportPage(context));
-	}
+  public LMSExportPage lmsPage() {
+    return clickAction("Add to external system", new LMSExportPage(context));
+  }
 
-	public FindUsesPage findUsesPage()
-	{
-		return clickAction("Find uses", new FindUsesPage(context));
-	}
+  public FindUsesPage findUsesPage() {
+    return clickAction("Find uses", new FindUsesPage(context));
+  }
 
-	public ExportItemPage exportPage()
-	{
-		return clickAction("Export", new ExportItemPage(context));
-	}
+  public ExportItemPage exportPage() {
+    return clickAction("Export", new ExportItemPage(context));
+  }
 
-	public TermsOfUsePage termsOfUsePage()
-	{
-		return clickAction("Terms of use", new TermsOfUsePage(context));
-	}
+  public TermsOfUsePage termsOfUsePage() {
+    return clickAction("Terms of use", new TermsOfUsePage(context));
+  }
 
-	public boolean canChangeOwnership()
-	{
-		return hasAction("Change ownership");
-	}
+  public boolean canChangeOwnership() {
+    return hasAction("Change ownership");
+  }
 
-	public boolean canModifyKeyResource()
-	{
-		return hasAction("Modify key resource");
-	}
+  public boolean canModifyKeyResource() {
+    return hasAction("Modify key resource");
+  }
 
-	public ChangeOwnershipPage changeOwnership()
-	{
-		return clickAction("Change ownership", new ChangeOwnershipPage(context));
-	}
+  public ChangeOwnershipPage changeOwnership() {
+    return clickAction("Change ownership", new ChangeOwnershipPage(context));
+  }
 
-	public ItemXmlPage itemXml()
-	{
-		ItemId itemId = getItemId();
-		return new ItemXmlPage(context, itemId).load();
-	}
+  public ItemXmlPage itemXml() {
+    ItemId itemId = getItemId();
+    return new ItemXmlPage(context, itemId).load();
+  }
 
-	public ItemUrlPage tilde()
-	{
-		return new ItemUrlPage(context, getItemId()).viewRoot();
-	}
+  public ItemUrlPage tilde() {
+    return new ItemUrlPage(context, getItemId()).viewRoot();
+  }
 
-	public SummaryPage summary()
-	{
-		driver.findElement(By.xpath("id('breadcrumbs')//a[2]")).click();
-		return new SummaryPage(context).get();
-	}
+  public SummaryPage summary() {
+    driver.findElement(By.xpath("id('breadcrumbs')//a[2]")).click();
+    return new SummaryPage(context).get();
+  }
 
-	public boolean hasMeta(String name, String content)
-	{
-		return isPresent(By.xpath("//head/meta[@name=" + quoteXPath(name) + " and @content=" + quoteXPath(content)
-			+ "]"));
-	}
+  public boolean hasMeta(String name, String content) {
+    return isPresent(
+        By.xpath(
+            "//head/meta[@name="
+                + quoteXPath(name)
+                + " and @content="
+                + quoteXPath(content)
+                + "]"));
+  }
 
-	public String getTitle()
-	{
-		return driver.getTitle();
-	}
+  public String getTitle() {
+    return driver.getTitle();
+  }
 
-	// eCommerece Item Summary Verifcation Method
-	public boolean isPricingTierTitlePresent(String pricingTierTitle)
-	{
-		return driver.findElement(By.xpath("//h3[text()=" + quoteXPath(pricingTierTitle) + ']')).isDisplayed();
+  // eCommerece Item Summary Verifcation Method
+  public boolean isPricingTierTitlePresent(String pricingTierTitle) {
+    return driver
+        .findElement(By.xpath("//h3[text()=" + quoteXPath(pricingTierTitle) + ']'))
+        .isDisplayed();
+  }
 
-	}
+  // eCommerece Item Summary Verifcation Method
+  public boolean isPricingTierPresent(String pricingTier) {
+    return isPresent(
+        By.xpath("//div[@class='tiers']//tbody//td[text() = " + quoteXPath(pricingTier) + "]"));
+  }
 
-	// eCommerece Item Summary Verifcation Method
-	public boolean isPricingTierPresent(String pricingTier)
-	{
-		return isPresent(By.xpath("//div[@class='tiers']//tbody//td[text() = " + quoteXPath(pricingTier) + "]"));
-	}
+  // eCommerece Item Summary Verifcation Method
+  public boolean isSubscripitonTierTablePresent() {
+    return driver.findElement(By.id("sc_ps_subt")).isDisplayed();
+  }
 
-	// eCommerece Item Summary Verifcation Method
-	public boolean isSubscripitonTierTablePresent()
-	{
-		return driver.findElement(By.id("sc_ps_subt")).isDisplayed();
-	}
+  // eCommerece Item Summary Verifcation Method
+  private String xpathForRow(int i) {
+    return ".//tbody/tr[contains(@class, 'odd') or contains(@class, 'even')][" + i + "]";
+  }
 
-	// eCommerece Item Summary Verifcation Method
-	private String xpathForRow(int i)
-	{
-		return ".//tbody/tr[contains(@class, 'odd') or contains(@class, 'even')][" + i + "]";
-	}
+  // eCommerece Item Summary Verifcation Method
+  public String verifyPricingTierAddedAtIndex(int i) {
+    return driver
+        .findElement(By.id("sc_ps_subt"))
+        .findElement(By.xpath(xpathForRow(i) + "/td[1]"))
+        .getText();
+  }
 
-	// eCommerece Item Summary Verifcation Method
-	public String verifyPricingTierAddedAtIndex(int i)
-	{
-		return driver.findElement(By.id("sc_ps_subt")).findElement(By.xpath(xpathForRow(i) + "/td[1]")).getText();
-	}
+  // eCommerece Item Summary Verifcation Method - Verify Purchase Tier Value
+  // Added - Dinuk
+  public String verifyPricingTierValueAddedAtIndex(int rowindex, int columnindex) {
+    Integer col = new Integer(columnindex);
+    String columnid = col.toString();
+    return driver
+        .findElement(By.id("sc_ps_subt"))
+        .findElement(By.xpath(xpathForRow(rowindex) + "/td[" + columnid + "]"))
+        .getText();
+  }
 
-	// eCommerece Item Summary Verifcation Method - Verify Purchase Tier Value
-	// Added - Dinuk
-	public String verifyPricingTierValueAddedAtIndex(int rowindex, int columnindex)
-	{
-		Integer col = new Integer(columnindex);
-		String columnid = col.toString();
-		return driver.findElement(By.id("sc_ps_subt"))
-			.findElement(By.xpath(xpathForRow(rowindex) + "/td[" + columnid + "]")).getText();
-	}
+  public VersionsPage clickShowAllVersion() {
+    driver.findElement(By.xpath("//a[@id='idg_sv']")).click();
+    return new VersionsPage(context).get();
+  }
 
-	public VersionsPage clickShowAllVersion()
-	{
-		driver.findElement(By.xpath("//a[@id='idg_sv']")).click();
-		return new VersionsPage(context).get();
-	}
+  public T unlockItem() {
+    WaitingPageObject<T> updateWaiter = updateWaiter();
+    driver.findElement(By.xpath("//button[text()='Discard editing']")).click();
+    acceptConfirmation();
+    return updateWaiter.get();
+  }
 
-	public T unlockItem()
-	{
-		WaitingPageObject<T> updateWaiter = updateWaiter();
-		driver.findElement(By.xpath("//button[text()='Discard editing']")).click();
-		acceptConfirmation();
-		return updateWaiter.get();
-	}
-
-	public WizardPageTab resumeItem()
-	{
-		driver.findElement(By.xpath("//button[text()='Resume editing']")).click();
-		acceptConfirmation();
-		return new WizardPageTab(context, 0).get();
-	}
-
+  public WizardPageTab resumeItem() {
+    driver.findElement(By.xpath("//button[text()='Resume editing']")).click();
+    acceptConfirmation();
+    return new WizardPageTab(context, 0).get();
+  }
 }
