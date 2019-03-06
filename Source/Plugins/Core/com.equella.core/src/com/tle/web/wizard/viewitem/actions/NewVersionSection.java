bbbf07034f2cec4c4dd8afa3a26ad92af3ff3d53
Penghai <penghai.zhang@edalex.com>
2019-03-06 13:51:33 +1100

    Optimize the REST API and UI of creating new versions (#718)

--- Source/Plugins/Core/com.equella.core/src/com/tle/web/wizard/viewitem/actions/NewVersionSection.java
+++ Source/Plugins/Core/com.equella.core/src/com/tle/web/wizard/viewitem/actions/NewVersionSection.java
@@ -37,7 +37,7 @@ public class NewVersionSection extends AbstractWizardViewItemActionSection {
   private static Label LABEL;
 
   @PlugKey("summary.sidebar.actions.newversion.moderationconflict")
-  private static Label NO_NEW_VERSION_LABEL;
+  private static Label VERSION_IN_MODERATION_ALERT;
 
   @Override
   protected Label getLinkLabel() {
@@ -68,7 +68,7 @@ public class NewVersionSection extends AbstractWizardViewItemActionSection {
       // Display a warning when creating a new version of this item because there is a current
       // version awaiting moderation.
       getComponent()
-          .setClickHandler(context, new OverrideHandler(Js.alert_s(NO_NEW_VERSION_LABEL)));
+          .setClickHandler(context, new OverrideHandler(Js.alert_s(VERSION_IN_MODERATION_ALERT)));
     }
     return super.renderHtml(context);
   }
