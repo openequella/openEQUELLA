5d907e64840340101230858aa3b75b53f4c5297d
C Beach <cbeach@unicon.net>
2019-04-29 14:02:42 -0600

    #858 repo-wide reword of legal headers

--- Source/Plugins/Core/com.equella.admin/src/com/dytech/edge/admin/wizard/model/AbstractControlModel.java
+++ Source/Plugins/Core/com.equella.admin/src/com/dytech/edge/admin/wizard/model/AbstractControlModel.java
@@ -1,9 +1,11 @@
 /*
- * Copyright 2017 Apereo
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0, (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
