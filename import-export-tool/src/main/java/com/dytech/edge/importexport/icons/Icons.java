3d4dc0f24b120f5e2f33cae673c049482ce2c92f
PenghaiZhang <47203811+PenghaiZhang@users.noreply.github.com>
2019-05-10 16:20:51 +1000

    Bugfix/build import export (#879)
    * Reuild the import/export tool with gradle
    
    * Call ./gradlew assemble to create jars of these two tools

--- import-export-tool/src/main/java/com/dytech/edge/importexport/icons/Icons.java
+++ import-export-tool/src/main/java/com/dytech/edge/importexport/icons/Icons.java
@@ -6,18 +6,19 @@ import javax.swing.ImageIcon;
 @SuppressWarnings("nls")
 public class Icons {
   public static ImageIcon getErrorIcon() {
-    return new ImageIcon(Icons.class.getResource("error.gif"));
+    return new ImageIcon(Icons.class.getResource("/icons/error.gif"));
   }
 
   public static JImage getHeader(boolean export) {
-    return new JImage(Icons.class.getResource((export ? "exportheader.gif" : "importheader.gif")));
+    return new JImage(
+        Icons.class.getResource((export ? "/icons/exportheader.gif" : "/icons/importheader.gif")));
   }
 
   public static JImage getFooter() {
-    return new JImage(Icons.class.getResource("footer.gif"));
+    return new JImage(Icons.class.getResource("/icons/footer.gif"));
   }
 
   public static JImage getWorking() {
-    return new JImage(Icons.class.getResource("working.gif"));
+    return new JImage(Icons.class.getResource("/icons/working.gif"));
   }
 }
