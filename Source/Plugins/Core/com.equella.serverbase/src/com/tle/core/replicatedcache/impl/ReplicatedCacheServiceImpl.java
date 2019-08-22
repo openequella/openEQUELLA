3cbe18c5289b1eda194021257dd4737a379d5a87
Penghai <penghai.zhang@edalex.com>
2019-08-22 15:37:01 +1000

    Refresh cache only when working in clustering environment

--- Source/Plugins/Core/com.equella.serverbase/src/com/tle/core/replicatedcache/impl/ReplicatedCacheServiceImpl.java
+++ Source/Plugins/Core/com.equella.serverbase/src/com/tle/core/replicatedcache/impl/ReplicatedCacheServiceImpl.java
@@ -144,7 +144,9 @@ public class ReplicatedCacheServiceImpl
     @Override
     public synchronized Optional<V> get(@NonNull String key) {
       checkNotNull(key);
-      cache.refresh(CurrentInstitution.get());
+      if (zookeeperService.isCluster()) {
+        cache.refresh(CurrentInstitution.get());
+      }
 
       LoadingCache<String, Optional<ExpiringValue<V>>> c =
           cache.getIfPresent(CurrentInstitution.get());
