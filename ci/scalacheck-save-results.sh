a57bb284a2f7fbe5ec91e149d8a76bce1e315b3c
Ian Stevenson <ian.stevenson@edalex.com>
2019-09-05 12:38:59 +1000

    Only copy to S3 when AWS creds available and not on forks
    On Travis, encrypted variables are not shared for builds on forks as a
    security measure. Therefore, we need to check whether credentials are
    available before attempting S3 copying etc.
    
    And seeing the `finalise` stage is only really about updating S3, we
    can completely disable.

--- ci/scalacheck-save-results.sh
+++ ci/scalacheck-save-results.sh
@@ -4,12 +4,12 @@ SCRIPTS_DIR=`dirname $0`
 SUFFIX=scalacheck-$1
 
 echo Saving Scalacheck results
-aws s3 $S3_REGION_OPT cp autotest/Tests/target/test-reports \
+${SCRIPTS_DIR}/s3cp.sh autotest/Tests/target/test-reports \
   ${S3_DEST_BUILD}${SUFFIX}/ \
   $S3_CP_OPTS
 
 echo Copying oEQ log
-aws s3 $S3_REGION_OPT cp autotest/equella-install/logs \
+${SCRIPTS_DIR}/s3cp.sh autotest/equella-install/logs \
   ${S3_DEST_BUILD}oeq-logs/${SUFFIX} \
   $S3_CP_OPTS
 
--- ci/tests-save-screenshots.sh
+++ ci/tests-save-screenshots.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+SCRIPTS_DIR=`dirname $0`
 SUFFIX=$1
 
-aws s3 $S3_REGION_OPT cp autotest/Tests/target/test-reports/screenshots \
+${SCRIPTS_DIR}/s3cp.sh autotest/Tests/target/test-reports/screenshots \
   ${S3_DEST_BUILD}screenshots/${SUFFIX}/ \
   $S3_CP_OPTS
