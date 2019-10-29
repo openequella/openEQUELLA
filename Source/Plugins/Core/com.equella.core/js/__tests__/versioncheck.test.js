9973652ac5f1acda3073b2024c1eca63571b3e09
PenghaiZhang <47203811+PenghaiZhang@users.noreply.github.com>
2019-10-30 09:47:55 +1100

    Change the functionality of version checking (#1314)
    * Make oEQ call Github api to retrieve new oEQ releases information
      instead of calling the Version Server
    
    * For major, minor and patch updates, the Health check page shows update
      information for each of them
    
    * Add a Jest test for the version checking.

---
+++ Source/Plugins/Core/com.equella.core/js/__tests__/versioncheck.test.js
@@ -0,0 +1,43 @@
+// versioncheck.js is not processed by Parcel and it only serves to the browser.
+// So here need to read the file and then evaluate its content
+const mockData = require("../__mocks__/versioncheck_mock_data");
+const fs = require("fs");
+const version_check_js_file = fs.readFileSync(
+  "../resources/web/js/versioncheck.js",
+  "utf8"
+);
+eval(version_check_js_file);
+
+test("Test major updates", () => {
+  const checkResult = createCheckResult("2018.1.0", mockData.mockReleases);
+  expect(checkResult.newer).toBe(true);
+  expect(checkResult.newerReleases.majorUpdate.major).toBe("2019");
+});
+
+test("Test minor updates", () => {
+  const checkResult = createCheckResult("2019.1.1", mockData.mockReleases);
+  expect(checkResult.newer).toBe(true);
+
+  const minorUpdateRelease = checkResult.newerReleases.minorUpdate;
+  expect(minorUpdateRelease.major).toBe("2019");
+  expect(minorUpdateRelease.minor).toBe("2");
+});
+
+test("Test patch updates", () => {
+  const checkResult = createCheckResult("2019.2.0", mockData.mockReleases);
+  expect(checkResult.newer).toBe(true);
+
+  const patchUpdateRelease = checkResult.newerReleases.patchUpdate;
+  expect(patchUpdateRelease.major).toBe("2019");
+  expect(patchUpdateRelease.minor).toBe("2");
+  expect(patchUpdateRelease.patch).toBe("1");
+});
+
+test("Test no updates", () => {
+  const checkResult = createCheckResult("2020.1.0", mockData.mockReleases);
+  expect(checkResult.newer).toBe(false);
+
+  expect(checkResult.newerReleases.majorUpdate).toBe(null);
+  expect(checkResult.newerReleases.minorUpdate).toBe(null);
+  expect(checkResult.newerReleases.patchUpdate).toBe(null);
+});
