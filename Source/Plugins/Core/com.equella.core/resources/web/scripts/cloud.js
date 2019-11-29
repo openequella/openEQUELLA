c3d4a5a6d84fe76f58a547e2c7e9f2126f77b573
Aaron Holland <aaron.holland@edalexsolutions.com>
2019-11-29 15:23:05 +1100

    Delay the cloud search until after page rendered by React
     #1162

--- Source/Plugins/Core/com.equella.core/resources/web/scripts/cloud.js
+++ Source/Plugins/Core/com.equella.core/resources/web/scripts/cloud.js
@@ -1,17 +1,15 @@
-var Cloud = 
-{
-		onSearch: function(cb, $span, searchingText)
-		{
-			//TODO: spinner
-			$span.text(searchingText);
-			
-			var updateCount = function(data)
-			{
-				if ($span[0] && $.contains(document, $span[0]))
-				{
-					$span.text(data.text);
-				}
-			}
-			cb.call(null, updateCount);
-		}
+var Cloud = {
+  onSearch: function(cb, $span, searchingText) {
+    setTimeout(function() {
+      //TODO: spinner
+      $span.text(searchingText);
+
+      var updateCount = function(data) {
+        if ($span[0] && $.contains(document, $span[0])) {
+          $span.text(data.text);
+        }
+      };
+      cb.call(null, updateCount);
+    }, 0);
+  }
 };
