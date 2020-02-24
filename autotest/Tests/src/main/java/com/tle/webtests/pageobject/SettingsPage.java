a3f9e4519b99d1dafdaa726a79076670795da54e
Samantha Fisher <samantha.fisher@edalex.com>
2020-02-24 13:53:07 +1100

    Update SearchSettingsTest to work with new category

--- autotest/Tests/src/main/java/com/tle/webtests/pageobject/SettingsPage.java
+++ autotest/Tests/src/main/java/com/tle/webtests/pageobject/SettingsPage.java
@@ -29,6 +29,7 @@ public class SettingsPage extends AbstractPage<SettingsPage> {
   public static final String SEARCH_SETTINGS_LINK_TITLE = "Searching and content indexing";
   public static final String COURSE_DEFAULTS_LINK_TITLE = "Copyright";
   private static final String SEARCH_SETTING_TITLE = "Searching and content indexing";
+  private static final String GROUP_SEARCHING = "Searching";
   private static final String GROUP_INTEGRATIONS = "Integrations";
 
   public SettingsPage(PageContext context) {
@@ -93,7 +94,8 @@ public class SettingsPage extends AbstractPage<SettingsPage> {
   }
 
   public SearchSettingsPage searchSettings() {
-    return clickSetting(SEARCH_SETTINGS_LINK_TITLE, new SearchSettingsPage(context));
+    return clickSetting(
+        GROUP_SEARCHING, SEARCH_SETTINGS_LINK_TITLE, new SearchSettingsPage(context));
   }
 
   public CourseDefaultsPage courseDefaultsSettings() {
