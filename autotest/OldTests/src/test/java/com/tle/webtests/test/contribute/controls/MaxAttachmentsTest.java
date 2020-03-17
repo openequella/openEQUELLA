2bd2061849cca8cc746de4fe262ea7ec781bece6
PenghaiZhang <47203811+PenghaiZhang@users.noreply.github.com>
2020-03-18 09:57:58 +1100

    Remove the usage of Youtube attachment/tool for three auto-tests (#1542)
    * Update MaxAttachmentsTest and its testing Collection
    
    * Update ItemUnlock Test and its testing Collection
    
    * Update ViewerTest and its testing Item

--- autotest/OldTests/src/test/java/com/tle/webtests/test/contribute/controls/MaxAttachmentsTest.java
+++ autotest/OldTests/src/test/java/com/tle/webtests/test/contribute/controls/MaxAttachmentsTest.java
@@ -4,7 +4,7 @@ import com.tle.webtests.framework.TestInstitution;
 import com.tle.webtests.pageobject.wizard.ContributePage;
 import com.tle.webtests.pageobject.wizard.WizardPageTab;
 import com.tle.webtests.pageobject.wizard.controls.UniversalControl;
-import com.tle.webtests.pageobject.wizard.controls.universal.YouTubeUniversalControlType;
+import com.tle.webtests.pageobject.wizard.controls.universal.UrlUniversalControlType;
 import com.tle.webtests.test.AbstractCleanupAutoTest;
 import com.tle.webtests.test.files.Attachments;
 import java.net.URL;
@@ -15,8 +15,8 @@ import org.testng.annotations.Test;
 public class MaxAttachmentsTest extends AbstractCleanupAutoTest {
   private static final String COLLECTION = "Max attachments restriction";
   private final URL[] ATTACHMENTS = {Attachments.get("page.html"), Attachments.get("pageB.html")};
-  private final String ORIGINAL_TITLE = "Peter Andre - Mysterious Girl (Official Music Video)";
-  private final String YOUTUBE_TITLE = "good song";
+  private final String LINK_ATTACHMENT_URL = "https://www.google.com";
+  private final String LINK_ATTACHMENT_NAME = "Google";
 
   @Test
   public void maxFiles() {
@@ -25,19 +25,15 @@ public class MaxAttachmentsTest extends AbstractCleanupAutoTest {
     wizard.editbox(1, fullName);
     UniversalControl control = wizard.universalControl(2);
 
-    YouTubeUniversalControlType youTubeControl =
-        control.addResource(new YouTubeUniversalControlType(control));
-    youTubeControl.search("mysterious girl peter andre", null).selectVideo(1, ORIGINAL_TITLE);
-    control
-        .editResource(youTubeControl.editPage(), ORIGINAL_TITLE)
-        .setDisplayName(YOUTUBE_TITLE)
-        .save();
+    UrlUniversalControlType urlControl = control.addResource(new UrlUniversalControlType(control));
+    urlControl.addUrl(LINK_ATTACHMENT_URL, LINK_ATTACHMENT_NAME);
+
     wizard.addFiles(2, false, ATTACHMENTS);
     wizard.save().finishInvalid(wizard);
     Assert.assertEquals(
         wizard.getErrorMessage(2),
         "This control is restricted to a maximum of 2 attachments. Please remove 1 attachment(s).");
-    control.deleteResource(YOUTUBE_TITLE);
+    control.deleteResource(LINK_ATTACHMENT_NAME);
     wizard.save().publish();
   }
 }
