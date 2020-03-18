201a8a3887fdf287deabd6d8e51482cab512b270
Samantha Fisher <samantha.fisher@edalex.com>
2020-03-18 16:28:00 +1100

    Delete obsolete js version of jest.setup.js

--- Source/Plugins/Core/com.equella.core/js/jest.setup.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var enzyme_1 = require("enzyme");
-var Adapter = require("enzyme-adapter-react-16");
-enzyme_1.configure({ adapter: new Adapter() });
