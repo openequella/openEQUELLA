51d7bc0402b0aedfca5c42661f3438d3c0119049
Samantha Fisher <samantha.fisher@edalex.com>
2020-03-27 10:39:11 +1100

    Use outlined version of Select

--- Source/Plugins/Core/com.equella.core/js/tsrc/settings/Search/components/DefaultSortOrderSetting.tsx
+++ Source/Plugins/Core/com.equella.core/js/tsrc/settings/Search/components/DefaultSortOrderSetting.tsx
@@ -1,13 +1,24 @@
-import { FormControl, FormLabel, MenuItem, Select } from "@material-ui/core";
+import {
+  FormControl,
+  MenuItem,
+  OutlinedInput,
+  Select
+} from "@material-ui/core";
 import { SortOrder } from "../SearchSettingsModule";
 import * as React from "react";
 import { languageStrings } from "../../../util/langstrings";
+import { makeStyles } from "@material-ui/styles";
 
 export interface DefaultSortOrderSettingProps {
   disabled: boolean;
   value: SortOrder;
   setValue: (order: SortOrder) => void;
 }
+const useStyles = makeStyles({
+  select: {
+    width: "200px"
+  }
+});
 export default function DefaultSortOrderSetting({
   disabled,
   value,
@@ -15,32 +26,36 @@ export default function DefaultSortOrderSetting({
 }: DefaultSortOrderSettingProps) {
   const searchPageSettingsStrings =
     languageStrings.settings.searching.searchPageSettings;
+  const classes = useStyles();
   return (
-    <FormControl>
-      <FormLabel>{searchPageSettingsStrings.defaultSortOrder}</FormLabel>
-      <Select
-        SelectDisplayProps={{ id: "_sortOrder" }}
-        disabled={disabled}
-        onChange={event => setValue(event.target.value as SortOrder)}
-        variant={"standard"}
-        value={value}
-      >
-        <MenuItem value={SortOrder.RANK}>
-          {searchPageSettingsStrings.relevance}
-        </MenuItem>
-        <MenuItem value={SortOrder.DATEMODIFIED}>
-          {searchPageSettingsStrings.lastModified}
-        </MenuItem>
-        <MenuItem value={SortOrder.DATECREATED}>
-          {searchPageSettingsStrings.dateCreated}
-        </MenuItem>
-        <MenuItem value={SortOrder.NAME}>
-          {searchPageSettingsStrings.title}
-        </MenuItem>
-        <MenuItem value={SortOrder.RATING}>
-          {searchPageSettingsStrings.userRating}
-        </MenuItem>
-      </Select>
-    </FormControl>
+    <>
+      <FormControl variant={"outlined"}>
+        <Select
+          SelectDisplayProps={{ id: "_sortOrder" }}
+          disabled={disabled}
+          onChange={event => setValue(event.target.value as SortOrder)}
+          variant={"outlined"}
+          value={value}
+          className={classes.select}
+          input={<OutlinedInput labelWidth={0} id={"_sortOrder"} />}
+        >
+          <MenuItem value={SortOrder.RANK}>
+            {searchPageSettingsStrings.relevance}
+          </MenuItem>
+          <MenuItem value={SortOrder.DATEMODIFIED}>
+            {searchPageSettingsStrings.lastModified}
+          </MenuItem>
+          <MenuItem value={SortOrder.DATECREATED}>
+            {searchPageSettingsStrings.dateCreated}
+          </MenuItem>
+          <MenuItem value={SortOrder.NAME}>
+            {searchPageSettingsStrings.title}
+          </MenuItem>
+          <MenuItem value={SortOrder.RATING}>
+            {searchPageSettingsStrings.userRating}
+          </MenuItem>
+        </Select>
+      </FormControl>
+    </>
   );
 }
