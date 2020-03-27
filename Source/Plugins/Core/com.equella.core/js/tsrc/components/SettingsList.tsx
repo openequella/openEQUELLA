92634891f6d299f5396d7c742900abaab3f7ce35
Samantha Fisher <samantha.fisher@edalex.com>
2020-03-27 12:45:11 +1100

    Fix JSDoc on SettingsList.tsx

--- Source/Plugins/Core/com.equella.core/js/tsrc/components/SettingsList.tsx
+++ Source/Plugins/Core/com.equella.core/js/tsrc/components/SettingsList.tsx
@@ -1,17 +1,15 @@
 import { List, ListSubheader } from "@material-ui/core";
 import * as React from "react";
-import { ReactNode } from "react";
 
 interface SettingsListProps {
   subHeading?: string;
-  children: ReactNode;
+  children: React.ReactNode;
 }
 /*
  * This component is used to define a settings list to be used in the page/settings/* pages.
- * It should be placed within a Card,
- * and the children of this components should be SettingsListControls.
- * subHeading: optional title of the settings list.
- * children: 0 or more SettingsListControls representing the rows of this list.
+ * The children of this components should be SettingsListControls.
+ * @param subHeading  optional title of the settings list.
+ * @param children    0 or more SettingsListControls representing the rows of this list.
  */
 export default function SettingsList({
   subHeading,
