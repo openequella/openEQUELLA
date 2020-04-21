977dfb25d94033b8f71d818ee85b831040b0b027
Samantha Fisher <samantha.fisher@edalex.com>
2020-04-21 11:13:27 +1000

    Add SettingsToggleSwitch story

--- Source/Plugins/Core/com.equella.core/js/tsrc/components/SettingsToggleSwitch.tsx
+++ Source/Plugins/Core/com.equella.core/js/tsrc/components/SettingsToggleSwitch.tsx
@@ -3,24 +3,38 @@ import { Switch } from "@material-ui/core";
 import * as React from "react";
 
 export interface SettingsToggleSwitchProps {
+  /**
+   * The value of the switch.
+   */
   value?: boolean;
+  /**
+   * A function that gets called upon change of the control value.
+   * @param {boolean} value - The new value of the control.
+   */
   setValue: (value: boolean) => void;
+  /**
+   * Determines whether the control is interactive.
+   */
   disabled?: boolean;
+  /**
+   * Optional label.
+   */
   label?: string;
+  /**
+   * Optional title string.
+   */
   title?: string;
+  /**
+   * Optional secondary label.
+   */
   formHelperText?: string;
+  /**
+   * ID of the control.
+   */
   id: string;
 }
-/*
+/**
  * This component is used to define a boolean toggle control using a Material UI switch.
- *
- * @param value                 The value of the switch
- * @param setValue              the function called when the value changes
- * @param disabled              Whether or not the control is interactible
- * @param label                 Optional label
- * @param title                 Optional title string
- * @param formHelperText        Optional secondary label
- * @param id                    ID of the control
  */
 export default function SettingsToggleSwitch({
   disabled,
