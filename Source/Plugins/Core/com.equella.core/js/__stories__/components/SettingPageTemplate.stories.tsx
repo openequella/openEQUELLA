d63a3abc99493c4399cf49ab2a6bc7807974e2a0
Penghai <penghai.zhang@edalex.com>
2020-04-29 14:32:19 +1000

    Describe SettingPageTemplate story with Knobs

--- Source/Plugins/Core/com.equella.core/js/__stories__/components/SettingPageTemplate.stories.tsx
+++ Source/Plugins/Core/com.equella.core/js/__stories__/components/SettingPageTemplate.stories.tsx
@@ -2,14 +2,13 @@ import * as React from "react";
 import { action } from "@storybook/addon-actions";
 import SettingPageTemplate from "../../tsrc/components/SettingPageTemplate";
 import { Card } from "@material-ui/core";
+import { boolean } from "@storybook/addon-knobs";
 
 export default {
   title: "SettingPageTemplate",
   component: SettingPageTemplate,
 };
 
-const onClose = action("Close snackbar");
-const onSave = action("save");
 const children = (
   <Card>
     <h3>SettingPageTemplate Story</h3>
@@ -18,23 +17,11 @@ const children = (
 
 export const saveButtonEnabled = () => (
   <SettingPageTemplate
-    onSave={onSave}
-    saveButtonDisabled={false}
-    snackbarOpen={false}
-    preventNavigation
-    snackBarOnClose={onClose}
-  >
-    {children}
-  </SettingPageTemplate>
-);
-
-export const snackbarDisplayed = () => (
-  <SettingPageTemplate
-    onSave={onSave}
-    saveButtonDisabled={true}
-    snackbarOpen={true}
-    preventNavigation
-    snackBarOnClose={onClose}
+    onSave={action("save")}
+    saveButtonDisabled={boolean("disable Save butto", false)}
+    snackbarOpen={boolean("open snack bar", false)}
+    preventNavigation={boolean("prevent navigation", true)}
+    snackBarOnClose={action("Close snackbar")}
   >
     {children}
   </SettingPageTemplate>
