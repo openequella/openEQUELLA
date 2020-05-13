338bb91737ae0f06d2a450442a9d72b20431005c
Ian Stevenson <ian.stevenson@edalex.com>
2020-05-13 12:07:51 +1000

    Remove unnecessary regenerator-runtime/runtime.js import
    Looks like the need for this was a side effect of not building a
    CommonJS format module and having main pointing to it.

--- oeq-ts-rest-api/src/index.ts
+++ oeq-ts-rest-api/src/index.ts
@@ -1,6 +1,3 @@
-// Needed to stop error of: "regeneratorRuntime is not defined"
-import "regenerator-runtime/runtime.js";
-
 export * as Auth from './Auth';
 export * as LegacyContent from './LegacyContent';
 export * as Errors from './Errors';
