50f2ad6dff460b5546149e35e711e40e58b8019d
Penghai <penghai.zhang@edalex.com>
2020-05-14 14:58:28 +1000

    Remove redundant 'describe' from the Jest jest of Facet setting page;
    Use props() to access one of the props of a component;
    
    Use enum values to indicate if the action is to add or edit;

--- Source/Plugins/Core/com.equella.core/js/__tests__/tsrc/mainui/FacetedSearchSettingsPage.test.tsx
+++ Source/Plugins/Core/com.equella.core/js/__tests__/tsrc/mainui/FacetedSearchSettingsPage.test.tsx
@@ -60,6 +60,10 @@ const mockBatchUpdateOrAdd = jest.spyOn(
 );
 
 describe("<FacetedSearchSettingsPage />", () => {
+  enum Actions {
+    Edit,
+    Add,
+  }
   let component: ReactWrapper;
   beforeEach(async () => {
     const getFacetsPromise = mockGetFacetsFromServer.mockImplementation(() =>
@@ -73,127 +77,116 @@ describe("<FacetedSearchSettingsPage />", () => {
   });
   afterEach(() => jest.clearAllMocks());
 
-  describe("when the page is mounted", () => {
-    const getListItems = () => component.find(".MuiListItem-container");
-    const getSaveButton = () => component.find("#_saveButton").hostNodes();
-    const deleteFacet = () => {
-      const deleteButton = getListItems().at(0).find("button").at(1);
-      deleteButton.simulate("click");
+  const getListItems = () => component.find(".MuiListItem-container");
+  const getSaveButton = () => component.find("#_saveButton").hostNodes();
+  const deleteFacet = () => {
+    const deleteButton = getListItems().at(0).find("button").at(1);
+    deleteButton.simulate("click");
+  };
+  const updateFacet = (action: Actions) => {
+    const fields = component.find("input");
+    fields.at(0).simulate("change", {
+      target: { value: action === Actions.Add ? "new facet" : "updated facet" },
+    });
+    fields.at(1).simulate("change", { target: { value: "item/name/first" } });
+    const button = component
+      .findWhere(
+        (node) =>
+          node.text() === (action === Actions.Add ? "Add" : "OK") &&
+          node.type() === "button"
+      )
+      .hostNodes();
+    button.simulate("click");
+  };
+
+  const openDialog = (action: Actions) => {
+    let button: ReactWrapper;
+    if (action === Actions.Add) {
+      button = component.find(".MuiCardActions-root button");
+    } else {
+      button = getListItems().at(0).find("button").at(0);
+    }
+    button.simulate("click");
+  };
+
+  it("should fetch facets", () => {
+    expect(
+      FacetedSearchSettingsModule.getFacetsFromServer
+    ).toHaveBeenCalledTimes(1);
+  });
+
+  it("should display a list of facets", () => {
+    expect(getListItems()).toHaveLength(2);
+  });
+
+  it("should show a dialog when an Edit button is clicked", async () => {
+    openDialog(Actions.Edit);
+    expect(component.find(FacetDialog).props().open).toBeTruthy();
+  });
+
+  it("should show a dialog when the Add button is clicked", async () => {
+    openDialog(Actions.Add);
+    expect(component.find(FacetDialog).props().open).toBeTruthy();
+  });
+
+  it("should remove a facet from the list and enable the Save button when a delete button is clicked", () => {
+    deleteFacet();
+    const items = getListItems();
+    expect(items).toHaveLength(1);
+    expect(getSaveButton().props().disabled).toBeFalsy();
+  });
+
+  it("should show the updated facet and enable the Save button when a facet is edited through the dialog", () => {
+    openDialog(Actions.Edit);
+    updateFacet(Actions.Edit);
+    const items = getListItems();
+    expect(items).toHaveLength(2);
+    expect(items.at(0).text()).toContain("updated facet");
+    expect(getSaveButton().props().disabled).toBeFalsy();
+  });
+
+  it("should add a facet to the list and enable the Save button when a new facet is added through the dialog", () => {
+    openDialog(Actions.Add);
+    updateFacet(Actions.Add);
+    const items = getListItems();
+    expect(items).toHaveLength(3);
+    expect(items.at(2).text()).toContain("new facet");
+    expect(getSaveButton().props().disabled).toBeFalsy();
+  });
+
+  describe("when the Save button is enabled and clicked", () => {
+    const makeChanges = () => {
+      deleteFacet();
+      openDialog(Actions.Edit);
+      updateFacet(Actions.Edit);
     };
-    const updateFacet = (addOrEdit: boolean) => {
-      const fields = component.find("input");
-      fields.at(0).simulate("change", {
-        target: { value: addOrEdit ? "new facet" : "updated facet" },
+    const save = async (errorsReturned: boolean) => {
+      const deletePromise = mockBatchDelete.mockResolvedValueOnce(
+        errorsReturned ? ["Failed to delete"] : []
+      );
+      const updatePromise = mockBatchUpdateOrAdd.mockResolvedValueOnce(
+        errorsReturned ? ["Failed to update"] : []
+      );
+      makeChanges();
+      getSaveButton().simulate("click");
+      await act(async () => {
+        await updatePromise;
+        await deletePromise;
       });
-      fields.at(1).simulate("change", { target: { value: "item/name/first" } });
-      const okButton = component
-        .findWhere(
-          (node) =>
-            node.text() === (addOrEdit ? "Add" : "OK") &&
-            node.type() === "button"
-        )
-        .hostNodes();
-      okButton.simulate("click");
-    };
-    const openDialog = (addOrEdit: boolean) => {
-      let button: ReactWrapper;
-      if (addOrEdit) {
-        button = component.find(".MuiCardActions-root button");
-      } else {
-        button = getListItems().at(0).find("button").at(0);
-      }
-      button.simulate("click");
     };
 
-    it("should fetch facets", () => {
-      expect(
-        FacetedSearchSettingsModule.getFacetsFromServer
-      ).toHaveBeenCalledTimes(1);
+    it("should display snackbar if no error messages are returned", async () => {
+      await save(false);
+      component.update();
+      const snackbar = component.find(MessageInfo);
+      expect(snackbar.props().open).toBeTruthy();
     });
 
-    it("should display a list of facets", () => {
-      expect(getListItems()).toHaveLength(2);
-    });
-
-    describe("when an Edit button is clicked", () => {
-      it("should show a dialog", async () => {
-        openDialog(false);
-        expect(component.find(FacetDialog).prop("open")).toBeTruthy();
-      });
-    });
-
-    describe("when the Add button is clicked", () => {
-      it("should show a dialog", async () => {
-        openDialog(true);
-        expect(component.find(FacetDialog).prop("open")).toBeTruthy();
-      });
-    });
-
-    describe("when a delete button is clicked", () => {
-      it("should remove a facet from the list and enable the Save button", () => {
-        deleteFacet();
-        const items = getListItems();
-        expect(items).toHaveLength(1);
-        expect(getSaveButton().prop("disabled")).toBeFalsy();
-      });
-    });
-
-    describe("when a facet is edited through the dialog", () => {
-      it("should show the updated facet and enable the Save button", () => {
-        openDialog(false);
-        updateFacet(false);
-        const items = getListItems();
-        expect(items).toHaveLength(2);
-        expect(items.at(0).text()).toContain("updated facet");
-        expect(getSaveButton().prop("disabled")).toBeFalsy();
-      });
-    });
-
-    describe("when a new facet is added through the dialog", () => {
-      it("should add the facet to the list and enable the Save button", () => {
-        openDialog(true);
-        updateFacet(true);
-        const items = getListItems();
-        expect(items).toHaveLength(3);
-        expect(items.at(2).text()).toContain("new facet");
-        expect(getSaveButton().prop("disabled")).toBeFalsy();
-      });
-    });
-
-    describe("when the Save button is enabled and clicked", () => {
-      const makeChanges = () => {
-        deleteFacet();
-        openDialog(true);
-        updateFacet(true);
-      };
-      const save = async (errorsReturned: boolean) => {
-        const deletePromise = mockBatchDelete.mockResolvedValueOnce(
-          errorsReturned ? ["Failed to delete"] : []
-        );
-        const updatePromise = mockBatchUpdateOrAdd.mockResolvedValueOnce(
-          errorsReturned ? ["Failed to update"] : []
-        );
-        makeChanges();
-        getSaveButton().simulate("click");
-        await act(async () => {
-          await updatePromise;
-          await deletePromise;
-        });
-      };
-
-      it("should display snackbar if no error messages are returned", async () => {
-        await save(false);
-        component.update();
-        const snackbar = component.find(MessageInfo);
-        expect(snackbar.prop("open")).toBeTruthy();
-      });
-
-      it("should display message dialog if error messages are returned", async () => {
-        await save(true);
-        component.update();
-        const messageDialog = component.find(MessageDialog);
-        expect(messageDialog.prop("open")).toBeTruthy();
-      });
+    it("should display message dialog if error messages are returned", async () => {
+      await save(true);
+      component.update();
+      const messageDialog = component.find(MessageDialog);
+      expect(messageDialog.props().open).toBeTruthy();
     });
   });
 });
