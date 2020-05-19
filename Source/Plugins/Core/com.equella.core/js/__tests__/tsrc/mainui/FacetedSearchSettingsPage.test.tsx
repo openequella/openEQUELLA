b1101bf8b8ce9f0ef6316408bdb0cd471e26808f
Penghai <penghai.zhang@edalex.com>
2020-05-19 13:14:38 +1000

    In the Jest test of FacetedSearchSettings, add tests for functions 'reorder' and 'removeFacetFromList'.

--- Source/Plugins/Core/com.equella.core/js/__tests__/tsrc/mainui/FacetedSearchSettingsPage.test.tsx
+++ Source/Plugins/Core/com.equella.core/js/__tests__/tsrc/mainui/FacetedSearchSettingsPage.test.tsx
@@ -25,6 +25,7 @@ import FacetDialog from "../../../tsrc/settings/Search/facetedsearch/FacetDialog
 import MessageInfo from "../../../tsrc/components/MessageInfo";
 import MessageDialog from "../../../tsrc/components/MessageDialog";
 import { Draggable } from "react-beautiful-dnd";
+import { FacetWithFlags } from "../../../tsrc/settings/Search/facetedsearch/FacetedSearchSettingsModule";
 
 const mockFacets: FacetedSearchSettingsModule.Facet[] = [
   {
@@ -32,16 +33,27 @@ const mockFacets: FacetedSearchSettingsModule.Facet[] = [
     name: "mocked facet1",
     schemaNode: "item/name",
     maxResults: 1,
-    orderIndex: 1,
+    orderIndex: 0,
   },
   {
     id: 2,
-    name: "mocked facet3",
+    name: "mocked facet2",
     schemaNode: "item/age",
     maxResults: 2,
+    orderIndex: 1,
+  },
+  {
+    id: 3,
+    name: "mocked facet3",
+    schemaNode: "item/age",
+    maxResults: 3,
     orderIndex: 2,
   },
 ];
+
+const mockFacetsWithFlags: FacetWithFlags[] = mockFacets.map((f) => {
+  return { ...f, updated: false, deleted: false };
+});
 
 // This test does not need NavigationGuard which is part of the Setting page template.
 jest.mock("../../../tsrc/components/NavigationGuard", () => ({
@@ -117,7 +129,7 @@ describe("<FacetedSearchSettingsPage />", () => {
   });
 
   it("should display a list of facets", () => {
-    expect(getListItems()).toHaveLength(2);
+    expect(getListItems()).toHaveLength(3);
   });
 
   it("should show a dialog when an Edit button is clicked", async () => {
@@ -133,7 +145,7 @@ describe("<FacetedSearchSettingsPage />", () => {
   it("should remove a facet from the list and enable the Save button when a delete button is clicked", () => {
     deleteFacet();
     const items = getListItems();
-    expect(items).toHaveLength(1);
+    expect(items).toHaveLength(2);
     expect(getSaveButton().props().disabled).toBeFalsy();
   });
 
@@ -141,7 +153,7 @@ describe("<FacetedSearchSettingsPage />", () => {
     openDialog(Actions.Edit);
     updateFacet(Actions.Edit);
     const items = getListItems();
-    expect(items).toHaveLength(2);
+    expect(items).toHaveLength(3);
     expect(items.at(0).text()).toContain("updated facet");
     expect(getSaveButton().props().disabled).toBeFalsy();
   });
@@ -150,8 +162,8 @@ describe("<FacetedSearchSettingsPage />", () => {
     openDialog(Actions.Add);
     updateFacet(Actions.Add);
     const items = getListItems();
-    expect(items).toHaveLength(3);
-    expect(items.at(2).text()).toContain("new facet");
+    expect(items).toHaveLength(4);
+    expect(items.at(3).text()).toContain("new facet");
     expect(getSaveButton().props().disabled).toBeFalsy();
   });
 
@@ -188,6 +200,77 @@ describe("<FacetedSearchSettingsPage />", () => {
       component.update();
       const messageDialog = component.find(MessageDialog);
       expect(messageDialog.props().open).toBeTruthy();
+    });
+  });
+
+  it("should update order indexes when reorder facets", () => {
+    // Given the mocked facets, reorder the second one and third one.
+    const reorderFacets = FacetedSearchSettingsModule.reorder(
+      mockFacetsWithFlags,
+      1,
+      2
+    );
+    // The second facet's order index should be changed from 1 to 2.
+    expect(
+      reorderFacets.filter((f) => f.id === 2 && f.updated && f.orderIndex === 2)
+    ).toHaveLength(1);
+    // The third facet's order index should be changed from 2 to 1.
+    expect(
+      reorderFacets.filter((f) => f.id === 3 && f.updated && f.orderIndex === 1)
+    ).toHaveLength(1);
+    // The first facet's order index keeps unchanged.
+    expect(
+      reorderFacets.filter((f) => f.id === 1 && f.orderIndex === 0)
+    ).toHaveLength(1);
+  });
+
+  describe("when remove a facet", () => {
+    // Add a new mock facet which does not have no ID.
+    const newFacet: FacetWithFlags = {
+      name: "mocked facet4",
+      schemaNode: "item/age",
+      maxResults: 4,
+      orderIndex: 3,
+      updated: false,
+      deleted: false,
+    };
+    mockFacetsWithFlags.push(newFacet);
+    it("should keep this facet in state if this facet has an ID", () => {
+      const facetToRemove = mockFacetsWithFlags[1];
+      // Remove the second facet which has an ID.
+      const updatedFacets = FacetedSearchSettingsModule.removeFacetFromList(
+        mockFacetsWithFlags,
+        facetToRemove
+      );
+      // The total number of facets stored in state should keep 4.
+      expect(updatedFacets).toHaveLength(4);
+      // The first facet' order index keeps 0.
+      expect(
+        updatedFacets.filter(
+          (f) => f.id === 1 && !f.updated && f.orderIndex === 0
+        )
+      ).toHaveLength(1);
+      // The third facet's order index is 1 now.
+      expect(
+        updatedFacets.filter(
+          (f) => f.id === 3 && f.updated && f.orderIndex === 1
+        )
+      ).toHaveLength(1);
+      // The fourth facet's order index is updated to 2 although it does not have an ID.
+      expect(
+        updatedFacets.filter((f) => !f.id && f.updated && f.orderIndex === 2)
+      ).toHaveLength(1);
+    });
+    it("should remove this facet from state if this facet does not have an ID", () => {
+      // Remove the last one.
+      const updatedFacets = FacetedSearchSettingsModule.removeFacetFromList(
+        mockFacetsWithFlags,
+        newFacet
+      );
+      // The total number of facets stored in state should change to 3.
+      expect(updatedFacets).toHaveLength(3);
+      // None of the first three facets should be updated.
+      expect(updatedFacets.filter((f) => f.updated)).toHaveLength(0);
     });
   });
 });
