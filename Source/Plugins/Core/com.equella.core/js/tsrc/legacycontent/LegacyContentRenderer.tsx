ebf64bcc65cca6ada14f8a6ade0bb08ec8f0b057
Penghai <penghai.zhang@edalex.com>
2020-06-24 15:15:53 +1000

    Centralise CSS padding of New UI main content area in 'template.tsx'

--- Source/Plugins/Core/com.equella.core/js/tsrc/legacycontent/LegacyContentRenderer.tsx
+++ Source/Plugins/Core/com.equella.core/js/tsrc/legacycontent/LegacyContentRenderer.tsx
@@ -21,8 +21,8 @@ import { makeStyles } from "@material-ui/core";
 import { PageContent } from "./LegacyContent";
 
 const useStyles = makeStyles((t) => ({
-  withPadding: {
-    padding: t.spacing(2),
+  noPadding: {
+    padding: 0,
   },
 }));
 
@@ -36,20 +36,8 @@ export function LegacyContentRenderer({
   const classes = useStyles();
 
   const { body, crumbs, upperbody } = html;
-  const extraClass = (function () {
-    switch (fullscreenMode) {
-      case "YES":
-      case "YES_WITH_TOOLBAR":
-        return "";
-      default:
-        switch (menuMode) {
-          case "HIDDEN":
-            return "";
-          default:
-            return classes.withPadding;
-        }
-    }
-  })();
+  const extraClass =
+    !fullscreenMode && menuMode !== "HIDDEN" ? classes.noPadding : "";
   const mainContent = (
     <div className={`content ${extraClass}`}>
       {crumbs && <JQueryDiv id="breadcrumbs" html={crumbs} />}
