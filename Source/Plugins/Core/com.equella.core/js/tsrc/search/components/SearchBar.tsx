0c6a359b3164df40bb1d4c504cf09e3d65d62b53
Samantha Fisher <samantha.fisher@edalex.com>
2020-06-30 12:36:45 +1000

    Add debounced searchbar component

---
+++ Source/Plugins/Core/com.equella.core/js/tsrc/search/components/SearchBar.tsx
@@ -0,0 +1,76 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0, (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { IconButton, InputAdornment, TextField } from "@material-ui/core";
+import * as React from "react";
+import { useCallback } from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import { Close } from "@material-ui/icons";
+import { debounce } from "lodash";
+import { languageStrings } from "../../util/langstrings";
+
+interface SearchBarProps {
+  /**
+   * Callback fired when the user stops typing (debounced for 500 milliseconds).
+   * @param query The string to search.
+   */
+  onChange: (query: string) => void;
+}
+
+/**
+ * Debounced searchbar component to be used in the Search Page.
+ * It also includes an adornment which allows clearing the search field in a single click.
+ * This component does not handle the API query itself,
+ * that should be done in the parent component with the onChange callback.
+ */
+export default function SearchBar({ onChange }: SearchBarProps) {
+  const [query, setQuery] = React.useState<string>("");
+  const searchStrings = languageStrings.searchpage;
+  /**
+   * uses lodash to debounce the search query by half a second
+   */
+  const delayedQuery = useCallback(
+    debounce((query: string) => onChange(query), 500),
+    []
+  );
+
+  React.useEffect(() => {
+    delayedQuery(query);
+  }, [query]);
+
+  return (
+    <TextField
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon fontSize={"small"} />
+          </InputAdornment>
+        ),
+        endAdornment: query.length > 0 && (
+          <IconButton onClick={() => setQuery("")} size={"small"}>
+            <Close />
+          </IconButton>
+        ),
+      }}
+      fullWidth
+      onChange={(event) => setQuery(event.target.value)}
+      variant={"outlined"}
+      value={query}
+      label={searchStrings.title}
+    />
+  );
+}
