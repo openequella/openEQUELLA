57ec6d5cdf551550d6a20dcc8b2770c5f2ca43b4
Christian Murphy <christian.murphy.42@gmail.com>
2020-07-01 14:29:14 -0700

    refactor: typescript import to match typings

--- Source/Plugins/Core/com.equella.core/js/src/Utils/UUID.js
+++ Source/Plugins/Core/com.equella.core/js/src/Utils/UUID.js
@@ -15,4 +15,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-exports.newUUID = require("uuid/v4");
+exports.newUUID = require("uuid").v4;
