c6767c774714dbe5eba87abf81727f74e8b769ed
Penghai <penghai.zhang@edalex.com>
2020-07-08 09:39:39 +1000

    Remove tsrc component Loader.

--- Source/Plugins/Core/com.equella.core/js/tsrc/components/index.ts
+++ Source/Plugins/Core/com.equella.core/js/tsrc/components/index.ts
@@ -20,7 +20,6 @@ import { generateFromError } from "../api/errors";
 import { TemplateUpdateProps, templateError } from "../mainui/Template";
 
 export { default as Error } from "./Error";
-export { default as Loader } from "./Loader";
 export { default as AppBarQuery } from "./AppBarQuery";
 export { default as SearchResult } from "./SearchResult";
 
