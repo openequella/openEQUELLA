41b52849acbbf272c7c5ce829abbd1e6ef6b68ba
Samantha Fisher <samantha.fisher@edalex.com>
2020-07-07 13:30:41 +1000

    Add helpertext for the schema selector

--- Source/Plugins/Core/com.equella.core/js/tsrc/util/langstrings.ts
+++ Source/Plugins/Core/com.equella.core/js/tsrc/util/langstrings.ts
@@ -466,6 +466,8 @@ export const languageStrings = {
         schemaSelector: {
           schema: "Schema",
           selectASchema: "Select a schema...",
+          permissionsHelperText:
+            "The LIST_SCHEMA permission is required to select a schema",
           nodeSelector: {
             expandAll: "Expand All",
             collapseAll: "Collapse All",
