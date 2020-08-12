4df5743f77ba3e63ca1858924393ebae21b135f7
Christian Murphy <christian.murphy.42@gmail.com>
2020-08-12 14:36:31 -0700

    chore: manual migration steps for storybook version 6
    see https://medium.com/storybookjs/storybook-6-migration-guide-200346241bb5 for more information

--- Source/Plugins/Core/com.equella.core/js/.storybook/preview.ts
+++ Source/Plugins/Core/com.equella.core/js/.storybook/preview.ts
@@ -1,6 +1,4 @@
 import { addDecorator } from "@storybook/react";
-import { withA11y } from "@storybook/addon-a11y";
 import StoryRouter from "storybook-react-router";
 
-addDecorator(withA11y);
 addDecorator(StoryRouter());
