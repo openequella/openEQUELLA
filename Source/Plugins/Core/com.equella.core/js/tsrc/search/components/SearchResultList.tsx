bde927131e7c4f71d0355d709577d30ba8bb2a09
Penghai <penghai.zhang@edalex.com>
2020-09-14 16:12:36 +1000

    Give Search result list an ID so that the autotest can access the list conveniently.

--- Source/Plugins/Core/com.equella.core/js/tsrc/search/components/SearchResultList.tsx
+++ Source/Plugins/Core/com.equella.core/js/tsrc/search/components/SearchResultList.tsx
@@ -100,7 +100,10 @@ export const SearchResultList = ({
    * A list that consists of search result items. Lower the list's opacity when spinner displays.
    */
   const searchResultList = (
-    <List className={showSpinner ? classes.transparentList : ""}>
+    <List
+      className={showSpinner ? classes.transparentList : ""}
+      id="search_result_list"
+    >
       {searchResults.length === 0 && !showSpinner ? (
         <ListItem key={searchPageStrings.noResultsFound} divider>
           <Typography>{searchPageStrings.noResultsFound}</Typography>
