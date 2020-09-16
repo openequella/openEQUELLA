2f72472bbb65630dc8bcf10660c915c50bcec794
Penghai <penghai.zhang@edalex.com>
2020-09-16 10:32:28 +1000

    Add more test cases for new Search page.

--- autotest/OldTests/src/test/java/com/tle/webtests/test/searching/NewSearchPageTest.java
+++ autotest/OldTests/src/test/java/com/tle/webtests/test/searching/NewSearchPageTest.java
@@ -7,26 +7,62 @@ import com.tle.webtests.pageobject.searching.NewSearchPage;
 import com.tle.webtests.pageobject.viewitem.SummaryPage;
 import com.tle.webtests.test.AbstractCleanupAutoTest;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 @TestInstitution("fiveo")
 public class NewSearchPageTest extends AbstractCleanupAutoTest {
   private NewSearchPage searchPage;
 
-  @BeforeMethod(description = "open the new Search page and wait for initial search completed")
-  public void openSearchPage() {
+  @Test(description = "open the new Search page and wait for initial search completed")
+  public void initialSearch() {
     searchPage = new NewSearchPage(context).load();
     // The initial search should return 10 items.
-    searchPage.getWaiter().until((driver) -> searchPage.getSearchResultItems().size() == 10);
+    searchPage.waitForSearchCompleted(45);
   }
 
-  @Test(description = "open an item's summary page")
+  @Test(dependsOnMethods = "initialSearch", description = "Search with a query and refine controls")
+  public void searchByFilters() {
+    searchPage.newSearch();
+    // Search by Collections.
+    searchPage.selectCollection("Basic Items", "DRM Attachment Only");
+    searchPage.waitForSearchCompleted(21);
+    // Search by date ranges.
+    searchPage.selectDateRangeQuickOption("Today");
+    searchPage.waitForSearchCompleted(0);
+    searchPage.selectDateRangeQuickOption("All");
+    searchPage.waitForSearchCompleted(21);
+    searchPage.selectCustomDateRange("2011-03-24", "2020-09-16");
+    searchPage.waitForSearchCompleted(14);
+    // Search by a query.
+    searchPage.changeQuery("item");
+    searchPage.waitForSearchCompleted(8);
+    // Search by Item status.
+    searchPage.selectStatus(true);
+    searchPage.waitForSearchCompleted(9);
+    // Exclude attachments in a search.
+    searchPage.selectSearchAttachments(false);
+    searchPage.waitForSearchCompleted(8);
+    // Search by an owner.
+    searchPage.selectOwner("DoNotUse");
+    searchPage.waitForSearchCompleted(1);
+  }
+
+  @Test(description = "open an item's summary page", dependsOnMethods = "searchByFilters")
   public void openItemSummaryPage() {
-    final String ITEM_TITLE = "ExclamationTest ! Crazy! Horse!";
+    final String ITEM_TITLE = "SearchFilters - Basic Item";
     WebElement titleLink = searchPage.getItemTitleLink(ITEM_TITLE);
     titleLink.click();
     SummaryPage summary = new SummaryPage(context).get();
     assertEquals(summary.getItemTitle(), ITEM_TITLE);
+  }
+
+  @Test(
+      description = "Go back to the Search page from another page",
+      dependsOnMethods = "openItemSummaryPage")
+  public void backToSearchPage() {
+    context.getDriver().navigate().back();
+    WebElement searchBar = searchPage.getSearchBar();
+    assertEquals(searchBar.getAttribute("value"), "item");
+    searchPage.waitForSearchCompleted(1);
   }
 }
