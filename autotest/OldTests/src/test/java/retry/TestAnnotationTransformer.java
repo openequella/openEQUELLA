9bf8e8cbcda36e7263f4470da0ce5bbc59470aef
Penghai <penghai.zhang@edalex.com>
2020-10-09 13:31:01 +1100

    Create a new annotation used to specify whether a test should be skipped in Old UI mode.

--- autotest/OldTests/src/test/java/retry/TestAnnotationTransformer.java
+++ autotest/OldTests/src/test/java/retry/TestAnnotationTransformer.java
@@ -1,5 +1,6 @@
 package retry;
 
+import com.tle.webtests.framework.TestConfig;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import org.testng.IAnnotationTransformer;
@@ -14,5 +15,10 @@ public class TestAnnotationTransformer implements IAnnotationTransformer {
     RetryTest maxRetryCount = testMethod.getAnnotation(RetryTest.class);
     if (maxRetryCount == null || maxRetryCount.value() < 1) return;
     annotation.setRetryAnalyzer(FailureRetryAnalyzer.class);
+
+    TestConfig testConfig = new TestConfig(testClass, false);
+    if (testClass.isAnnotationPresent(SkipOldUITest.class) && !testConfig.isNewUI()) {
+      annotation.setEnabled(false);
+    }
   }
 }
