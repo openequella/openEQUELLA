23246e5fcde68bb96270bde6c1b079d873bf597f
Penghai <penghai.zhang@edalex.com>
2020-10-13 15:19:48 +1100

    Run the script 'check-index' from folder '.github'.

--- .github/check-index.sh
+++ .github/check-index.sh
@@ -7,7 +7,7 @@ function checkIndexFolderSize {
 TIMEOUT=10 # Represent 10 minutes
 TIMER=0 # A timer used to check if timeout is reached
 
-LAST_FOLDER_SIZE=`checkIndexFolderSize` 
+LAST_FOLDER_SIZE=`checkIndexFolderSize`
 NEW_FOLDER_SIZE=
 
 while [[ $NEW_FOLDER_SIZE != $LAST_FOLDER_SIZE ]]
@@ -21,7 +21,7 @@ do
    exit 1
  fi
 
- NEW_FOLDER_SIZE=`checkIndexFolderSize` 
+ NEW_FOLDER_SIZE=`checkIndexFolderSize`
  echo "freetext folder size: $NEW_FOLDER_SIZE"
 done
 
