9081024bc7c6c6c8d689fbc73285828db101fc83
mrblippy <nickcharles@gmail.com>
2020-10-27 13:50:42 +1100

    Modify enzyme adapter import, so enzyme based tests run correctly with the esModuleInterop tsconfig flag set to true

--- Source/Plugins/Core/com.equella.core/js/jest.setup.ts
+++ Source/Plugins/Core/com.equella.core/js/jest.setup.ts
@@ -15,12 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// import { configure } from "enzyme";
-// import * as Adapter from "enzyme-adapter-react-16";
-// configure({ adapter: new Adapter() });
-
 import { configure } from "enzyme";
-import * as ReactSixteenAdapter from "enzyme-adapter-react-16";
+import Adapter = require("enzyme-adapter-react-16");
 
-const adapter = ReactSixteenAdapter as any;
-configure({ adapter: new adapter.default() });
+configure({ adapter: new Adapter() });
