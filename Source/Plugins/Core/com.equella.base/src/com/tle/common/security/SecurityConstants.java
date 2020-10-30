ac9aa44d429de0c500d409919fabaa398217e93b
Samantha Fisher <samantha.fisher@edalex.com>
2020-10-30 12:10:02 +1100

    Add LIST_USERS ACL to check by default on SelectUserDialog

--- Source/Plugins/Core/com.equella.base/src/com/tle/common/security/SecurityConstants.java
+++ Source/Plugins/Core/com.equella.base/src/com/tle/common/security/SecurityConstants.java
@@ -46,6 +46,7 @@ public final class SecurityConstants {
   public static final String DISCOVER_ITEM = "DISCOVER_ITEM";
   public static final String ARCHIVE_ITEM = "ARCHIVE_ITEM";
   public static final String VIEW_VIEWCOUNT = "VIEW_VIEWCOUNT";
+  public static final String LIST_USERS = "LIST_USERS";
 
   public static final String EDIT_TAXONOMY = "EDIT_TAXONOMY";
 
