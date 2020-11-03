255fe28982e73a4d8b3dd9f86630261c99fc65d9
Ian Stevenson <ian.stevenson@edalex.com>
2020-11-03 14:51:31 +1100

    security(server): Re-use Tomcat basedir on service restarts
    If the Tomcat service is restart, historically oEQ used
    the same basedir. To ensure compatibility with tests, we
    do the same here.
    
    This is part of the fix for GHSA-79w4-xjfh-9rmf.
    
    (This was a manual backport, and included minor fixes for the logging
    compared to the other stable branches for 2018.x, 2019.x, 2020.x.)

--- Source/Plugins/Core/com.equella.core/src/com/tle/tomcat/service/impl/TomcatServiceImpl.java
+++ Source/Plugins/Core/com.equella.core/src/com/tle/tomcat/service/impl/TomcatServiceImpl.java
@@ -73,6 +73,9 @@ public class TomcatServiceImpl implements TomcatService, StartupBean, TomcatRest
 
 	private static final Log LOGGER = LogFactory.getLog(TomcatServiceImpl.class);
 
+        private String tomcatContextPath;
+        private String tomcatBasePath;
+
 	@Inject(optional = true)
 	@Named("userService.useXForwardedFor")
 	private boolean useXForwardedFor;
@@ -126,18 +129,16 @@ public class TomcatServiceImpl implements TomcatService, StartupBean, TomcatRest
 		}
 		try
 		{
-                        final String[] paths = setupDirectories();
-                        final String basePath = paths[0];
-                        final String contextPath = paths[1];
+                        setupDirectories();
 
 			tomcat = new Tomcat();
-			tomcat.setBaseDir(basePath);
+			tomcat.setBaseDir(tomcatBasePath);
 
 			Context context = new StandardContext();
 			context.addLifecycleListener(new AprLifecycleListener());
 			context.setName("/");
 			context.setPath("");
-			context.setDocBase(contextPath);
+			context.setDocBase(tomcatContextPath);
 			context.setUseHttpOnly(false);
 			if (useXForwardedFor) {
 				RemoteIpValve protoValve = new RemoteIpValve();
@@ -362,26 +363,27 @@ public class TomcatServiceImpl implements TomcatService, StartupBean, TomcatRest
 
         /**
          * Setup the directory for Tomcat and the oEQ webapp context.
-         *
-         * @return a tuple with the first element being the Tomcat base path, the second being the context
-         *         path
          */
-        private String[] setupDirectories() throws IOException {
+        private void setupDirectories() throws IOException {
+                if (tomcatContextPath != null && tomcatBasePath != null) {
+                        return;
+                }
+
                 // Setup a temporary directory to sandbox this instance of Tomcat and the context for
                 // the oEQ web app.
                 final File tempDir = Files.createTempDirectory("oeq-tomcat-basedir-").toFile();
                 // Ideally here we'd be able to use File.deleteOnExit() or a Runtime.addShutdownHook()
                 // to clean-up this file on termination. However we'd have to synchronise with the
                 // tomcat daemon etc. That could be a future optimisation if needed.
-                final String basePath = tempDir.getAbsolutePath();
-                LOGGER.info("Using base directory: " + basePath);
                 final File contextDir = new File(tempDir, "context/");
-                final String contextPath = contextDir.getAbsolutePath();
                 if (!contextDir.mkdir()) {
-                        throw new RuntimeException("Failed to setup context directory: " + contextPath);
+                        throw new RuntimeException("Failed to setup context directory: " + contextDir.getAbsolutePath());
                 }
-                LOGGER.info("Using context directory: " + contextPath);
 
-                return new String[] {basePath, contextPath};
+                tomcatBasePath = tempDir.getAbsolutePath();
+                tomcatContextPath = contextDir.getAbsolutePath();
+
+                LOGGER.info("Using base directory: " + tomcatBasePath);
+                LOGGER.info("Using context directory: " + tomcatContextPath);
         }
 }
