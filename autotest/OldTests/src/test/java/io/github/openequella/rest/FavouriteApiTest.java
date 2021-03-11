391a3394be45d03ffc982c50b4e71032538c881b
Penghai <penghai.zhang@edalex.com>
2021-03-11 14:51:36 +1100

    Functional test for adding favourite search.

--- autotest/OldTests/src/test/java/io/github/openequella/rest/FavouriteApiTest.java
+++ autotest/OldTests/src/test/java/io/github/openequella/rest/FavouriteApiTest.java
@@ -16,14 +16,16 @@ import org.codehaus.jackson.node.ObjectNode;
 import org.testng.annotations.Test;
 
 public class FavouriteApiTest extends AbstractRestApiTest {
-  private final String FAVOURITE_API_ENDPOINT =
-      getTestConfig().getInstitutionUrl() + "api/favourite";
+  private final String FAVOURITE_ITEM_API_ENDPOINT =
+      getTestConfig().getInstitutionUrl() + "api/favourite/item";
+  private final String FAVOURITE_SEARCH_API_ENDPOINT =
+      getTestConfig().getInstitutionUrl() + "api/favourite/search";
   private final String ITEM_KEY = "8a9ea41c-e28d-45de-b0a5-d75bca48d701/1";
   private long bookmarkId = 0L;
 
   @Test
-  public void testAddFavourite() throws IOException {
-    final PostMethod method = new PostMethod(FAVOURITE_API_ENDPOINT);
+  public void testAddFavouriteItem() throws IOException {
+    final PostMethod method = new PostMethod(FAVOURITE_ITEM_API_ENDPOINT);
     final String[] tags = {"a", "b"};
     ObjectNode body = mapper.createObjectNode();
     body.put("itemID", ITEM_KEY);
@@ -41,11 +43,32 @@ public class FavouriteApiTest extends AbstractRestApiTest {
     bookmarkId = response.get("bookmarkID").asLong();
   }
 
-  @Test(dependsOnMethods = "testAddFavourite")
-  public void testRemoveFavourite() throws IOException {
-    final HttpMethod method = new DeleteMethod(FAVOURITE_API_ENDPOINT + "/" + bookmarkId);
+  @Test(dependsOnMethods = "testAddFavouriteItem")
+  public void testRemoveFavouriteItem() throws IOException {
+    final HttpMethod method = new DeleteMethod(FAVOURITE_ITEM_API_ENDPOINT + "/" + bookmarkId);
     assertEquals(HttpStatus.SC_NO_CONTENT, makeClientRequest(method));
     // Try to delete again and the response code should be 404 as the bookmark is already deleted.
     assertEquals(HttpStatus.SC_NOT_FOUND, makeClientRequest(method));
+  }
+
+  @Test
+  public void testAddFavouriteSearch() throws IOException {
+    String searchName = "testFavouriteSearch";
+    String searchPath =
+        "/page/search?searchOptions=%7B%22rowsPerPage%22%3A10%2C%22currentPage%22%3A0%2C%22sortOrder%22%3A%22RANK%22%2C%22rawMode%22%3Afalse%2C%22status%22%3A%5B%22LIVE%22%2C%22REVIEW%22%5D%2C%22searchAttachments%22%3Atrue%2C%22query%22%3A%22%22%2C%22collections%22%3A%5B%5D%2C%22lastModifiedDateRange%22%3A%7B%7D%2C%22mimeTypeFilters%22%3A%5B%5D%2C%22dateRangeQuickModeEnabled%22%3Atrue%7D";
+    final PostMethod method = new PostMethod(FAVOURITE_SEARCH_API_ENDPOINT);
+    ObjectNode body = mapper.createObjectNode();
+    body.put("name", searchName);
+    body.put("url", searchPath);
+    method.setRequestEntity(new StringRequestEntity(body.toString(), "application/json", "UTF-8"));
+
+    assertEquals(HttpStatus.SC_CREATED, makeClientRequest(method));
+    JsonNode response = mapper.readTree(method.getResponseBody());
+    assertNotNull(response.get("id"));
+    assertNotNull(response.get("owner"));
+    assertNotNull(response.get("dateModified"));
+    assertNotNull(response.get("institution"));
+    assertEquals(searchName, response.get("name").asText());
+    assertEquals(searchPath, response.get("url").asText());
   }
 }
