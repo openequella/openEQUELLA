b04194d63acc66caf46ffe52e25c681161a266a2
Ian Stevenson <ian.stevenson@edalex.com>
2021-03-15 16:56:09 +1100

    test(front-end): Add test for GallerySearchResult

--- Source/Plugins/Core/com.equella.core/js/__stories__/search/GallerySearchResult.stories.tsx
+++ Source/Plugins/Core/com.equella.core/js/__stories__/search/GallerySearchResult.stories.tsx
@@ -16,13 +16,8 @@
  * limitations under the License.
  */
 import { Meta, Story } from "@storybook/react";
-import { range } from "lodash";
 import * as React from "react";
-import { v4 as uuidV4 } from "uuid";
-import {
-  GalleryEntry,
-  GallerySearchResultItem,
-} from "../../tsrc/modules/GallerySearchModule";
+import { buildItems } from "../../__tests__/tsrc/search/components/GallerySearchResultHelpers";
 import GallerySearchResult, {
   GallerySearchResultProps,
 } from "../../tsrc/search/components/GallerySearchResult";
@@ -31,31 +26,6 @@ export default {
   title: "Search/GallerySearchResult",
   component: GallerySearchResult,
 } as Meta<GallerySearchResultProps>;
-
-const buildGalleryEntry = (name: string): GalleryEntry => ({
-  mimeType: "image/png",
-  name,
-  imagePathSmall: "./placeholder-135x135.png",
-  imagePathMedium: "./placeholder-500x500.png",
-  imagePathFull: "./placeholder-500x500.png",
-});
-
-const buildItems = (howMany: number): GallerySearchResultItem[] =>
-  range(howMany).map(
-    (i: number): GallerySearchResultItem => ({
-      uuid: uuidV4(),
-      version: 1,
-      name: `Test item ${i}`,
-      links: {
-        self: `link-to-self-${i}`,
-        view: `link-to-view-${i}`,
-      },
-      mainEntry: buildGalleryEntry(`Main Entry #${i}`),
-      additionalEntries: range([0, 1, 2, 3, 5][i % 5]).map((_, idx) =>
-        buildGalleryEntry(`Additional Entry #${i}/${idx}`)
-      ),
-    })
-  );
 
 export const TenItems: Story<GallerySearchResultProps> = (args) => (
   <GallerySearchResult {...args} />
