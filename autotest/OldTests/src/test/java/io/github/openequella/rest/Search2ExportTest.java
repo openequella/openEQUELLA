72362b638f81f3eeecb5f5033aac0a94f41eda06
Penghai <penghai.zhang@edalex.com>
2021-04-12 10:00:32 +1000

    Use OpenCSV to validate the CSV content in the test.

--- autotest/OldTests/src/test/java/io/github/openequella/rest/Search2ExportTest.java
+++ autotest/OldTests/src/test/java/io/github/openequella/rest/Search2ExportTest.java
@@ -3,10 +3,14 @@ package io.github.openequella.rest;
 import static com.tle.webtests.test.AbstractSessionTest.AUTOTEST_LOW_PRIVILEGE_LOGON;
 import static org.testng.Assert.assertEquals;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import com.tle.webtests.framework.TestConfig;
 import com.tle.webtests.framework.TestInstitution;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.Arrays;
+import java.util.List;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.GetMethod;
@@ -30,7 +34,7 @@ public class Search2ExportTest extends AbstractRestApiTest {
     return testConfig;
   }
 
-  @Test(description = "Export without ACL")
+  @Test(description = "Export should fail when ACL is not granted")
   public void withoutACL() throws IOException {
     // Login as a low privilege user.
     makeClientRequest(buildLoginMethod(AUTOTEST_LOW_PRIVILEGE_LOGON, PASSWORD));
@@ -39,7 +43,7 @@ public class Search2ExportTest extends AbstractRestApiTest {
     assertEquals(statusCode, 403);
   }
 
-  @Test(description = "Export with unsupported file formats")
+  @Test(description = "Request to export in format other than CSV should fail")
   public void unsupportedFormat() throws IOException {
     HttpMethod method = buildExportRequest(null);
     method.setRequestHeader("accept", "text/xml");
@@ -47,7 +51,7 @@ public class Search2ExportTest extends AbstractRestApiTest {
     assertEquals(statusCode, 406);
   }
 
-  @Test(description = "Export with multiple Collections")
+  @Test(description = "Attempts to export with multiple Collections should fail")
   public void onlyOneCollection() throws IOException {
     NameValuePair[] queryStrings = Arrays.copyOf(defaultQueryStrings, 4);
     queryStrings[3] = new NameValuePair("collections", "b2be4e8e-a0d4-4e6a-b9ff-4c65a7c8024e");
@@ -57,11 +61,19 @@ public class Search2ExportTest extends AbstractRestApiTest {
   }
 
   @Test(description = "A standard export")
-  public void export() throws IOException {
+  public void export() throws IOException, CsvException {
     HttpMethod method = buildExportRequest(null);
     int statusCode = makeClientRequest(method);
     assertEquals(statusCode, 200);
     assertEquals(method.getResponseHeader("Content-Type").getValue(), "text/csv");
+
+    // This CSV should have 8 rows including one row for headers and 29 columns.
+    CSVReader reader = new CSVReader(new StringReader(method.getResponseBodyAsString()));
+    List<String[]> csv = reader.readAll();
+    assertEquals(csv.size(), 8);
+
+    String[] headers = csv.get(0);
+    assertEquals(headers.length, 29);
   }
 
   private HttpMethod buildExportRequest(NameValuePair[] queryStrings) {
