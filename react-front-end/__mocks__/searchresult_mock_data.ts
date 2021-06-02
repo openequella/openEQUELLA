e3475211e9107760d0a4caa68ab208802bc2bf6d
Samantha Fisher <samantha.fisher@edalex.com>
2021-06-02 18:21:20 +1000

    Update mocks to account for new flag

--- react-front-end/__mocks__/searchresult_mock_data.ts
+++ react-front-end/__mocks__/searchresult_mock_data.ts
@@ -68,6 +68,7 @@ const attachSearchObj: OEQ.Search.SearchResultItem = {
       preview: false,
       mimeType: "image/png",
       hasGeneratedThumb: true,
+      brokenAttachment: false,
       links: {
         view:
           "http://localhost:8080/rest/items/72558c1d-8788-4515-86c8-b24a28cc451e/1/?attachment.uuid=78b8af7e-f0f5-4b5c-9f44-16f212583fe8",
@@ -112,6 +113,7 @@ const keywordFoundInAttachmentObj: OEQ.Search.SearchResultItem = {
       preview: false,
       mimeType: "image/png",
       hasGeneratedThumb: true,
+      brokenAttachment: false,
       links: {
         view:
           "http://localhost:8080/rest/items/72558c1d-8788-4515-86c8-b24a28cc451e/1/?attachment.uuid=78b8af7e-f0f5-4b5c-9f44-16f212583fe8",
@@ -156,6 +158,7 @@ const customMetaSearchObj: OEQ.Search.SearchResultItem = {
       preview: false,
       mimeType: "image/png",
       hasGeneratedThumb: true,
+      brokenAttachment: false,
       links: {
         view:
           "http://localhost:8080/rest/items/72558c1d-8788-4515-86c8-b24a28cc451e/1/?attachment.uuid=78b8af7e-f0f5-4b5c-9f44-16f212583fe8",
@@ -197,9 +200,114 @@ const customMetaSearchObj: OEQ.Search.SearchResultItem = {
   },
   isLatestVersion: false,
 };
+const oneDeadAttachObj: OEQ.Search.SearchResultItem = {
+  uuid: "72558c1d-8788-4515-86c8-b24a28cc451e",
+  version: 1,
+  name: "Little Larry",
+  description: "A description of a bird",
+  status: "live",
+  createdDate: new Date("2020-05-26T13:24:00.889+10:00"),
+  modifiedDate: new Date("2020-05-26T12:45:06.857+10:00"),
+  collectionId: "b28f1ffe-2008-4f5e-d559-83c8acd79316",
+  commentCount: 2,
+  starRatings: 1.5,
+  attachments: [
+    {
+      attachmentType: "file",
+      id: "9e751549-5cba-47dd-bccb-722c48072287",
+      description: "broken.png",
+      preview: false,
+      mimeType: "image/png",
+      hasGeneratedThumb: true,
+      brokenAttachment: true,
+      links: {
+        view:
+          "http://localhost:8080/rest/items/72558c1d-8788-4515-86c8-b24a28cc451e/1/?attachment.uuid=78b8af7e-f0f5-4b5c-9f44-16f212583fe8",
+        thumbnail: "./thumb.jpg",
+      },
+    },
+  ],
+  thumbnail: "default",
+  displayFields: [],
+  displayOptions: {
+    attachmentType: "STRUCTURED",
+    disableThumbnail: false,
+    standardOpen: true,
+    integrationOpen: true,
+  },
+  keywordFoundInAttachment: false,
+  links: {
+    view:
+      "http://localhost:8080/rest/items/72558c1d-8788-4515-86c8-b24a28cc451e/1/",
+    self:
+      "http://localhost:8080/rest/api/item/72558c1d-8788-4515-86c8-b24a28cc451e/1/",
+  },
+  isLatestVersion: true,
+};
+
+const oneDeadOneAliveAttachObj: OEQ.Search.SearchResultItem = {
+  uuid: "72558c1d-8788-4515-86c8-b24a28cc451e",
+  version: 1,
+  name: "Little Larry",
+  description: "A description of a bird",
+  status: "live",
+  createdDate: new Date("2020-05-26T13:24:00.889+10:00"),
+  modifiedDate: new Date("2020-05-26T12:45:06.857+10:00"),
+  collectionId: "b28f1ffe-2008-4f5e-d559-83c8acd79316",
+  commentCount: 2,
+  starRatings: 1.5,
+  attachments: [
+    {
+      attachmentType: "file",
+      id: "9e751549-5cba-47dd-bccb-722c48072287",
+      description: "broken.png",
+      preview: false,
+      mimeType: "image/png",
+      hasGeneratedThumb: true,
+      brokenAttachment: true,
+      links: {
+        view:
+          "http://localhost:8080/rest/items/72558c1d-8788-4515-86c8-b24a28cc451e/1/?attachment.uuid=78b8af7e-f0f5-4b5c-9f44-16f212583fe8",
+        thumbnail: "./thumb.jpg",
+      },
+    },
+    {
+      attachmentType: "file",
+      id: "78883eff-7cf6-4b14-ab76-2b7f84dbe833",
+      description: "notbroken.png",
+      preview: false,
+      mimeType: "image/png",
+      hasGeneratedThumb: true,
+      brokenAttachment: false,
+      links: {
+        view:
+          "http://localhost:8080/rest/items/72558c1d-8788-4515-86c8-b24a28cc451e/1/?attachment.uuid=78b8af7e-f0f5-4b5c-9f44-16f212583fe8",
+        thumbnail: "./thumb.jpg",
+      },
+    },
+  ],
+  thumbnail: "default",
+  displayFields: [],
+  displayOptions: {
+    attachmentType: "STRUCTURED",
+    disableThumbnail: false,
+    standardOpen: true,
+    integrationOpen: true,
+  },
+  keywordFoundInAttachment: false,
+  links: {
+    view:
+      "http://localhost:8080/rest/items/72558c1d-8788-4515-86c8-b24a28cc451e/1/",
+    self:
+      "http://localhost:8080/rest/api/item/72558c1d-8788-4515-86c8-b24a28cc451e/1/",
+  },
+  isLatestVersion: true,
+};
 export {
   basicSearchObj,
   attachSearchObj,
   customMetaSearchObj,
   keywordFoundInAttachmentObj,
+  oneDeadAttachObj,
+  oneDeadOneAliveAttachObj,
 };
