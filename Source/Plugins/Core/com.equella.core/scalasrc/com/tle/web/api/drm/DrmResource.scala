7ed95d74744748aac9c908c5b940c4db6d67d7fa
Penghai <penghai.zhang@edalex.com>
2021-07-12 13:41:33 +1000

    feat: REST endpoint to support accepting DRM

---
+++ Source/Plugins/Core/com.equella.core/scalasrc/com/tle/web/api/drm/DrmResource.scala
@@ -0,0 +1,57 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0, (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tle.web.api.drm
+
+import com.dytech.edge.exceptions.{DRMException, ItemNotFoundException}
+import com.tle.beans.item.ItemId
+import com.tle.legacy.LegacyGuice
+import com.tle.web.api.ApiErrorResponse.{
+  badRequest,
+  forbiddenRequest,
+  resourceNotFound,
+  unauthorizedRequest
+}
+import io.swagger.annotations.{Api, ApiParam}
+import org.jboss.resteasy.annotations.cache.NoCache
+import javax.ws.rs.core.Response
+import javax.ws.rs.{BadRequestException, POST, Path, PathParam, Produces}
+import com.tle.exceptions.AccessDeniedException;
+
+@NoCache
+@Path("drm")
+@Produces(Array("application/json"))
+@Api("DRM")
+class DrmResource {
+  val drmService = LegacyGuice.drmService
+
+  @POST
+  @Path("/{uuid}/{version}")
+  def acceptDRM(@ApiParam("Item UUID") @PathParam("uuid") uuid: String,
+                @ApiParam("Item Version") @PathParam("version") version: Int): Response = {
+    try {
+      val item = LegacyGuice.itemService.getUnsecure(new ItemId(uuid, version))
+      Response.ok().entity(drmService.acceptLicenseOrThrow(item)).build()
+    } catch {
+      case e: BadRequestException   => badRequest(e.getMessage)
+      case e: AccessDeniedException => forbiddenRequest(e.getMessage)
+      case e: DRMException          => unauthorizedRequest(e.getMessage)
+      case e: ItemNotFoundException => resourceNotFound(e.getMessage)
+    }
+  }
+}
