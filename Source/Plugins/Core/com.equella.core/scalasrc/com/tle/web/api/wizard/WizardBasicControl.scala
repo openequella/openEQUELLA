0dcce6b60aa6234fd877e8b10ba3c18e10f9d34d
Penghai <penghai.zhang@edalex.com>
2021-08-26 11:18:17 +1000

    feat: data structure for Wizard controls

---
+++ Source/Plugins/Core/com.equella.core/scalasrc/com/tle/web/api/wizard/WizardBasicControl.scala
@@ -0,0 +1,151 @@
+package com.tle.web.api.wizard
+
+import com.dytech.edge.wizard.TargetNode
+import com.dytech.edge.wizard.beans.control.{WizardControl, WizardControlItem}
+import com.fasterxml.jackson.annotation.JsonUnwrapped
+import com.tle.beans.entity.LanguageBundle
+import com.tle.common.i18n.{CurrentLocale, LangUtils}
+import scala.collection.JavaConverters._
+
+/** Data structure for Wizard Control option
+  *
+  * @param text Text of the option. None in some components like Calendar.
+  * @param value  Value of the option.
+  */
+case class WizardControlOption(text: Option[String], value: String)
+
+object WizardControlOption {
+  def apply(item: WizardControlItem): WizardControlOption = {
+    WizardControlOption(
+      text = Option(item.getName).map(name => LangUtils.getString(name)),
+      value = item.getValue,
+    )
+  }
+}
+
+// This trait is used to support the union type of different Wizard control types.
+// We can probably remove it trait once we can use Scala 3.
+sealed trait WizardControlDefinition
+
+/**
+  * Data structure for key information of 'com.dytech.edge.wizard.beans.control.WizardControl'.
+  *
+  * @param mandatory Whether the control must have a value.
+  * @param reload Whether to reload all controls after this control has different selection. This may affect how scripting works.
+  * @param include #todo
+  * @param size1 #todo
+  * @param size2 #todo
+  * @param customName The controls' customised name.
+  * @param title Title of the control.
+  * @param description Description of the control.
+  * @param visibilityScript Script running on Client to control the visibility of the control.
+  * @param targetNodes Schema nodes that the control targets to.
+  * @param options Options available for selection.
+  * @param powerSearchFriendlyName #todo
+  * @param controlType Type of the control.
+  */
+case class WizardBasicControl(
+    mandatory: Boolean,
+    reload: Boolean,
+    include: Boolean,
+    size1: Int,
+    size2: Int,
+    customName: Option[String],
+    title: Option[String],
+    description: Option[String],
+    visibilityScript: Option[String],
+    targetNodes: List[TargetNode],
+    options: List[WizardControlOption],
+    powerSearchFriendlyName: Option[String],
+    controlType: String
+) extends WizardControlDefinition
+
+object WizardBasicControl {
+  def getLanguageString(bundle: LanguageBundle): String = {
+    LangUtils.getString(bundle, CurrentLocale.getLocale, null)
+  }
+
+  def apply(control: WizardControl): WizardBasicControl = {
+    WizardBasicControl(
+      mandatory = control.isMandatory,
+      reload = control.isReload,
+      include = control.isInclude,
+      size1 = control.getSize1,
+      size2 = control.getSize2,
+      customName = Option(control.getCustomName),
+      title = Option(getLanguageString(control.getTitle)),
+      description = Option(getLanguageString(control.getDescription)),
+      visibilityScript = Option(control.getScript),
+      targetNodes = control.getTargetnodes.asScala.toList,
+      options = control.getItems.asScala.map(i => WizardControlOption(i)).toList,
+      powerSearchFriendlyName = Option(getLanguageString(control.getPowerSearchFriendlyName)),
+      controlType = control.getClassType
+    )
+  }
+}
+
+/**
+  * Data structure for Calendar control.
+  *
+  * @param basicControl The basic control providing common fields.
+  * @param isRange Whether to support a date range.
+  */
+case class WizardCalendarControl(@JsonUnwrapped
+                                 basicControl: WizardBasicControl,
+                                 isRange: Boolean)
+    extends WizardControlDefinition
+
+/**
+  * Data structure for ShuffleList control.
+  *
+  * @param basicControl The basic control providing common fields.
+  * @param isTokenise Whether to tokenise the value.
+  * @param isForceUnique Whether each value must be unique.
+  * @param isCheckDuplication Whether to check duplicated values.
+  */
+case class WizardShuffleListControl(
+    @JsonUnwrapped
+    basicControl: WizardBasicControl,
+    isTokenise: Boolean,
+    isForceUnique: Boolean,
+    isCheckDuplication: Boolean
+) extends WizardControlDefinition
+
+/**
+  * Data structure for EditBox control.
+  *
+  * @param basicControl The basic control providing common fields.
+  * @param isAllowLinks Whether to allow links.
+  * @param isNumber Whether to use numbers only.
+  * @param isAllowMultiLang Whether to allow multiple lines.
+  * @param isForceUnique Whether each value must be unique.
+  * @param isCheckDuplication Whether to check duplicated values.
+  */
+case class WizardEditBoxControl(
+    @JsonUnwrapped
+    basicControl: WizardBasicControl,
+    isAllowLinks: Boolean,
+    isNumber: Boolean,
+    isAllowMultiLang: Boolean,
+    isForceUnique: Boolean,
+    isCheckDuplication: Boolean,
+) extends WizardControlDefinition
+
+/**
+  * Data structure for Custom Wizard control.
+  *
+  * @param basicControl The basic control providing common fields.
+  * @param attributes Custom attributes
+  */
+case class WizardCustomControl(
+    @JsonUnwrapped
+    basicControl: WizardBasicControl,
+    attributes: Map[Object, Object]
+) extends WizardControlDefinition
+
+/**
+  * Data structure for unknown Wizard control.
+  */
+case class UnknownWizardControl() extends WizardControlDefinition {
+  val controlType: String = "unknown"
+}
