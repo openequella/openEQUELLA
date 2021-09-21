cbf8b9d7828149d23e247e06b81dafba90aa2772
Ian Stevenson <ian.stevenson@edalex.com>
2021-09-21 14:14:41 +1000

    feat(front-end): initial implementation for wizard rendering (Editbox only)

---
+++ react-front-end/__mocks__/WizardHelper.mock.ts
@@ -0,0 +1,129 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0, (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import * as OEQ from "@openequella/rest-api-client";
+
+export const controls: OEQ.WizardControl.WizardControl[] = [
+  {
+    mandatory: false,
+    reload: true,
+    include: true,
+    size1: 2,
+    size2: 0,
+    title: "Do you want to show or hide?",
+    targetNodes: [
+      {
+        target: "/item/name",
+        attribute: "",
+        fullTarget: "/item/name",
+        xoqlPath: "/item/name",
+        freetextField: "/item/name",
+      },
+    ],
+    options: [
+      {
+        text: "Hide",
+        value: "hide",
+      },
+      {
+        text: "Show",
+        value: "",
+      },
+    ],
+    controlType: "radiogroup",
+  },
+  {
+    mandatory: true,
+    reload: false,
+    include: true,
+    size1: 0,
+    size2: 1,
+    customName: "Edit Box #1",
+    title: "Edit Box #1",
+    description: "The first edit box - target name",
+    visibilityScript:
+      "var bRet = false; \nif( xml.get('/item/name') != 'hide' ) \n{ \n    bRet = true; \n} \nreturn bRet; \n",
+    targetNodes: [
+      {
+        target: "/item/name",
+        attribute: "",
+        fullTarget: "/item/name",
+        xoqlPath: "/item/name",
+        freetextField: "/item/name",
+      },
+    ],
+    options: [],
+    controlType: "editbox",
+    isAllowLinks: false,
+    isNumber: false,
+    isAllowMultiLang: false,
+    isForceUnique: false,
+    isCheckDuplication: false,
+  },
+  {
+    mandatory: false,
+    reload: false,
+    include: true,
+    size1: 0,
+    size2: 3,
+    title: "Edit Box #2",
+    description:
+      "The second edit box, with three rows and targetting description",
+    visibilityScript:
+      "var bRet = false; \nif( xml.get('/item/name') != 'hide' && xml.get('/item/name') != 'hide2' ) \n{ \n    bRet = true; \n} \nreturn bRet; \n",
+    targetNodes: [
+      {
+        target: "/item/description",
+        attribute: "",
+        fullTarget: "/item/description",
+        xoqlPath: "/item/description",
+        freetextField: "/item/description",
+      },
+    ],
+    options: [],
+    controlType: "editbox",
+    isAllowLinks: false,
+    isNumber: false,
+    isAllowMultiLang: false,
+    isForceUnique: false,
+    isCheckDuplication: false,
+  },
+  {
+    mandatory: true,
+    reload: false,
+    include: true,
+    size1: 0,
+    size2: 1,
+    description: "This has no title - only a description",
+    targetNodes: [
+      {
+        target: "/item/description",
+        attribute: "",
+        fullTarget: "/item/description",
+        xoqlPath: "/item/description",
+        freetextField: "/item/description",
+      },
+    ],
+    options: [],
+    controlType: "editbox",
+    isAllowLinks: false,
+    isNumber: false,
+    isAllowMultiLang: false,
+    isForceUnique: false,
+    isCheckDuplication: false,
+  },
+];
---
+++ react-front-end/__tests__/tsrc/components/wizard/WizardHelper.test.ts
@@ -0,0 +1,114 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0, (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { controls } from "../../../../__mocks__/WizardHelper.mock";
+import {
+  ControlTarget,
+  FieldValue,
+  render,
+} from "../../../../tsrc/components/wizard/WizardHelper";
+
+describe("render()", () => {
+  const logOnChange = (update: FieldValue): void =>
+    console.debug("onChange called", update);
+
+  const nameEditboxTarget: ControlTarget = {
+    type: "editbox",
+    schemaNode: ["/item/name"],
+  };
+
+  it("creates JSX.Elements matching definition", () => {
+    // We currently only support editboxes, so just filter to them for now
+    // Each time we add a control, we need to expand this out
+    const supportedControls = controls.filter(
+      (c) => c.controlType === "editbox"
+    );
+    expect(supportedControls.length).toBeGreaterThan(1); // quick assertions we have good test data
+
+    const elements: JSX.Element[] = render(supportedControls, [], logOnChange);
+    expect(elements).toHaveLength(supportedControls.length);
+    // for now, we just expect Editboxes, we'll need to elaborate on this in the future
+    expect(elements.every((e) => e.type.name === "WizardEditBox")).toBeTruthy();
+  });
+
+  it("creates WizardUnsupported components for unknown/unsupported ones", () => {
+    const elements: JSX.Element[] = render(controls, [], logOnChange);
+    // Current the 'controls' include a radio group which we've not yet written support for
+    expect(
+      elements.filter((e) => e.type.name === "WizardUnsupported")
+    ).toHaveLength(1);
+  });
+
+  it("handles `controlType === 'unknown'` - i.e. `UnknownWizardControl`", () => {
+    const elements: JSX.Element[] = render(
+      [{ controlType: "unknown" }],
+      [],
+      logOnChange
+    );
+    expect(
+      elements.filter((e) => e.type.name === "WizardUnsupported")
+    ).toHaveLength(1);
+  });
+
+  it.each<[string, FieldValue, number]>([
+    [
+      "singular",
+      {
+        target: nameEditboxTarget,
+        value: ["an editbox - name"],
+      },
+      1,
+    ],
+    [
+      "multiple",
+      {
+        target: { type: "editbox", schemaNode: ["/item/description"] },
+        value: ["an editbox - description"],
+      },
+      2,
+    ],
+  ])(
+    "correctly sets the initial value when provided (%s)",
+    (_, value, fieldsSet) => {
+      // smoke test the test data
+      expect(value.value[0]).toBeTruthy();
+
+      const elements = render(controls, [value], logOnChange);
+      // Test the field(s) were set
+      expect(
+        elements.filter((e) => e.props.value === value.value[0])
+      ).toHaveLength(fieldsSet);
+    }
+  );
+
+  it("throws errors when there's a mismatch of ControlValues for ControlTargets", () => {
+    const commonTarget: ControlTarget = nameEditboxTarget;
+    const misMatchedValues: FieldValue[] = [
+      { target: commonTarget, value: ["first value"] },
+      { target: commonTarget, value: ["different value"] },
+    ];
+    expect(() => render(controls, misMatchedValues, logOnChange)).toThrow(
+      Error
+    );
+  });
+
+  it("throws an error if the incorrect control value type is provided", () => {
+    expect(() =>
+      render(controls, [{ target: nameEditboxTarget, value: [1] }], logOnChange)
+    ).toThrow(TypeError);
+  });
+});
