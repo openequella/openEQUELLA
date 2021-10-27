00d435c01f77b7407735df04d28b7be35d9eeaae
Penghai <penghai.zhang@edalex.com>
2021-10-27 14:48:03 +1100

    test: add new test case for WizardCalendar initial values

--- react-front-end/__tests__/tsrc/components/wizard/WizardCalendar.test.tsx
+++ react-front-end/__tests__/tsrc/components/wizard/WizardCalendar.test.tsx
@@ -25,22 +25,52 @@ import { ISODateFormat } from "../../../../tsrc/util/Date";
 
 describe("<WizardCalendar/>", () => {
   const today = DateTime.now();
-  it.each<[string, OEQ.WizardCommonTypes.WizardDateFormat, ISODateFormat]>([
-    ["year only", "Y", "yyyy"],
-    ["year and month", "MY", "yyyy-MM"],
-    ["year, month and day", "DMY", "yyyy-MM-dd"],
+  const onChange = jest.fn();
+
+  it("displays the date range initially provided", () => {
+    const providedDateRange = [
+      today.toISODate(),
+      today.plus({ days: 1 }).toISODate(),
+    ];
+    const { container } = render(
+      <WizardCalendar
+        isRange
+        mandatory
+        onChange={onChange}
+        values={providedDateRange}
+      />
+    );
+
+    const displayedRange = Array.from(
+      container.querySelectorAll<HTMLInputElement>("input")
+    ).map((input) => input.value);
+    expect(displayedRange).toEqual(providedDateRange);
+  });
+
+  it.each<
+    [string, OEQ.WizardCommonTypes.WizardDateFormat, ISODateFormat, string[]]
+  >([
+    ["year only", "Y", "yyyy", [today.startOf("year").toISODate(), ""]],
+    [
+      "year and month",
+      "MY",
+      "yyyy-MM",
+      [today.startOf("month").toISODate(), ""],
+    ],
+    ["year, month and day", "DMY", "yyyy-MM-dd", [today.toISODate(), ""]],
   ])(
     "supports date format: %s",
     (
       _: string,
       wizardControlDateformat: OEQ.WizardCommonTypes.WizardDateFormat,
-      displayFormat: ISODateFormat
+      displayFormat: ISODateFormat,
+      dateRange: string[]
     ) => {
       const { container } = render(
         <WizardCalendar
           isRange
           mandatory
-          onChange={jest.fn()}
+          onChange={onChange}
           dateFormat={wizardControlDateformat}
         />
       );
@@ -48,6 +78,9 @@ describe("<WizardCalendar/>", () => {
       const start = container.querySelectorAll<HTMLInputElement>("input")[0]; // The component must have at least one DatePicker.
       userEvent.type(start, today.toISODate());
       expect(start.value).toEqual(today.toFormat(displayFormat));
+
+      // We only set `start` so the
+      expect(onChange).toHaveBeenLastCalledWith(dateRange);
     }
   );
 });
