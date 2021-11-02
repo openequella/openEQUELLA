ee52160b58d1c14b9e6a5904125b2027d7b28b00
Ian Stevenson <ian.stevenson@edalex.com>
2021-11-02 12:48:07 +1100

    feat(front-end): add WizardShuffleList component

---
+++ react-front-end/__stories__/components/wizard/WizardShuffleList.stories.tsx
@@ -0,0 +1,53 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0, (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Meta, Story } from "@storybook/react";
+import * as React from "react";
+import {
+  WizardShuffleList,
+  WizardShuffleListProps,
+} from "../../../tsrc/components/wizard/WizardShuffleList";
+
+export default {
+  title: "Component/Wizard/WizardShuffleList",
+  component: WizardShuffleList,
+  argTypes: {
+    onChange: {
+      action: "onChange called",
+    },
+  },
+} as Meta<WizardShuffleListProps>;
+
+export const NoValues: Story<WizardShuffleListProps> = (args) => (
+  <WizardShuffleList {...args} />
+);
+NoValues.args = {
+  id: "wizard-shufflelist-story",
+  label: "WizardShuffleList",
+  description: "A Shuffle List with no values set",
+  values: new Set<string>([]),
+};
+
+export const WithValues: Story<WizardShuffleListProps> = (args) => (
+  <WizardShuffleList {...args} />
+);
+WithValues.args = {
+  ...NoValues.args,
+  description:
+    "A Shuffle List with values provided - they should be nicely sorted",
+  values: new Set<string>(["first one", "second one", "and another"]),
+};
---
+++ react-front-end/__tests__/tsrc/components/wizard/WizardShuffleList.test.tsx
@@ -0,0 +1,102 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0, (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { pipe } from "fp-ts/function";
+import * as RSET from "fp-ts/ReadonlySet";
+import * as S from "fp-ts/string";
+import * as React from "react";
+import { WizardShuffleList } from "../../../../tsrc/components/wizard/WizardShuffleList";
+import { languageStrings } from "../../../../tsrc/util/langstrings";
+
+const {
+  common: {
+    action: { add: addString, delete: deleteString },
+  },
+  shuffleList: shuffleListStrings,
+} = languageStrings;
+
+describe("<WizardShuffleList/>", () => {
+  it("displays the values provided", () => {
+    const values: readonly string[] = ["mouse", "cat", "dog"];
+    const { queryByText } = render(
+      <WizardShuffleList
+        values={new Set(values)}
+        onChange={jest.fn()}
+        mandatory={false}
+      />
+    );
+
+    expect(values.every((v) => queryByText(v))).toBeTruthy();
+  });
+
+  it.each([
+    ["pressing enter", true],
+    ["using add button", false],
+  ])(
+    "supports the adding of a value by %s",
+    (_: string, pressEnterKey: boolean) => {
+      const onChange = jest.fn();
+      const { getByLabelText } = render(
+        <WizardShuffleList
+          values={new Set()}
+          onChange={onChange}
+          mandatory={false}
+        />
+      );
+
+      const doAdd = (value: string) => {
+        userEvent.type(
+          getByLabelText(shuffleListStrings.newEntry),
+          `${value}${pressEnterKey ? "{enter}" : ""}`
+        );
+        if (!pressEnterKey) {
+          userEvent.click(getByLabelText(addString));
+        }
+      };
+
+      const me = "Add me";
+      doAdd(me);
+      expect(onChange).toHaveBeenLastCalledWith(new Set([me]));
+    }
+  );
+
+  it("supports removing a value", () => {
+    const onChange = jest.fn();
+    const deleteMe = "Please delete me";
+    const testValues: ReadonlySet<string> = new Set<string>([
+      "Keep me",
+      deleteMe,
+      "Save me for later",
+    ]);
+    const { getByLabelText } = render(
+      <WizardShuffleList
+        values={testValues}
+        onChange={onChange}
+        mandatory={false}
+      />
+    );
+
+    userEvent.click(getByLabelText(`${deleteString} ${deleteMe}`));
+
+    expect(onChange).toHaveBeenLastCalledWith(
+      pipe(testValues, RSET.remove(S.Eq)(deleteMe))
+    );
+  });
+});
