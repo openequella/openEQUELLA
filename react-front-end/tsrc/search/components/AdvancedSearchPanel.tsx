93c51bbc90008b8c6357be240ff896dec54dec1b
yinzi <yinzi.xie@edalex.com>
2021-12-07 15:18:08 +1100

    fix: additional whitespace in user selector

--- react-front-end/tsrc/search/components/AdvancedSearchPanel.tsx
+++ react-front-end/tsrc/search/components/AdvancedSearchPanel.tsx
@@ -130,7 +130,7 @@ export const AdvancedSearchPanel = ({
             currentValues,
             onChangeHandler
           ).map((e) => (
-            <Grid key={e.props.id} item>
+            <Grid key={e.props.id} item container direction="column">
               {e}
             </Grid>
           ))}
