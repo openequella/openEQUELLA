156679d3dd31b739bdc7cae13c9455744c2beb63
Penghai <penghai.zhang@edalex.com>
2022-09-02 15:46:23 +1000

    feat: support opening the legacy Scrapbook creating pages

--- react-front-end/tsrc/modules/ScrapbookModule.ts
+++ react-front-end/tsrc/modules/ScrapbookModule.ts
@@ -16,7 +16,10 @@
  * limitations under the License.
  */
 import * as OEQ from "@openequella/rest-api-client";
+import Axios from "axios";
+import { History } from "history";
 import { API_BASE_URL } from "../AppConfig";
+import type { ChangeRoute } from "../legacycontent/LegacyContent";
 
 /**
  * Delete one Scrapbook Item by UUID.
@@ -24,3 +27,23 @@ import { API_BASE_URL } from "../AppConfig";
  */
 export const deleteScrapbook = (uuid: string): Promise<void> =>
   OEQ.Scrapbook.deleteScrapbook(API_BASE_URL, uuid);
+
+const legacyPageUrl = `${API_BASE_URL}/content/submit/access/myresources.do`;
+
+/**
+ * Open the legacy Scrapbook creating pages.
+ *
+ * A LegacyContent request is firstly sent with different payload determined by
+ * whether to create a file or a web page. The route to the Legacy creating page
+ * will be provided in the response and be used to open the page.
+ *
+ * @param scrapbookType Type of Scrapbook which must be either 'file' or 'page'.
+ * @param history History of browser in which the returning route will be pushed in.
+ */
+export const openLegacyFileCreatingPage = (
+  scrapbookType: "file" | "page",
+  history: History
+): Promise<void> =>
+  Axios.post<ChangeRoute>(legacyPageUrl, {
+    event__: [`${scrapbookType === "file" ? "cmca" : "cmca2"}.contribute`],
+  }).then(({ data: { route } }) => history.push(`/${route}`));
