174bb3905dff29c3d063325ce872cf74a145b36d
Penghai <penghai.zhang@edalex.com>
2022-09-21 10:27:33 +1000

    refactor: update ScrapbookModule to support saving SearchPageOptions to session storage

--- react-front-end/tsrc/modules/ScrapbookModule.ts
+++ react-front-end/tsrc/modules/ScrapbookModule.ts
@@ -17,7 +17,11 @@
  */
 import * as OEQ from "@openequella/rest-api-client";
 import { History } from "history";
+import { MD5 } from "object-hash";
+import { v4 } from "uuid";
 import { API_BASE_URL } from "../AppConfig";
+import type { SearchPageOptions } from "../search/SearchPageHelper";
+import { saveDataToStorage } from "./BrowserStorageModule";
 import {
   getLegacyScrapbookCreatingPageRoute,
   getLegacyScrapbookEditingPageRoute,
@@ -31,25 +35,55 @@ export const deleteScrapbook = (uuid: string): Promise<void> =>
   OEQ.Scrapbook.deleteScrapbook(API_BASE_URL, uuid);
 
 export type ScrapbookType = "file" | "page";
+
+// Convert the supplied SearchPageOptions into a JSON string, generate a MD5 hash from the string,
+// concatenate the hash and the string into one and save it in session storage.
+const saveSearchPageOptions = (searchPageOptions: SearchPageOptions) => {
+  const uuid = v4();
+
+  saveDataToStorage(
+    uuid,
+    searchPageOptions,
+    (json: string) => `${MD5(json)}${json}`,
+    window.sessionStorage
+  );
+
+  return uuid;
+};
+
 /**
  * Open Legacy Scrapbook creating pages by pushing the route of the page to the browser history.
  *
+ * Due to the communication between New UI and Legacy UI, after the creation is completed or cancelled,
+ * the browser will return to the new UI, and we should apply the last SearchPageOptions that was used
+ * before we went to the Legacy UI to the initial search. To achieve this, we save that SearchPageOptions
+ * in the session storage so that we can retrieve it again.
+ *
  * @param scrapbookType Type of Scrapbook which must be either 'file' or 'page'.
  * @param history History of browser in which the returning route will be pushed in.
+ * @param searchPageOptions Current SearchPageOptions to be saved in the session storage.
  */
 export const openLegacyFileCreatingPage = async (
   scrapbookType: ScrapbookType,
-  history: History
-): Promise<void> =>
-  history.push(await getLegacyScrapbookCreatingPageRoute(scrapbookType));
+  history: History,
+  searchPageOptions: SearchPageOptions
+): Promise<void> => {
+  const uuid = saveSearchPageOptions(searchPageOptions);
+  history.push(await getLegacyScrapbookCreatingPageRoute(scrapbookType, uuid));
+};
 
 /**
- * Open Legacy Scrapbook editing pages by pushing the route of the page to the browser history.
+ * Similar to {@link openLegacyFileCreatingPage}, returns the route for Scrapbook editing pages.
  *
  * @param key Unique key of the Scrapbook including the UUID and version.
  * @param history History of browser in which the returning route will be pushed in.
+ * @param searchPageOptions Current SearchPageOptions to be saved in the session storage.
  */
 export const openLegacyFileEditingPage = async (
   key: string,
-  history: History
-): Promise<void> => history.push(await getLegacyScrapbookEditingPageRoute(key));
+  history: History,
+  searchPageOptions: SearchPageOptions
+): Promise<void> => {
+  const uuid = saveSearchPageOptions(searchPageOptions);
+  history.push(await getLegacyScrapbookEditingPageRoute(key, uuid));
+};
