4651a5182ca575fd6cc3478201b15296d59f3207
yinzi <yinzi.xie@edalex.com>
2023-02-10 09:00:39 +1100

    fix: commit missing AuthHelper class file

---
+++ autotest/OldTests/src/test/java/io/github/openequella/rest/AuthHelper.java
@@ -0,0 +1,34 @@
+package io.github.openequella.rest;
+
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.PutMethod;
+
+/** Helper class to build login/logout methods. */
+public class AuthHelper {
+  private String institutionUrl;
+
+  public AuthHelper(String institutionUrl) {
+    this.institutionUrl = institutionUrl;
+  };
+
+  public String getAuthApiEndpoint() {
+    return institutionUrl + "api/auth";
+  }
+
+  protected HttpMethod buildLogoutMethod() {
+    final String logoutEndpoint = getAuthApiEndpoint() + "/logout";
+    return new PutMethod(logoutEndpoint);
+  }
+
+  protected HttpMethod buildLoginMethod(String username, String password) {
+    final String loginEndpoint = getAuthApiEndpoint() + "/login";
+    final NameValuePair[] queryVals = {
+      new NameValuePair("username", username), new NameValuePair("password", password)
+    };
+    final HttpMethod method = new PostMethod(loginEndpoint);
+    method.setQueryString(queryVals);
+    return method;
+  }
+}
