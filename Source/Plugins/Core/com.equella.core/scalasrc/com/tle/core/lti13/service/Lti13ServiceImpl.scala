5fd05c7154d665ff23b8ccc59ce7f1b3c5538a10
Penghai <penghai.zhang@edalex.com>
2023-03-20 16:39:45 +1100

    chore: add annotation '@Transactional' in Lti13ServiceImpl where needed

--- Source/Plugins/Core/com.equella.core/scalasrc/com/tle/core/lti13/service/Lti13ServiceImpl.scala
+++ Source/Plugins/Core/com.equella.core/scalasrc/com/tle/core/lti13/service/Lti13ServiceImpl.scala
@@ -3,6 +3,7 @@ package com.tle.core.lti13.service
 import com.tle.beans.lti.LtiPlatform
 import com.tle.core.guice.Bind
 import com.tle.core.lti13.dao.Lti13DAO
+import org.springframework.transaction.annotation.Transactional
 import javax.inject.{Inject, Singleton}
 import scala.jdk.CollectionConverters._
 
@@ -16,9 +17,12 @@ class Lti13ServiceImpl extends Lti13Service {
 
   override def getAll: List[LtiPlatform] = lti13Dao.enumerateAll.asScala.toList
 
+  @Transactional
   override def create(ltiPlatform: LtiPlatform): Long = lti13Dao.save(ltiPlatform)
 
+  @Transactional
   override def update(ltiPlatform: LtiPlatform): Unit = lti13Dao.update(ltiPlatform)
 
+  @Transactional
   override def delete(ltiPlatform: LtiPlatform): Unit = lti13Dao.delete(ltiPlatform)
 }
