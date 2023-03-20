df9957d49227f1bed20a77297502c36709293a5b
Penghai <penghai.zhang@edalex.com>
2023-03-20 16:38:39 +1100

    chore: fix typo issues in Lti13DAOImpl

--- Source/Plugins/Core/com.equella.core/scalasrc/com/tle/core/lti13/dao/Lti13DAOImpl.scala
+++ Source/Plugins/Core/com.equella.core/scalasrc/com/tle/core/lti13/dao/Lti13DAOImpl.scala
@@ -16,14 +16,14 @@ class Lti13DAOImpl
     extends GenericInstitionalDaoImpl[LtiPlatform, java.lang.Long](classOf[LtiPlatform])
     with Lti13DAO {
 
-  override def getByPlatformID(platformID: String): Option[LtiPlatform] =
+  override def getByPlatformID(platformId: String): Option[LtiPlatform] =
     Try {
       getHibernateTemplate
         .execute(
           (session: Session) =>
             session
               .getNamedQuery("getByPlatformID")
-              .setParameter("platformID", platformID)
+              .setParameter("platformId", platformId)
               .setParameter("institution", CurrentInstitution.get())
               .getResultList)
         .asScala
@@ -31,6 +31,6 @@ class Lti13DAOImpl
         .asInstanceOf[List[LtiPlatform]]
     }.toEither
       .filterOrElse(_.size <= 1,
-                    new Throwable(s"More than one LTI platforms matching ID $platformID"))
+                    new Throwable(s"More than one LTI platforms matching ID $platformId"))
       .fold(throw _, _.headOption)
 }
