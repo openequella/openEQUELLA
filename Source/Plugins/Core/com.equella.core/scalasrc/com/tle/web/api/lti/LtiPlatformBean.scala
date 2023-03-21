99c9b7010e85760064c8fdb76ee0810659289110
Penghai <penghai.zhang@edalex.com>
2023-03-21 17:18:35 +1100

    chore: add file headers for all the new scala files

--- Source/Plugins/Core/com.equella.core/scalasrc/com/tle/web/api/lti/LtiPlatformBean.scala
+++ Source/Plugins/Core/com.equella.core/scalasrc/com/tle/web/api/lti/LtiPlatformBean.scala
@@ -1,13 +1,29 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0, (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.tle.web.api.lti
 
-import cats.data.{Validated, ValidatedNel}
-import com.tle.beans.cloudproviders.CloudControlConfigType.XPath
+import cats.data.Validated
 import com.tle.beans.lti.{LtiCustomRole, LtiPlatform}
 import com.tle.common.Check
 import com.tle.common.institution.CurrentInstitution
 import com.tle.integration.lti13.UnknownUserHandling
 import cats.implicits._
-
 import java.net.URL
 import scala.jdk.CollectionConverters._
 import scala.util.Try
