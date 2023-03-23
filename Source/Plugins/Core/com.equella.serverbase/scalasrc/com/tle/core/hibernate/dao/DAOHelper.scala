6753fc1de39d2b853dd866ccf542f1d72a4b4b47
Penghai <penghai.zhang@edalex.com>
2023-03-23 18:17:22 +1100

    feat: create a new helper function to support deleting with Hibernate in Scala

--- Source/Plugins/Core/com.equella.serverbase/scalasrc/com/tle/core/hibernate/dao/DAOHelper.scala
+++ Source/Plugins/Core/com.equella.serverbase/scalasrc/com/tle/core/hibernate/dao/DAOHelper.scala
@@ -1,6 +1,7 @@
 package com.tle.core.hibernate.dao
 
 import org.hibernate.Session
+import org.hibernate.query.Query
 
 import scala.jdk.CollectionConverters._
 import scala.util.Try
@@ -9,6 +10,14 @@ import scala.util.Try
   * This object provides helper functions to assist dealing with Hibernate in Scala.
   */
 object DAOHelper {
+  private def prepareQuery(session: Session, query: String, params: Map[String, Any]): Query[_] = {
+    val namedQuery = session.getNamedQuery(query)
+    params.foreach {
+      case (key, value) => namedQuery.setParameter(key, value)
+    }
+
+    namedQuery
+  }
 
   /**
     * This generic function is used to retrieve one entity by the provided parameters. If more than one entities returned
@@ -27,14 +36,7 @@ object DAOHelper {
                                                params: Map[String, Any]): Option[T] = {
     Try {
       dao.getHibernateTemplate
-        .execute((session: Session) => {
-          val namedQuery = session.getNamedQuery(query)
-          params.foreach {
-            case (key, value) => namedQuery.setParameter(key, value)
-          }
-
-          namedQuery.getResultList
-        })
+        .execute(prepareQuery(_, query, params).getResultList)
         .asScala
         .toList
         .asInstanceOf[List[T]]
@@ -43,4 +45,21 @@ object DAOHelper {
                     new Throwable(s"More than one entities matching the provided parameters"))
       .fold(throw _, _.headOption)
   }
+
+  /**
+    * This generic function is used to delete entities by the provided parameters.
+    *
+    * @param dao Implementation of a DAO.
+    * @param query Name of a pre-defined named query used to delete some entities.
+    * @param params A list of parameters to be applied to the query.
+    * @tparam D Type of the DAO which MUST extend AbstractHibernateDao.
+    * @return Either thrown exceptions or the number of entities that have been deleted.
+    */
+  def delete[D <: AbstractHibernateDao](dao: D,
+                                        query: String,
+                                        params: Map[String, Any]): Either[Throwable, Int] =
+    Try {
+      dao.getHibernateTemplate
+        .execute(prepareQuery(_, query, params).executeUpdate())
+    }.toEither
 }
