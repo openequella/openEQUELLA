acb93e157c3d2d674c44df795d77fe841ea6bb64
Ian Stevenson <ian.stevenson@edalex.com>
2023-06-02 16:36:26 +1000

    test: add TrieMapCache from component branch
    I needed this test ReplicatedCache for the tests of the new
    WebDavAuthService, however it was part of a larger commit. So I manually
     copied across the file from the LTI 1.3 component branch.
    
    OEQ-1485

---
+++ Source/Plugins/Core/com.equella.core/test/scala/com/tle/core/replicatedcache/TrieMapCache.scala
@@ -0,0 +1,29 @@
+package com.tle.core.replicatedcache
+
+import com.google.common.base.Optional
+import com.tle.common.Pair
+import com.tle.core.replicatedcache.ReplicatedCacheService.ReplicatedCache
+
+import java.lang
+import scala.collection.concurrent.TrieMap
+
+/**
+  * A implementation of `ReplicatedCache` for testing which is _not_ replicated and simply stores
+  * things in a TriMap instance.
+  *
+  * @tparam T the type of data to be stored
+  */
+class TrieMapCache[T <: Serializable] extends ReplicatedCache[T] {
+  private val cache: TrieMap[String, T] = TrieMap()
+
+  override def get(key: String): Optional[T] = cache.get(key) match {
+    case Some(value) => Optional.of(value)
+    case None        => Optional.absent()
+  }
+
+  override def put(key: String, value: T): Unit = cache.addOne(key, value)
+
+  override def invalidate(keys: String*): Unit = keys.foreach(cache.remove)
+
+  override def iterate(keyPrefixFilter: String): lang.Iterable[Pair[String, T]] = ???
+}
