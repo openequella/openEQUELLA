85f380b8001e4535d85880c3fef1fbe28569b23b
Penghai <penghai.zhang@edalex.com>
2023-10-13 10:22:35 +1100

    test: tweaks for OAuthRedirector
    1. Add ScalaDoc and comments to explain how it works;
    2. Drop the useless function 'ent';
    3. Return response properly for failed requests.

--- autotest/IntegTester/src/main/scala/integtester/oauthredirector/OAuthRedirector.scala
+++ autotest/IntegTester/src/main/scala/integtester/oauthredirector/OAuthRedirector.scala
@@ -3,7 +3,6 @@ package integtester.oauthredirector
 import cats.effect.IO
 import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import com.google.common.collect.MapMaker
 import com.google.common.io.CharStreams
 import org.http4s.dsl.Http4sDsl
 import org.http4s.headers._
@@ -11,18 +10,24 @@ import org.http4s._
 import java.io.InputStreamReader
 import java.net.{URL, URLEncoder}
 import java.util.UUID
-import java.util.concurrent.ConcurrentMap
-import scala.util.Using
+import java.util.concurrent.{ConcurrentHashMap, ConcurrentMap}
+import scala.util.{Failure, Success, Try, Using}
 
-case class TestInfo(clientId: String, clientSecret: String, equellaUrl: String)
+case class TestInfo(clientId: String, clientSecret: String, equellaUrl: String, state: String)
+case class WebException(code: Int, error: String) extends RuntimeException
 
+/**
+  * It is called by OAuthTest with an initial request which contains OAuth credentials as params
+  * prefixed with 'test_', then it does the OAuth authentication with oEQ, and then finally presents
+  * back a page with the results which the test then checks.
+  */
 object OAuthRedirector extends Http4sDsl[IO] {
   private val PARAM_STATE                              = "state"
   private val PARAM_CODE                               = "code"
   private val PARAM_RESPONSE_TYPE                      = "response_type"
   private val PARAM_ERROR                              = "error"
   private val PARAM_ERROR_DESCRIPTION                  = "error_description"
-  private val session: ConcurrentMap[String, TestInfo] = new MapMaker().makeMap()
+  private val session: ConcurrentMap[String, TestInfo] = new ConcurrentHashMap()
 
   private def writeMapResponse(vals: Map[String, String]): IO[Response[IO]] = {
     val writer = new StringBuilder
@@ -31,11 +36,11 @@ object OAuthRedirector extends Http4sDsl[IO] {
     vals.foreach {
       case (key, value) =>
         writer.append("<div>")
-        writer.append(ent(key + ": "))
+        writer.append(key + ": ")
         writer.append("<span id=\"")
-        writer.append(ent(key))
+        writer.append(key)
         writer.append("\">")
-        writer.append(ent(value))
+        writer.append(value)
         writer.append("</span></div>")
     }
 
@@ -44,43 +49,7 @@ object OAuthRedirector extends Http4sDsl[IO] {
 
   private def urlEncode(value: String): String = URLEncoder.encode(value, "UTF-8")
 
-  private def ent(szStr: String): String = {
-    Option(szStr) match {
-      case Some(str) =>
-        val szOut              = new StringBuilder()
-        val chars: Array[Char] = str.toCharArray
-
-        chars.foreach {
-          case '<' => szOut.append("&lt;")
-          case '>' => szOut.append("&gt;")
-          case '&' => szOut.append("&amp;")
-          case '"' => szOut.append("&quot;")
-          case other
-              if other == 0xA || other == 0xD || other == 0x9 || (other >= 0x20 && other <= 0x007F) =>
-            szOut.append(other)
-          case other
-              if (other > 0x007F && other <= 0xD7FF)
-                || (other >= 0xE000 && other <= 0xFFFD)
-                || (other >= 0x10000 && other <= 0x10FFFF) =>
-            szOut.append("&#x")
-            val hexed = Integer.toHexString(other)
-            4 - hexed.length() match {
-              case 3 => szOut.append('0')
-              case 2 => szOut.append('0')
-              case 1 => szOut.append('0')
-              case _ => // nothing for else
-            }
-            szOut.append(hexed)
-            szOut.append(';')
-          case _ => // discard the character entirely
-        }
-
-        szOut.toString()
-      case None => ""
-    }
-  }
-
-  private def initialise(params: Map[String, String]) = {
+  private def initialise(params: Map[String, String]): TestInfo = {
     val state: String = params.getOrElse(PARAM_STATE, UUID.randomUUID().toString)
 
     params.get("test_client_id") match {
@@ -88,13 +57,15 @@ object OAuthRedirector extends Http4sDsl[IO] {
         val clientSecret: String = params.getOrElse("test_client_secret", "")
         val equellaUrl: String   = params.getOrElse("test_equella_url", "")
 
-        session.put(state, TestInfo(testClientId, clientSecret, equellaUrl))
+        val testInfo = TestInfo(testClientId, clientSecret, equellaUrl, state)
+        session.put(state, testInfo)
 
-        (testClientId, clientSecret, equellaUrl, state)
-      case None if state.nonEmpty =>
+        testInfo
+      case None
+          if state.nonEmpty => // This is a redirect back from oEQ and the state has been registered.
         val conf = session.get(state)
-        (conf.clientId, conf.clientSecret, conf.equellaUrl, state)
-      case _ => throw new RuntimeException("test_client_id not supplied")
+        TestInfo(conf.clientId, conf.clientSecret, conf.equellaUrl, state)
+      case _ => throw WebException(400, "Client ID not supplied")
     }
   }
 
@@ -105,6 +76,8 @@ object OAuthRedirector extends Http4sDsl[IO] {
                            clientSecret: String,
                            redirectUri: String): IO[Response[IO]] = params.get(PARAM_CODE) match {
     case Some(code) if session.containsKey(state) =>
+      // A response from oEQ has been received with the state previously provided,
+      // now do the next step and request an access token using the provided auth code.
       val tokenUrl = new URL(
         s"${equellaUrl}oauth/access_token?grant_type=authorization_code&client_id=$clientId&redirect_uri=${urlEncode(
           redirectUri)}&client_secret=$clientSecret&code=${urlEncode(code)}")
@@ -122,29 +95,40 @@ object OAuthRedirector extends Http4sDsl[IO] {
       session.remove(state)
       writeMapResponse(Map("access_token" -> tokenData))
     case None =>
+      // undertake first OAuth redirect to authenticate to oEQ and register a state value.
       val responseType = params.getOrElse(PARAM_RESPONSE_TYPE, "code")
       val authUrl =
         Uri.unsafeFromString(
           s"${equellaUrl}oauth/authorise?response_type=$responseType&client_id=$clientId&redirect_uri=${urlEncode(
             redirectUri)}&state=${urlEncode(state)}")
       Found(authUrl).map(_.putHeaders(Location(authUrl)))
-    case _ => throw new RuntimeException("The state does not match. You may be a victim of CSRF.");
+    case _ => throw WebException(403, "The state does not match. You may be a victim of CSRF.")
   }
 
   def oauthRedirector(request: Request[IO]): IO[Response[IO]] = {
-    val params                                      = request.params
-    val (clientId, clientSecret, equellaUrl, state) = initialise(params)
-    val redirect_uri                                = s"http://localhost:8083${request.uri.path}"
+    val params = request.params
+    Try {
+      val TestInfo(clientId, clientSecret, equellaUrl, state) = initialise(params)
+      val redirect_uri                                        = s"http://localhost:8083${request.uri.path}"
 
-    params.get(PARAM_ERROR) match {
-      case Some(error) =>
-        val vals = Map(
-          PARAM_ERROR             -> error,
-          PARAM_ERROR_DESCRIPTION -> params.getOrElse(PARAM_ERROR_DESCRIPTION, "")
-        )
-        writeMapResponse(vals)
-      case None =>
-        processState(params, state, equellaUrl, clientId, clientSecret, redirect_uri)
+      params.get(PARAM_ERROR) match {
+        case Some(error) =>
+          val vals = Map(
+            PARAM_ERROR             -> error,
+            PARAM_ERROR_DESCRIPTION -> params.getOrElse(PARAM_ERROR_DESCRIPTION, "")
+          )
+          writeMapResponse(vals)
+        case None =>
+          processState(params, state, equellaUrl, clientId, clientSecret, redirect_uri)
+      }
+    } match {
+      case Success(resp) => resp
+      case Failure(ex: WebException) =>
+        ex.code match {
+          case 400 => BadRequest(ex.error)
+          case 403 => Forbidden(ex.error)
+        }
+      case Failure(ex) => throw new RuntimeException(ex)
     }
   }
 }
