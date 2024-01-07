2e3d983275e34778ccd092d35e5848b11d653a31
Yinzi Xie <yinzi.xie@edalex.com>
2024-01-07 22:46:30 +0000

    OEQ-1699, OEQ-1700 - feat: add hierarchy rest module

---
+++ react-front-end/tsrc/modules/HierarchyModule.ts
@@ -0,0 +1,84 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0, (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import * as OEQ from "@openequella/rest-api-client";
+import { API_BASE_URL } from "../AppConfig";
+
+/**
+ * Get summaries of all the hierarchy topics.
+ */
+export const getHierarchies = (): Promise<
+  OEQ.BrowseHierarchy.HierarchyTopicSummary[]
+> => OEQ.BrowseHierarchy.browseHierarchies(API_BASE_URL);
+
+/**
+ * Get details of a hierarchy topic, including all the key resources and basic information of parent topics.
+ *
+ * @param compoundUuid Compound UUID of the hierarchy topic.
+ */
+export const getHierarchy = (
+  compoundUuid: string,
+): Promise<OEQ.BrowseHierarchy.HierarchyTopic<OEQ.Search.SearchResultItem>> =>
+  OEQ.BrowseHierarchy.browseHierarchy(API_BASE_URL, compoundUuid);
+
+/**
+ * Get all ACLs for the hierarchy topic.
+ *
+ * @param compoundUuid Topic compound UUID.
+ */
+export const getMyAcls = (
+  compoundUuid: string,
+): Promise<OEQ.Hierarchy.HierarchyTopicAcl> =>
+  OEQ.Hierarchy.getMyAcls(API_BASE_URL, compoundUuid);
+
+/**
+ * Add a key resource to a hierarchy topic
+ *
+ * @param compoundUuid Topic compound UUID.
+ * @param itemUuid The item UUID.
+ * @param itemVersion The item version.
+ */
+export const addKeyResource = (
+  compoundUuid: string,
+  itemUuid: string,
+  itemVersion: number,
+): Promise<void> =>
+  OEQ.Hierarchy.addKeyResource(
+    API_BASE_URL,
+    compoundUuid,
+    itemUuid,
+    itemVersion,
+  );
+
+/**
+ * Delete a key resource to a hierarchy topic
+ *
+ * @param compoundUuid Topic compound UUID.
+ * @param itemUuid The item UUID.
+ * @param itemVersion The item version.
+ */
+export const deleteKeyResource = (
+  compoundUuid: string,
+  itemUuid: string,
+  itemVersion: number,
+): Promise<void> =>
+  OEQ.Hierarchy.deleteKeyResource(
+    API_BASE_URL,
+    compoundUuid,
+    itemUuid,
+    itemVersion,
+  );
