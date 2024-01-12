cd9f07bec8d0a0d01b8819747c17ccae74e30ba6
Penghai <penghai.zhang@edalex.com>
2024-01-12 13:44:42 +1100

    chore: fix a typo issue in entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 STANDARD_JVM_ARGS=$(grep "JAVA_OPTS" manager/equellaserver-config.sh  | awk -F '"' '{print $2}')
-JVM_ARGS="$TANDARD_JVM_ARGS $JVM_ARGS"
+JVM_ARGS="$STANDARD_JVM_ARGS $JVM_ARGS"
 echo $JVM_ARGS
 
 java $JVM_ARGS -cp learningedge-config:server/equella-server.jar com.tle.core.equella.runner.EQUELLAServer
