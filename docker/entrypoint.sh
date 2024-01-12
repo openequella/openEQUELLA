1cb373a81d008814b4efb7469482250536c2b01f
Penghai <penghai.zhang@edalex.com>
2024-01-12 13:34:27 +1100

    fix: make sure standard oEQ JVM args are used in the docker image
    Also update the Dockerfile to use ENTRYPOINT to start OEQ server.

---
+++ docker/entrypoint.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+STANDARD_JVM_ARGS=$(grep "JAVA_OPTS" manager/equellaserver-config.sh  | awk -F '"' '{print $2}')
+JVM_ARGS="$TANDARD_JVM_ARGS $JVM_ARGS"
+echo $JVM_ARGS
+
+java $JVM_ARGS -cp learningedge-config:server/equella-server.jar com.tle.core.equella.runner.EQUELLAServer
+
