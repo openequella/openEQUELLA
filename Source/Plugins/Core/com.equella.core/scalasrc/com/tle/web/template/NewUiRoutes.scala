b60fdeb821dcd69788f44317a0297db020b574a0
Yinzi Xie <yinzi.xie@edalex.com>
2024-03-26 23:27:58 +0000

    OEQ-1904 - fix: each Hierarchy menu triggers a full page refresh

--- Source/Plugins/Core/com.equella.core/scalasrc/com/tle/web/template/NewUiRoutes.scala
+++ Source/Plugins/Core/com.equella.core/scalasrc/com/tle/web/template/NewUiRoutes.scala
@@ -25,9 +25,10 @@ import io.lemonlabs.uri.{QueryString, RelativeUrl}
   * Centralises the various routes for the New UI - especially useful when doing `forwardToUrl`.
   */
 object NewUiRoutes {
-  val PATH_MYRESOURCES: String        = "page/myresources"
-  val PATH_BROWSE_HIERARCHIES: String = "page/hierarchies"
-  val PATH_HIERARCHY: String          = "page/hierarchy"
+  val RELATIVE_PATH_MYRESOURCES: String = "page/myresources"
+  val PATH_MYRESOURCES: String          = "/" + RELATIVE_PATH_MYRESOURCES
+  val PATH_BROWSE_HIERARCHIES: String   = "/page/hierarchies"
+  val PATH_HIERARCHY: String            = "/page/hierarchy"
 
   /**
     * Builds a relative URL for the New UI Hierarchy pages.
@@ -62,6 +63,6 @@ object NewUiRoutes {
         .filter({
           case (_, v) => v.isDefined
         })
-    buildRelativeUrl(path = PATH_MYRESOURCES, params)
+    buildRelativeUrl(path = RELATIVE_PATH_MYRESOURCES, params)
   }
 }
