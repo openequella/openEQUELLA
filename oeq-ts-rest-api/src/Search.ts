9efaaf130f70039d50015804645e85ac364e3015
Penghai <penghai.zhang@edalex.com>
2024-08-29 15:23:38 +1000

    feat: update the type definition of search result attachment to include viewer configuration

--- oeq-ts-rest-api/src/Search.ts
+++ oeq-ts-rest-api/src/Search.ts
@@ -264,6 +264,10 @@ export interface Attachment {
     externalId?: string;
   };
   /**
+   * The configuration of attachment viewer such as Kaltura player width and height.
+   */
+  viewerConfig?: Record<string, string>;
+  /**
    * If a file attachment, the path for the represented file
    */
   filePath?: string;
