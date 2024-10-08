050805d3365d67f0386f3844a0bb4174b2e17e6b
Penghai <penghai.zhang@edalex.com>
2024-10-08 15:30:28 +1100

    chore: import generateRandomHexString properly in Lti13StateService

--- Source/Plugins/Core/com.equella.core/scalasrc/com/tle/integration/lti13/Lti13StateService.scala
+++ Source/Plugins/Core/com.equella.core/scalasrc/com/tle/integration/lti13/Lti13StateService.scala
@@ -18,10 +18,10 @@
 
 package com.tle.integration.lti13
 
+import com.tle.common.util.StringUtils.generateRandomHexString
 import com.tle.core.guice.Bind
 import com.tle.core.replicatedcache.ReplicatedCacheService
 import com.tle.core.replicatedcache.ReplicatedCacheService.ReplicatedCache
-import com.tle.integration.oidc.generateRandomHexString
 
 import java.net.URI
 import java.util.concurrent.TimeUnit
