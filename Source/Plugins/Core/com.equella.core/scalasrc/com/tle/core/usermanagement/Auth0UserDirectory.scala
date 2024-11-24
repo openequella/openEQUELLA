be036a1b18d7eba84d2b9ea88b342198dc35d075
Penghai <penghai.zhang@edalex.com>
2024-11-25 10:01:49 +1100

    feat: support searching users with Entra ID
    While working on this, a new abstract class is created to provide all the required
    abstraction for using REST APIs.

--- Source/Plugins/Core/com.equella.core/scalasrc/com/tle/core/usermanagement/Auth0UserDirectory.scala
+++ Source/Plugins/Core/com.equella.core/scalasrc/com/tle/core/usermanagement/Auth0UserDirectory.scala
@@ -18,35 +18,50 @@
 
 package com.tle.core.usermanagement
 
-import cats.implicits._
-import com.tle.common.Pair
 import com.tle.common.usermanagement.user.valuebean.{DefaultUserBean, UserBean}
 import com.tle.core.guice.Bind
-import com.tle.core.oauthclient.{OAuthClientService, OAuthTokenState, TokenRequest}
-import com.tle.core.usermanagement.GenericIdPUser.toUserBean
+import com.tle.core.oauthclient.TokenRequest
 import com.tle.integration.oidc.idp.{GenericIdentityProviderDetails, IdentityProviderPlatform}
-import com.tle.plugins.ump.UserDirectory
 import io.circe.Decoder
 import io.circe.generic.semiauto.deriveDecoder
-import org.slf4j.LoggerFactory
-import sttp.client.circe.asJson
-import sttp.client.{UriContext, basicRequest}
-
-import java.util
-import scala.jdk.CollectionConverters._
 
 /**
-  * Structure for the information of a single user returned from a generic Identity Provider.
+  * Structure for the information of a single user returned from Auth0.
   */
-case class GenericIdPUser(user_id: String,
-                          name: String,
-                          username: Option[String],
-                          family_name: Option[String],
-                          given_name: Option[String],
-                          email: Option[String])
+case class Auth0User(user_id: String,
+                     name: String,
+                     username: Option[String],
+                     family_name: Option[String],
+                     given_name: Option[String],
+                     email: Option[String])
 
-object GenericIdPUser {
-  implicit val idpUserDecoder = deriveDecoder[GenericIdPUser]
+/**
+  * The primary target of this User Directory is Auth0's User Management APIs. However, it can also be reused for other
+  * platform's APIs as long as the OIDC configuration fits [[GenericIdentityProviderDetails]] and their APIs meet the
+  * requirements listed below:
+  *
+  * 1. The returned data MUST fit the structure of `Auth0User` for searching one user and `List[Auth0User]` for
+  *    searching multiple users;
+  * 2. The APIs MUST have the same endpoints as Auth0's User Management APIs, as well as support the same query parameters;
+  * 3. The APIs can't require any additional data in the POST request for an OAuth2 Access Token.
+  *
+  * Reference link: https://auth0.com/docs/api/management/v2/users/get-users
+  */
+@Bind
+class Auth0UserDirectory extends ApiUserDirectory {
+
+  override val targetPlatform: IdentityProviderPlatform.Value = IdentityProviderPlatform.AUTH0
+
+  override protected type USER = Auth0User
+
+  override protected implicit val userDecoder: Decoder[Auth0User] = deriveDecoder[Auth0User]
+
+  override protected type USERS = List[Auth0User]
+
+  override protected implicit val usersDecoder: Decoder[List[Auth0User]] =
+    Decoder.decodeList[Auth0User]
+
+  override protected def toUserList(users: USERS): List[Auth0User] = users
 
   /**
     * Family name and given name are optional fields in Auth0's user data. If family name is absent, use the account name
@@ -54,7 +69,7 @@ object GenericIdPUser {
     * absent, use an empty string as the given name. With this set up, those pages which use family name and given name will
     * not end up showing a blank string.
     */
-  def toUserBean(user: GenericIdPUser): UserBean = {
+  override protected def toUserBean(user: Auth0User): UserBean = {
     new DefaultUserBean(
       user.user_id,
       user.username.getOrElse(user.user_id),
@@ -63,100 +78,22 @@ object GenericIdPUser {
       user.email.orNull
     )
   }
-}
 
-/**
-  * The primary target of this User Directory is Auth0's User Management APIs. However, it can also be reused for other
-  * platform's APIs as long as the OIDC configuration fits [[GenericIdentityProviderDetails]] and their APIs meet the
-  * requirements listed below:
-  *
-  * 1. The returned data MUST fit the structure of `GenericIdPUser` for searching one user and `List[GenericIdPUser]` for
-  *    searching multiple users;
-  * 2. The APIs MUST have the same endpoints as Auth0's User Management APIs, as well as support the same query parameters;
-  * 3. The APIs can't require any additional data in the POST request for an OAuth2 Access Token.
-  *
-  * Reference link: https://auth0.com/docs/api/management/v2/users/get-users
-  */
-@Bind
-class Auth0UserDirectory extends OidcUserDirectory {
-  private val LOGGER = LoggerFactory.getLogger(classOf[Auth0UserDirectory])
+  override protected def userEndpoint(idp: GenericIdentityProviderDetails, id: String): String =
+    s"${idp.apiUrl.toString}users/$id"
 
-  override val targetPlatform: IdentityProviderPlatform.Value = IdentityProviderPlatform.AUTH0
+  override protected def userListEndpoint(idp: GenericIdentityProviderDetails,
+                                          query: String): String =
+    s"${idp.apiUrl.toString}users?q=$query"
 
-  override type IDP = GenericIdentityProviderDetails
-
-  override type AuthResult = OAuthTokenState
-
-  override def searchUsers(
-      query: String): Pair[UserDirectory.ChainResult, util.Collection[UserBean]] = {
-    lazy val search: String => (IDP, OAuthTokenState) => Either[Throwable, List[GenericIdPUser]] =
-      (query: String) =>
-        (idp: IDP, tokenState: OAuthTokenState) => {
-          val apiUrl   = idp.apiUrl.toString
-          val endpoint = s"${apiUrl}users?q=$query"
-          request[List[GenericIdPUser]](endpoint, tokenState)
-      }
-
-    val users = execute(search(query))
-      .leftMap(LOGGER.error(s"Failed to search users", _))
-      .getOrElse(List.empty)
-      .map(toUserBean)
-      .asJavaCollection
-
-    new Pair(UserDirectory.ChainResult.CONTINUE, users)
-  }
-
-  override def getInformationForUser(userId: String): UserBean = {
-    lazy val search: String => (IDP, OAuthTokenState) => Either[Throwable, GenericIdPUser] =
-      (id: String) =>
-        (idp: IDP, tokenState: OAuthTokenState) => {
-          val apiUrl   = idp.apiUrl.toString
-          val endpoint = s"${apiUrl}users/$id"
-          request[GenericIdPUser](endpoint, tokenState)
-      }
-
-    execute(search(userId))
-      .leftMap(LOGGER.error(s"Failed to get information for user $userId", _))
-      .map(toUserBean)
-      .toOption
-      .orNull
-  }
-
-  override def getInformationForUsers(
-      userIDs: util.Collection[String]): util.Map[String, UserBean] =
-    userIDs.asScala
-      .map(id => id -> Option(getInformationForUser(id)))
-      .collect {
-        case (id, Some(info)) => (id, info)
-      }
-      .toMap
-      .asJava
-
-  override protected def authenticate(idp: IDP): Either[Throwable, AuthResult] = {
-    val tokenUrl = idp.commonDetails.tokenUrl.toString
-
-    // To get more details of what params are required, please refer to https://auth0.com/docs/secure/tokens/access-tokens/get-access-tokens.
-    val tokenRequest = TokenRequest(
-      tokenUrl,
-      idp.apiClientId,
-      idp.apiClientSecret,
-      Option(Map("audience" -> idp.apiUrl.toString))
-    )
-
-    Either.catchNonFatal(OAuthClientService.tokenForClient(tokenRequest))
-  }
-
-  // Send a GET request to the supplied endpoint with the supplied access token.
-  private def request[T](endpoint: String, tokenState: OAuthTokenState)(
-      implicit decoder: Decoder[T]): Either[Throwable, T] = {
-    val req = basicRequest
-      .get(uri"$endpoint")
-      .response(asJson[T])
-
-    for {
-      resp <- Either.catchNonFatal(
-        OAuthClientService.requestWithToken(req, tokenState.token, tokenState.tokenType))
-      data <- resp.body
-    } yield data
-  }
+  /**
+    * According to the doco of Auth0, 'audience' is required in the request and the value should be the API URL.
+    *
+    * Reference link: https://auth0.com/docs/secure/tokens/access-tokens/get-access-tokens.
+    */
+  override protected def tokenRequest(idp: IDP): TokenRequest =
+    TokenRequest(idp.commonDetails.tokenUrl.toString,
+                 idp.apiClientId,
+                 idp.apiClientSecret,
+                 Option(Map("audience" -> idp.apiUrl.toString)))
 }
