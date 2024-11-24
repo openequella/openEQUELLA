1a705ad8007d0db0391b3c6efed4d4aa8b8c2407
Penghai <penghai.zhang@edalex.com>
2024-11-25 10:34:56 +1100

    chore: make sure using the Graph REST API v1

--- Source/Plugins/Core/com.equella.core/scalasrc/com/tle/integration/oidc/idp/EntraId.scala
+++ Source/Plugins/Core/com.equella.core/scalasrc/com/tle/integration/oidc/idp/EntraId.scala
@@ -43,7 +43,7 @@ final case class EntraId(
     with RestApi {
   override def platform: IdentityProviderPlatform.Value = IdentityProviderPlatform.ENTRA_ID
 
-  override val apiUrl: String = "https://graph.microsoft.com"
+  override val apiUrl: String = "https://graph.microsoft.com/v1.0"
 
   override def validate: ValidatedNel[String, EntraId] =
     (super.validate, validateApiDetails).mapN((_, _) => this)
