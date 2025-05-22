dfded1869dd788249c276861b3c0b403f0b01fa3
Penghai <penghai.zhang@edalex.com>
2025-05-22 10:55:02 +1000

    chore: fix TS format issues

--- oeq-ts-rest-api/test/TestUtils.ts
+++ oeq-ts-rest-api/test/TestUtils.ts
@@ -15,20 +15,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import type { AxiosInstance } from "axios";
-import type { CookieJar } from "tough-cookie";
+import type { AxiosInstance } from 'axios';
+import type { CookieJar } from 'tough-cookie';
 import * as OEQ from '../src';
-import { axiosInstance } from "../src/AxiosInstance";
+import { axiosInstance } from '../src/AxiosInstance';
 import * as TC from './TestConfig';
 
 /**
  * Executes a logout request and then clears all cookies if the request succeeds.
  */
-export const logout =  (): Promise<void> =>
+export const logout = (): Promise<void> =>
   OEQ.Auth.logout(TC.API_PATH).then(() => {
     console.log('Clearing all cookies.');
 
-    const mockedAxios = axiosInstance()
-    const defaultConfig: AxiosInstance['defaults'] & { jar?: CookieJar } = mockedAxios.defaults;
+    const mockedAxios = axiosInstance();
+    const defaultConfig: AxiosInstance['defaults'] & { jar?: CookieJar } =
+      mockedAxios.defaults;
     defaultConfig.jar?.removeAllCookiesSync();
   });
