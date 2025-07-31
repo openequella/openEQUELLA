e196300574cc2515c46bfca10c1dcdd09582f488
edalex-yinzi <yinzi.xie@edalex.com>
2025-07-31 15:23:08 +1000

    feat: integrate FavouritesSelector into FavouritesPage

--- react-front-end/tsrc/favourites/FavouritesPage.tsx
+++ react-front-end/tsrc/favourites/FavouritesPage.tsx
@@ -16,24 +16,39 @@
  * limitations under the License.
  */
 
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { NEW_FAVOURITES_PATH } from "../mainui/routes";
 import { TemplateUpdateProps } from "../mainui/Template";
 import * as React from "react";
 import { InitialSearchConfig, Search } from "../search/Search";
 import { SearchPageBody } from "../search/SearchPageBody";
-import { SearchPageOptions } from "../search/SearchPageHelper";
+import {
+  SearchContext,
+  SearchContextProps,
+  SearchPageHeaderConfig,
+  SearchPageOptions,
+  SearchPageRefinePanelConfig,
+  defaultSearchPageRefinePanelConfig,
+} from "../search/SearchPageHelper";
 import { languageStrings } from "../util/langstrings";
+import FavouritesSelector, {
+  FavouritesType,
+} from "./components/FavouritesSelector";
 
 const { title } = languageStrings.favourites;
+const { title: favouritesSelectorTitle } =
+  languageStrings.favourites.favouritesSelector;
 
 const FavouritesPage = ({ updateTemplate }: TemplateUpdateProps) => {
+  const [favouritesType, setFavouritesType] =
+    useState<FavouritesType>("resources");
+
   const initialSearchConfig = useMemo<InitialSearchConfig>(() => {
     const customiseInitialSearchOptions = (
       searchPageOptions: SearchPageOptions,
     ): SearchPageOptions => ({
       ...searchPageOptions,
-      filterExpansion: true,
+      filterExpansion: false,
     });
 
     return {
@@ -43,13 +58,61 @@ const FavouritesPage = ({ updateTemplate }: TemplateUpdateProps) => {
     };
   }, []);
 
+  const onFavouritesTypeChangeBuilder =
+    (_: SearchContextProps) => (value: FavouritesType) => {
+      setFavouritesType(value);
+      //TODO: trigger a new search.
+    };
+
+  const favouritesPageHeaderConfig: SearchPageHeaderConfig = {
+    enableCSVExportButton: false,
+    enableShareSearchButton: false,
+    enableFavouriteSearchButton: false,
+  };
+
+  const favouritesPageRefinePanelConfig = (
+    searchContextProps: SearchContextProps,
+  ): SearchPageRefinePanelConfig => {
+    const defaultSearchConfig =
+      favouritesType === "resources" ? defaultSearchPageRefinePanelConfig : {};
+
+    return {
+      ...defaultSearchConfig,
+      enableAdvancedSearchSelector: false,
+      enableDateRangeSelector: true,
+      customRefinePanelControl: [
+        {
+          idSuffix: "FavouritesSelector",
+          title: favouritesSelectorTitle,
+          component: (
+            <FavouritesSelector
+              value={favouritesType}
+              onChange={onFavouritesTypeChangeBuilder(searchContextProps)}
+            />
+          ),
+          alwaysVisible: true,
+        },
+      ],
+    };
+  };
+
   return (
     <Search
       updateTemplate={updateTemplate}
       initialSearchConfig={initialSearchConfig}
       pageTitle={title}
     >
-      <SearchPageBody pathname={NEW_FAVOURITES_PATH} />
+      <SearchContext.Consumer>
+        {(searchContextProps: SearchContextProps) => (
+          <SearchPageBody
+            pathname={NEW_FAVOURITES_PATH}
+            headerConfig={favouritesPageHeaderConfig}
+            refinePanelConfig={favouritesPageRefinePanelConfig(
+              searchContextProps,
+            )}
+          />
+        )}
+      </SearchContext.Consumer>
     </Search>
   );
 };
