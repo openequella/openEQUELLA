b9f95ea2a7a57f5fac37ffd363370790febaa685
siddharth-edalex <siddharth@edalex.com>
2025-08-08 08:49:58 +1000

    refactor: favourite unit tests

--- oeq-ts-rest-api/test/Favourite.test.ts
+++ oeq-ts-rest-api/test/Favourite.test.ts
@@ -33,7 +33,7 @@ beforeAll(() => OEQ.Auth.login(TC.API_PATH, TC.USERNAME, TC.PASSWORD));
 afterAll(() => logout(TC.API_PATH));
 
 describe('FavouriteItem', () => {
-  const add = (): Promise<FavouriteItem> => {
+  const addFavItem = (): Promise<FavouriteItem> => {
     const itemKey = '2f6e9be8-897d-45f1-98ea-7aa31b449c0e/1';
     const favouriteItem: FavouriteItem = {
       itemID: itemKey,
@@ -43,13 +43,13 @@ describe('FavouriteItem', () => {
     return addFavouriteItem(TC.API_PATH, favouriteItem);
   };
   it('should be possible to add a favourite Item', async () => {
-    const newFavouriteItem: FavouriteItem = await add();
+    const newFavouriteItem: FavouriteItem = await addFavItem();
     expect(newFavouriteItem).not.toBeNull();
     expect(newFavouriteItem.bookmarkID).toBeTruthy();
   });
 
   it('should be possible to delete a favourite Item', async () => {
-    const newFavouriteItem: FavouriteItem = await add();
+    const newFavouriteItem: FavouriteItem = await addFavItem();
     const newBookmarkID = newFavouriteItem.bookmarkID;
     if (!newBookmarkID) {
       throw new Error("Bookmark ID can't be falsy");
@@ -61,43 +61,60 @@ describe('FavouriteItem', () => {
 });
 
 describe('FavouriteSearch', () => {
-  const add = async (name: string): Promise<FavouriteSearch> =>
-    addFavouriteSearch(TC.API_PATH, {
+  // Keep track of the ids of the favourite search added in each test
+  let favSearchIds: number[] = [];
+
+  const addFavSearch = async (name: string): Promise<FavouriteSearch> => {
+    const favAdded = await addFavouriteSearch(TC.API_PATH, {
       name,
       url: '/page/search?searchOptions=%7B%22rowsPerPage%22%3A10%2C%22currentPage%22%3A0%2C%22sortOrder%22%3A%22RATING%22%2C%22rawMode%22%3Afalse%2C%22status%22%3A%5B%22LIVE%22%2C%22REVIEW%22%5D%2C%22searchAttachments%22%3Atrue%2C%22query%22%3A%22crab%22%2C%22collections%22%3A%5B%5D%2C%22lastModifiedDateRange%22%3A%7B%7D%2C%22mimeTypeFilters%22%3A%5B%5D%2C%22dateRangeQuickModeEnabled%22%3Atrue%7D',
     });
+    favSearchIds.push(favAdded.id);
+    return favAdded;
+  };
+
+  afterEach(async () => {
+    await Promise.all(
+      favSearchIds.map((id) => deleteFavouriteSearch(TC.API_PATH, id))
+    );
+    favSearchIds = [];
+  });
 
   it('should be able to add a favourite search', async () => {
-    const newFavouriteSearch = await add('addFavSearch');
+    const newFavouriteSearch = await addFavSearch('addFavSearch');
     expect(newFavouriteSearch).toBeTruthy();
+    expect(newFavouriteSearch).toHaveProperty('id');
   });
 
   it('should be able to delete a favourite search', async () => {
-    const { id, name } = await add('deleteFavSearch');
+    const { id, name } = await addFavSearch('deleteFavSearch');
     await deleteFavouriteSearch(TC.API_PATH, id);
+    // Filter out this fav. search id from favSearchIds as it has already been deleted
+    favSearchIds = favSearchIds.filter((i) => i !== id);
+
     const res = await getFavouriteSearches(TC.API_PATH, { query: name });
     expect(res.results.map((s) => s.id)).not.toContain(id);
   });
 
   describe('Favourite Search with GET:', () => {
     it('no filters', async () => {
-      const initialRes = await getFavouriteSearches(TC.API_PATH);
-      const initialResLength = initialRes.results.length;
-
-      await add('getWithNoFilters');
+      await addFavSearch('getWithNoFilters1');
+      await addFavSearch('getWithNoFilters2');
+      await addFavSearch('getWithNoFilters3');
 
       const res = await getFavouriteSearches(TC.API_PATH);
-      expect(res.results).toHaveLength(initialResLength + 1);
+      expect(res.results).toHaveLength(3);
     });
 
     it('filters by query', async () => {
-      const { name }: FavouriteSearch = await add('getWithQueryParam');
+      const { name }: FavouriteSearch = await addFavSearch('getWithQueryParam');
       const res = await getFavouriteSearches(TC.API_PATH, { query: name });
       expect(res.results.map((r) => r.name)).toContain(name);
     });
 
     it('supports paging (start / length)', async () => {
-      const { name }: FavouriteSearch = await add(`getWithPagingParams`);
+      const { name }: FavouriteSearch =
+        await addFavSearch(`getWithPagingParams`);
 
       const page: SearchResult<FavouriteSearch> = await getFavouriteSearches(
         TC.API_PATH,
@@ -107,28 +124,29 @@ describe('FavouriteSearch', () => {
       expect(page.start).toBe(0);
       expect(page).toHaveLength(1);
       expect(page.results).toHaveLength(1);
-      expect(getNameFromResults(page, 0)).toBe(name);
+      expect(getSearchName(page, 0)).toBe(name);
     });
 
     it('sorts alphabetically when order="name"', async () => {
-      const searchARes = await add('searchA-getWithOrderParam');
-      const searchZRes = await add('searchZ-getWithOrderParam');
+      const searchARes = await addFavSearch('searchA-getWithOrderParam');
+      const searchZRes = await addFavSearch('searchZ-getWithOrderParam');
 
       const res = await getFavouriteSearches(TC.API_PATH, {
         query: 'getWithOrderParam',
         order: 'name',
       });
 
-      expect(getNameFromResults(res, 0)).toBe(searchARes.name);
-      expect(getNameFromResults(res, 1)).toBe(searchZRes.name);
+      expect(getSearchName(res, 0)).toBe(searchARes.name);
+      expect(getSearchName(res, 1)).toBe(searchZRes.name);
     });
 
     it('Get favourite searches within a date range', async () => {
-      const { id, name, addedAt } = await add('getWithDateRangeParams');
+      const { id, name, addedAt } = await addFavSearch(
+        'getWithDateRangeParams'
+      );
 
       // Helper function to format a Date as 'yyyy-MM-dd' (ISO date without time)
       const toIsoDate = (d: Date) => d.toISOString().slice(0, 10);
-
       // Number of milliseconds in one day
       const MS_DAY = 24 * 60 * 60 * 1_000;
 
@@ -144,7 +162,7 @@ describe('FavouriteSearch', () => {
   });
 });
 
-const getNameFromResults = (
+const getSearchName = (
   res: SearchResult<FavouriteSearch>,
   index: number
 ): string | undefined => res.results[index]?.name;
