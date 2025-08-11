6354e775adf5b6edf6f3965f8c85f09ff0ab0a02
Penghai Zhang <penghai.zhang@edalex.com>
2025-08-11 19:31:59 +1000

    feat: support using the configure user ID attribute to search for users
    This support is mainly used in the authentication process of an LMS integration where
    the provided username is typically a legacy user ID instead of an IdP standard user ID.
    Therefore, rather than using the standard single-user GET endpoint, the multi-user endpoint
    which supports filtering by custom attributes should be used to retrieve user details.

--- Source/Plugins/Core/com.equella.core/scalasrc/com/tle/core/usermanagement/Auth0UserDirectory.scala
+++ Source/Plugins/Core/com.equella.core/scalasrc/com/tle/core/usermanagement/Auth0UserDirectory.scala
@@ -18,26 +18,11 @@
 
 package com.tle.core.usermanagement
 
-import com.tle.common.usermanagement.user.valuebean.{DefaultUserBean, UserBean}
 import com.tle.core.guice.Bind
 import com.tle.core.oauthclient.ClientSecretTokenRequest
 import com.tle.integration.oidc.idp.{GenericIdentityProviderDetails, IdentityProviderPlatform}
 import io.circe.Decoder
-import io.circe.generic.semiauto.deriveDecoder
 import sttp.model.Uri
-
-import java.net.URI
-
-/** Structure for the information of a single user returned from Auth0.
-  */
-case class Auth0User(
-    user_id: String,
-    name: String,
-    username: Option[String],
-    family_name: Option[String],
-    given_name: Option[String],
-    email: Option[String]
-)
 
 /** The primary target of this User Directory is Auth0's User Management APIs. However, it can also
   * be reused for other platform's APIs as long as the OIDC configuration fits
@@ -57,32 +42,24 @@ class Auth0UserDirectory extends ApiUserDirectory {
 
   override type IDP = GenericIdentityProviderDetails
 
-  override protected type USER = Auth0User
-
-  override protected implicit val userDecoder: Decoder[Auth0User] = deriveDecoder[Auth0User]
-
-  override protected type USERS = List[Auth0User]
-
-  override protected implicit val usersDecoder: Decoder[List[Auth0User]] =
-    Decoder.decodeList[Auth0User]
-
-  override protected def toUserList(users: USERS): List[Auth0User] = users
-
   /** Family name and given name are optional fields in Auth0's user data. If family name is absent,
     * use the account name as the family name since account name always exists and is more
     * human-readable than the user ID. If given name is absent, use an empty string as the given
     * name. With this set up, those pages which use family name and given name will not end up
     * showing a blank string.
     */
-  override protected def toUserBean(user: Auth0User): UserBean = {
-    new DefaultUserBean(
-      user.user_id,
-      user.username.getOrElse(user.user_id),
-      user.given_name.getOrElse(""),
-      user.family_name.getOrElse(user.name),
-      user.email.orNull
-    )
+  override protected implicit val userDecoder: Decoder[IdPUser] = Decoder.instance { cursor =>
+    for {
+      id          <- cursor.downField("user_id").as[String]
+      accountName <- cursor.downField("name").as[Option[String]]
+      username    <- cursor.downField("username").as[Option[String]]
+      firstName   <- cursor.downField("given_name").as[Option[String]]
+      lastName    <- cursor.downField("family_name").as[Option[String]]
+      email       <- cursor.downField("email").as[Option[String]]
+    } yield IdPUser(id, username.orElse(accountName), firstName, lastName, email, cursor.value)
   }
+
+  override protected implicit val usersDecoder: Decoder[List[IdPUser]] = Decoder.decodeList[IdPUser]
 
   override protected def userEndpoint(idp: GenericIdentityProviderDetails, id: String): Uri =
     ApiUserDirectory.buildCommonUserEndpoint(idp.apiUrl, Seq("users"), id)
@@ -91,6 +68,24 @@ class Auth0UserDirectory extends ApiUserDirectory {
     Uri(idp.apiUrl.toURI)
       .addPath("users")
       .addParam("q", query)
+
+  /** Auth0 also supports using query parameter `q` to search against user attributes, with each
+    * condition in the format of `attr:"value"`, but wildcard searches against custom attributes are
+    * not supported.
+    *
+    * Double quotes around the value are required to prevent syntax errors when the value contains
+    * special characters like `*`.
+    *
+    * Multiple criteria can be combined using `OR` or `AND`.
+    */
+  override protected def userListByAttrsEndpoint(
+      idp: GenericIdentityProviderDetails,
+      value: String,
+      attrs: List[String]
+  ): Uri = {
+    val filters = attrs.map(attr => s"$attr:\"$value\"").mkString(" OR ")
+    Uri(idp.apiUrl.toURI).addPath("users").addParam("q", filters)
+  }
 
   /** According to the doco of Auth0, 'audience' is required in the request and the value should be
     * the API URL.
---
+++ autotest/OldTests/src/test/java/io/github/openequella/pages/myresources/MyResourcesPage.scala
@@ -0,0 +1,9 @@
+package io.github.openequella.pages.myresources
+
+import com.tle.webtests.framework.PageContext
+import io.github.openequella.pages.search.AbstractSearchPage
+import org.openqa.selenium.By
+
+class MyResourcesPage(context: PageContext) extends AbstractSearchPage[MyResourcesPage](context) {
+  loadedBy = By.xpath("//h5[text()='My Resources']")
+}
