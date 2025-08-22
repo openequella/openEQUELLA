f9e360d9a904261c4bd55ae23a58845b274b8428
Penghai Zhang <penghai.zhang@edalex.com>
2025-08-22 10:05:24 +1000

    test: run test cases 'searchUser' and 'userIdAttribute' in New UI only
    These two test cases are UI-irrelevant so use New UI only.

--- autotest/OldTests/src/test/java/io/github/openequella/pages/myresources/NewMyResourcesPage.scala
+++ autotest/OldTests/src/test/java/io/github/openequella/pages/myresources/NewMyResourcesPage.scala
@@ -4,10 +4,7 @@ import com.tle.webtests.framework.PageContext
 import io.github.openequella.pages.search.AbstractSearchPage
 import org.openqa.selenium.By
 
-class MyResourcesPage(context: PageContext) extends AbstractSearchPage[MyResourcesPage](context) {
+class NewMyResourcesPage(context: PageContext)
+    extends AbstractSearchPage[NewMyResourcesPage](context) {
   loadedBy = By.xpath("//h5[text()='My Resources']")
-
-  override protected def loadUrl(): Unit = {
-    driver.get(context.getBaseUrl + "page/myresources")
-  }
 }
