b7019778b3b27d13c84bf6472b00e4f1975d0965
Penghai Zhang <penghai.zhang@edalex.com>
2025-08-26 10:37:08 +1000

    test: add test description to DashboardApiTest

--- autotest/OldTests/src/test/scala/io/github/openequella/rest/DashboardApiTest.scala
+++ autotest/OldTests/src/test/scala/io/github/openequella/rest/DashboardApiTest.scala
@@ -1,16 +1,14 @@
 package io.github.openequella.rest
 
-import com.tle.webtests.framework.TestInstitution
 import org.apache.commons.httpclient.HttpStatus
 import org.apache.commons.httpclient.methods.GetMethod
 import org.junit.Assert.assertEquals
 import org.testng.annotations.Test
 
-@TestInstitution("fiveo")
 class DashboardApiTest extends AbstractRestApiTest {
   private val DASHBOARD_API_ENDPOINT = getTestConfig.getInstitutionUrl + "api/dashboard"
 
-  @Test(description = "")
+  @Test(description = "Retrieve dashboard details")
   def dashboardDetails(): Unit = {
     val request = new GetMethod(DASHBOARD_API_ENDPOINT)
 
@@ -18,6 +16,8 @@ class DashboardApiTest extends AbstractRestApiTest {
     assertEquals(HttpStatus.SC_OK, respCode)
 
     val resp = mapper.readTree(request.getResponseBodyAsStream)
-    assertEquals(4, resp.get("portlets").size())
+    assertEquals(4, resp.get("portlets").size()) // User 'autotest' has 4 viewable portlets.
+
+    // todo: assert dashboard layout when implemented
   }
 }
