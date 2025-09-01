6aaee8d6d4b1283f23c579c961829a1a8e997a1d
Penghai Zhang <penghai.zhang@edalex.com>
2025-09-01 14:36:29 +1000

    feat: create a new PUT endpoint for dashboard layout update

--- autotest/OldTests/src/test/scala/io/github/openequella/rest/DashboardApiTest.scala
+++ autotest/OldTests/src/test/scala/io/github/openequella/rest/DashboardApiTest.scala
@@ -1,12 +1,13 @@
 package io.github.openequella.rest
 
 import org.apache.commons.httpclient.HttpStatus
-import org.apache.commons.httpclient.methods.GetMethod
+import org.apache.commons.httpclient.methods.{GetMethod, PutMethod}
 import org.junit.Assert.assertEquals
 import org.testng.annotations.Test
 
 class DashboardApiTest extends AbstractRestApiTest {
   private val DASHBOARD_API_ENDPOINT = getTestConfig.getInstitutionUrl + "api/dashboard"
+  private val LAYOUT                 = "layout"
 
   @Test(description = "Retrieve dashboard details")
   def dashboardDetails(): Unit = {
@@ -17,7 +18,37 @@ class DashboardApiTest extends AbstractRestApiTest {
 
     val resp = mapper.readTree(request.getResponseBodyAsStream)
     assertEquals(4, resp.get("portlets").size()) // User 'autotest' has 4 viewable portlets.
+    assertEquals("SingleColumn", resp.get(LAYOUT).asText)
+  }
 
-    // todo: assert dashboard layout when implemented
+  @Test(
+    description = "Update dashboard layout using a valid value",
+    dependsOnMethods = Array("dashboardDetails")
+  )
+  def dashboardLayoutUpdate(): Unit = {
+    val newLayout = "TwoEqualColumns"
+
+    val updateRequest = new PutMethod(DASHBOARD_API_ENDPOINT)
+    val body          = mapper.createObjectNode
+    body.put(LAYOUT, newLayout)
+    val updateResultCode = makeClientRequestWithEntity(updateRequest, body)
+    assertEquals(HttpStatus.SC_NO_CONTENT, updateResultCode)
+
+    // Get the dashboard details to verify the layout was updated.
+    val request = new GetMethod(DASHBOARD_API_ENDPOINT)
+    makeClientRequest(request)
+    val resp = mapper.readTree(request.getResponseBodyAsStream)
+    assertEquals(newLayout, resp.get(LAYOUT).asText)
+  }
+
+  @Test(
+    description = "Update dashboard layout using an invalid value"
+  )
+  def dashboardLayoutUpdateFailed(): Unit = {
+    val updateRequest = new PutMethod(DASHBOARD_API_ENDPOINT)
+    val body          = mapper.createObjectNode
+    body.put(LAYOUT, "ThreeColumns")
+    val updateResultCode = makeClientRequestWithEntity(updateRequest, body)
+    assertEquals(HttpStatus.SC_BAD_REQUEST, updateResultCode)
   }
 }
