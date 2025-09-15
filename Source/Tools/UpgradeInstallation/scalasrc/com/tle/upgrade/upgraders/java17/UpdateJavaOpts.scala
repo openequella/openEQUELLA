a30f3a7284572eb9886612aef1de65993da3c676
Ian Stevenson <ian.stevenson@edalex.com>
2025-09-15 18:43:16 +1000

    refactor(upgrader): move server config constants to ApplicationFiles interface
    Provides better organization allowing for easier use elsewhere.
    
    OEQ-2746

--- Source/Tools/UpgradeInstallation/scalasrc/com/tle/upgrade/upgraders/java17/UpdateJavaOpts.scala
+++ Source/Tools/UpgradeInstallation/scalasrc/com/tle/upgrade/upgraders/java17/UpdateJavaOpts.scala
@@ -22,10 +22,7 @@ import com.dytech.edge.common.Constants
 import com.tle.common.util.ExecUtils
 import com.tle.upgrade.{LineFileModifier, UpgradeResult}
 import com.tle.upgrade.upgraders.AbstractUpgrader
-import com.tle.upgrade.upgraders.AbstractUpgrader.{
-  EQUELLA_SERVER_CONFIG_LINUX,
-  EQUELLA_SERVER_CONFIG_WINDOWS
-}
+import com.tle.upgrade.ApplicationFiles.{EQUELLA_SERVER_CONFIG_LINUX, EQUELLA_SERVER_CONFIG_WINDOWS}
 
 import java.io.File
 import scala.util.{Failure, Success, Try}
---
+++ Source/Tools/UpgradeInstallation/src/com/tle/upgrade/ApplicationFiles.java
@@ -0,0 +1,6 @@
+package com.tle.upgrade;
+
+public interface ApplicationFiles {
+  String EQUELLA_SERVER_CONFIG_LINUX = "equellaserver-config.sh";
+  String EQUELLA_SERVER_CONFIG_WINDOWS = "equellaserver-config.bat";
+}
