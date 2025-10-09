08095d39b141e0b94176bf9ee76524259e0cb9f4
I Stevenson <ian+gitlab@edalex.com>
2025-10-10 08:57:35 +1100

    Apply 1 suggestion(s) to 1 file(s)

--- project/CommonSettings.scala
+++ project/CommonSettings.scala
@@ -41,7 +41,7 @@ object CommonSettings extends AutoPlugin {
     lazy val platformSwing   = LocalProject("com_tle_platform_swing")
     lazy val platformEquella = LocalProject("com_tle_platform_equella")
     lazy val postgresDep     = "org.postgresql"          % "postgresql" % "42.7.8"
-    lazy val sqlServerDep    = "com.microsoft.sqlserver" % "mssql-jdbc" % "13.2.0.x86"
+    lazy val sqlServerDep    = "com.microsoft.sqlserver" % "mssql-jdbc" % "13.2.0.jre11"
 
     lazy val log4jVersion   = "2.25.2"
     lazy val log4j          = "org.apache.logging.log4j" % "log4j"             % log4jVersion
